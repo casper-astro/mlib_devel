a307493576c2286bf1b8a256edb56d8dd5170fae
Andrew Martens <martens.andrew@gmail.com>
2008-06-12 11:55:53 +0000

    Added tostring.m which checks input is not a string already before converting to a string. Implemented in flatstrcell.m and seems to operate as needed.

---
+++ casper_library/tostring.m
@@ -0,0 +1,16 @@
+function string = tostring( something )
+    
+if isa(something, 'char'), 
+%    disp('string')
+    string = something;
+    return;
+end
+%disp('not string');
+if isa(something, 'numeric'), 
+    string = mat2str(something);
+    return;
+end
+disp('Error in data passed to tostring function')
+    
+    
+    
