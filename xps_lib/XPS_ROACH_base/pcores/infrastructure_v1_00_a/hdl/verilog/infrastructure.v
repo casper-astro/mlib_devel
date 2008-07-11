a444344252c627d6fc713f168047372e84656e71
David George <david.george@ska.ac.za>
2008-07-11 22:05:07 +0000

    changed infrastructure to use sys_clk as primary fabric clock changed sys_clk to epb_clk where appropriate

--- xps_lib/XPS_ROACH_base/pcores/infrastructure_v1_00_a/hdl/verilog/infrastructure.v
+++ xps_lib/XPS_ROACH_base/pcores/infrastructure_v1_00_a/hdl/verilog/infrastructure.v
@@ -1,22 +1,22 @@
 module infrastructure(
-    mstr_clk_n, mstr_clk_p,
-    mstr_clk,
+    sys_clk_n, sys_clk_p,
+    sys_clk, sys_clk90,
     dly_clk_n,  dly_clk_p,
     dly_clk,
     epb_clk_buf,
-    epb_clk, epb_clk90,
+    epb_clk,
     idelay_rst, idelay_rdy,
     aux_clk_0_n, aux_clk_0_p,
     aux_clk_0,
     aux_clk_1_n, aux_clk_1_p,
     aux_clk_1
   );
-  input  mstr_clk_n, mstr_clk_p;
-  output mstr_clk;
+  input  sys_clk_n, sys_clk_p;
+  output sys_clk, sys_clk90;
   input  dly_clk_n, dly_clk_p;
   output dly_clk;
   input  epb_clk_buf;
-  output epb_clk, epb_clk90;
+  output epb_clk;
   input  aux_clk_0_n, aux_clk_0_p;
   output aux_clk_0;
   input  aux_clk_1_n, aux_clk_1_p;
@@ -34,46 +34,50 @@ module infrastructure(
     .O(epb_clk_ibuf)
   );
 
-  wire epb_clk_dcm, epb_clk90_dcm;
-  DCM_BASE #(
-    .CLKIN_PERIOD(10.0)
-  ) DCM_BASE_inst (
-    .CLK0(epb_clk_dcm),
-    .CLK180(),
-    .CLK270(),
-    .CLK2X(),
-    .CLK2X180(),
-    .CLK90(epb_clk90_dcm),
-    .CLKDV(),
-    .CLKFX(),
-    .CLKFX180(),
-    .LOCKED(),
-    .CLKFB(epb_clk),
-    .CLKIN(epb_clk_ibuf),
-    .RST(1'b0)
-  );
-
-  BUFG bufg_epb[1:0](
-    .I({epb_clk_dcm, epb_clk90_dcm}), .O({epb_clk, epb_clk90})
+  BUFG bufg_epb(
+    .I(epb_clk_ibuf), .O(epb_clk)
   );
 
   /* other clocks */
 
   wire [2:0] bufg_int;
 
+  wire sys_clk_int;
+
   IBUFGDS #(
     .IOSTANDARD("LVDS_25"),
     .DIFF_TERM("TRUE")
   ) ibufgd_arr [2:0](
-    .I ({mstr_clk_p, aux_clk_1_p, aux_clk_0_p}),
-    .IB({mstr_clk_n, aux_clk_1_n, aux_clk_0_n}),
-    .O (bufg_int)
+    .I ({sys_clk_p, aux_clk_1_p, aux_clk_0_p}),
+    .IB({sys_clk_n, aux_clk_1_n, aux_clk_0_n}),
+    .O ({sys_clk_int, aux_clk_1, aux_clk_0})
   );
 
-  BUFG bufg_arr[2:0](
-    .I(bufg_int), .O({mstr_clk, aux_clk_1, aux_clk_0})
+  wire sys_clk_dcm, sys_clk90_dcm;
+  DCM_BASE #(
+    .CLKIN_PERIOD(10.0)
+  ) DCM_BASE_inst (
+    .CLK0(sys_clk_dcm),
+    .CLK180(),
+    .CLK270(),
+    .CLK2X(),
+    .CLK2X180(),
+    .CLK90(sys_clk90_dcm),
+    .CLKDV(),
+    .CLKFX(),
+    .CLKFX180(),
+    .LOCKED(),
+    .CLKFB(sys_clk),
+    .CLKIN(sys_clk_int),
+    .RST(1'b0)
   );
 
+  BUFG bufg_sys_clk[1:0](
+    .I({sys_clk_dcm, sys_clk90_dcm}),
+    .O({sys_clk, sys_clk90})
+  );
+
+  /******** Delay Clock *********/
   wire dly_clk_int;
   IBUFDS ibufds_dly_clk(
     .I (dly_clk_p),
