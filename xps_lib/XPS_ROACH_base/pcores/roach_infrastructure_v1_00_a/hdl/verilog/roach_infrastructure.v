ccf3a633ebbc722ec8d1b732946a9cb06d395c04
Henry Chen <hchen05@ssl.berkeley.edu>
2009-01-05 00:31:22 +0000

    - Added DCMs for aux_clks - Removed 180, 270 phase sys_clks; use inversion to infer Xilinx clock inversion structures.

--- xps_lib/XPS_ROACH_base/pcores/roach_infrastructure_v1_00_a/hdl/verilog/roach_infrastructure.v
+++ xps_lib/XPS_ROACH_base/pcores/roach_infrastructure_v1_00_a/hdl/verilog/roach_infrastructure.v
@@ -38,46 +38,88 @@ module roach_infrastructure(
     .I(epb_clk_ibuf), .O(epb_clk)
   );
 
-  /* other clocks */
-
-  wire [2:0] bufg_int;
-
+  /* system clock */
   wire sys_clk_int;
+  wire sysclk_dcm_locked;
 
   IBUFGDS #(
     .IOSTANDARD("LVDS_25"),
     .DIFF_TERM("TRUE")
-  ) ibufgd_arr [2:0](
-    .I ({sys_clk_p, aux_clk_1_p, aux_clk_0_p}),
-    .IB({sys_clk_n, aux_clk_1_n, aux_clk_0_n}),
-    .O ({sys_clk_int, aux_clk_1, aux_clk_0})
+  ) ibufgd_sys
+    .I (sys_clk_p),
+    .IB(sys_clk_n),
+    .O (sys_clk_int)
   );
 
-  wire sys_clk_dcm, sys_clk90_dcm, sys_clk180_dcm, sys_clk270_dcm;
+  wire sys_clk_dcm, sys_clk90_dcm;
   DCM_BASE #(
     .CLKIN_PERIOD(10.0)
-  ) DCM_BASE_inst (
+  ) SYS_CLK_DCM (
     .CLK0(sys_clk_dcm),
-    .CLK180(sys_clk180_dcm),
-    .CLK270(sys_clk270_dcm),
+    .CLK180(),
+    .CLK270(),
     .CLK2X(),
     .CLK2X180(),
     .CLK90(sys_clk90_dcm),
     .CLKDV(),
     .CLKFX(),
     .CLKFX180(),
-    .LOCKED(),
+    .LOCKED(sysclk_dcm_locked),
     .CLKFB(sys_clk),
     .CLKIN(sys_clk_int),
     .RST(1'b0)
   );
 
-  BUFG bufg_sys_clk[3:0](
-    .I({sys_clk_dcm, sys_clk90_dcm, sys_clk180_dcm, sys_clk270_dcm}),
-    .O({sys_clk,     sys_clk90,     sys_clk180,     sys_clk270})
+  BUFG bufg_sys_clk[1:0](
+    .I({sys_clk_dcm, sys_clk90_dcm}),
+    .O({sys_clk,     sys_clk90})
   );
 
-  /******** Delay Clock *********/
+  // rely on Xilinx internal clock inversion structures down the line
+  assign sys_clk180 = ~sys_clk;
+  assign sys_clk270 = ~sys_clk90;
+
+  /* Aux clocks */
+  wire  aux_clk_0_int;
+  wire  aux_clk_1_int;
+  IBUFGDS #(
+    .IOSTANDARD("LVDS_25"),
+    .DIFF_TERM("TRUE")
+  ) ibufgd_aux_arr[1:0]
+    .I ({aux_clk_0_p,   aux_clk_1_p}),
+    .IB({aux_clk_0_n,   aux_clk_1_n}),
+    .O ({aux_clk_0_int, aux_clk_1_int})
+  );
+
+  wire  aux_clk_0_dcm;
+  wire  aux_clk_1_dcm;
+
+  DCM_BASE #(
+    .CLKIN_PERIOD(5.0)
+  ) AUX_CLK_0_DCM (
+    .CLK0(  aux_clk_0_dcm),
+    .LOCKED(),
+    .CLKFB( aux_clk_0),
+    .CLKIN( aux_clk_0_int),
+    .RST(   ~sysclk_dcm_locked)
+  );
+
+  DCM_BASE #(
+    .CLKIN_PERIOD(5.0)
+  ) AUX_CLK_0\1_DCM (
+    .CLK0(  aux_clk_1_dcm),
+    .LOCKED(),
+    .CLKFB( aux_clk_1),
+    .CLKIN( aux_clk_1_int),
+    .RST(   ~sysclk_dcm_locked)
+  );
+
+  BUFG bufg_aux_clk[1:0](
+    .I({aux_clk_0_dcm, aux_clk_1_dcm}),
+    .O({aux_clk_0,     aux_clk_1})
+  );
+
+  /* Delay Clock */
   wire dly_clk_int;
   IBUFDS ibufds_dly_clk(
     .I (dly_clk_p),
