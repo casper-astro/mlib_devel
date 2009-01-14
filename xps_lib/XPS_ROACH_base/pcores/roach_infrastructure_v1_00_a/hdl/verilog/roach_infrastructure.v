0f0c6af4180decbc3746eae23f2cdb6b1dab351a
Henry Chen <hchen05@ssl.berkeley.edu>
2009-01-14 02:42:23 +0000

    - Changed naming for aux_clk inputs to better match convention.

--- xps_lib/XPS_ROACH_base/pcores/roach_infrastructure_v1_00_a/hdl/verilog/roach_infrastructure.v
+++ xps_lib/XPS_ROACH_base/pcores/roach_infrastructure_v1_00_a/hdl/verilog/roach_infrastructure.v
@@ -6,10 +6,11 @@ module roach_infrastructure(
     epb_clk_buf,
     epb_clk,
     idelay_rst, idelay_rdy,
-    aux_clk_0_n, aux_clk_0_p,
+    aux0_clk_n, aux0_clk_p,
     aux0_clk, aux0_clk90, aux0_clk180, aux0_clk270,
-    aux_clk_1_n, aux_clk_1_p,
-    aux1_clk, aux1_clk90, aux1_clk180, aux1_clk270
+    aux1_clk_n, aux1_clk_p,
+    aux1_clk, aux1_clk90, aux1_clk180, aux1_clk270,
+    aux0_clk2x, aux0_clk2x90, aux0_clk2x180, aux0_clk2x270
   );
   input  sys_clk_n, sys_clk_p;
   output sys_clk, sys_clk90, sys_clk180, sys_clk270;
@@ -17,10 +18,11 @@ module roach_infrastructure(
   output dly_clk;
   input  epb_clk_buf;
   output epb_clk;
-  input  aux_clk_0_n, aux_clk_0_p;
+  input  aux0_clk_n, aux0_clk_p;
   output aux0_clk, aux0_clk90, aux0_clk180, aux0_clk270;
-  input  aux_clk_1_n, aux_clk_1_p;
+  input  aux1_clk_n, aux1_clk_p;
   output aux1_clk, aux1_clk90, aux1_clk180, aux1_clk270;
+  output aux0_clk2x, aux0_clk2x90, aux0_clk2x180, aux0_clk2x270;
 
   input  idelay_rst;
   output idelay_rdy;
@@ -55,6 +57,7 @@ module roach_infrastructure(
   DCM_BASE #(
     .CLKIN_PERIOD   (10.0)
   ) SYSCLK_DCM (
+    .CLKIN      (sys_clk_int),
     .CLK0       (sys_clk_dcm),
     .CLK180     (),
     .CLK270     (),
@@ -66,7 +69,6 @@ module roach_infrastructure(
     .CLKFX180   (),
     .LOCKED     (sysclk_dcm_locked),
     .CLKFB      (sys_clk),
-    .CLKIN      (sys_clk_int),
     .RST        (1'b0)
   );
 
@@ -80,15 +82,15 @@ module roach_infrastructure(
   assign sys_clk270 = ~sys_clk90;
 
   /* Aux clocks */
-  wire  aux_clk_0_int;
-  wire  aux_clk_1_int;
+  wire  aux0_clk_int;
+  wire  aux1_clk_int;
   IBUFGDS #(
     .IOSTANDARD ("LVDS_25"),
     .DIFF_TERM  ("TRUE")
   ) ibufgd_aux_arr[1:0] (
-    .I  ({aux_clk_0_p,   aux_clk_1_p}),
-    .IB ({aux_clk_0_n,   aux_clk_1_n}),
-    .O  ({aux_clk_0_int, aux_clk_1_int})
+    .I  ({aux0_clk_p,   aux1_clk_p}),
+    .IB ({aux0_clk_n,   aux1_clk_n}),
+    .O  ({aux0_clk_int, aux1_clk_int})
   );
 
   wire  aux0_clk_dcm;
@@ -96,15 +98,24 @@ module roach_infrastructure(
   wire  aux1_clk_dcm;
   wire  aux1_clk90_dcm;
 
+  wire  aux0_clk_dcm_locked;
+  wire  aux1_clk_dcm_locked;
+
+  wire  aux0_clk2x_int;
+  wire  aux0_clk2x_buf;
+  wire  aux0_clk2x_dcm;
+  wire  aux0_clk2x90_dcm;
+
   DCM_BASE #(
     .CLKIN_PERIOD       (5.0),
     .DLL_FREQUENCY_MODE ("HIGH")
   ) AUXCLK0_DCM (
+    .CLKIN  (aux0_clk_int),
     .CLK0   (aux0_clk_dcm),
     .CLK90  (aux0_clk90_dcm),
-    .LOCKED (),
+    .CLK2X  (aux0_clk2x_int),
+    .LOCKED (aux0_clk_dcm_locked),
     .CLKFB  (aux0_clk),
-    .CLKIN  (aux_clk_0_int),
     .RST    (~sysclk_dcm_locked)
   );
 
@@ -112,17 +123,35 @@ module roach_infrastructure(
     .CLKIN_PERIOD       (5.0),
     .DLL_FREQUENCY_MODE ("HIGH")
   ) AUXCLK1_DCM (
+    .CLKIN  (aux1_clk_int),
     .CLK0   (aux1_clk_dcm),
     .CLK90  (aux1_clk90_dcm),
-    .LOCKED (),
+    .LOCKED (aux1_clk_dcm_locked),
     .CLKFB  (aux1_clk),
-    .CLKIN  (aux_clk_1_int),
     .RST    (~sysclk_dcm_locked)
   );
+
+  DCM_BASE #(
+    .CLKIN_PERIOD       (5.0),
+    .DLL_FREQUENCY_MODE ("HIGH")
+  ) AUXCLK0_2x_DCM (
+    .CLKIN  (aux0_clk2x_buf),
+    .CLK0   (aux0_clk2x_dcm),
+    .CLK90  (aux0_clk2x90_dcm),
+    .LOCKED (),
+    .CLKFB  (aux0_clk2x),
+    .RST    (~aux0_clk_dcm_locked)
+  );
+
 
   BUFG bufg_aux_clk[3:0](
     .I({aux0_clk_dcm, aux0_clk90_dcm, aux1_clk_dcm, aux1_clk90_dcm}),
     .O({aux0_clk,     aux0_clk90,     aux1_clk,     aux1_clk90})
+  );
+
+  BUFG bufg_aux2_clk[2:0](
+    .I({aux0_clk2x_int, aux0_clk2x_dcm, aux0_clk2x90_dcm}),
+    .O({aux0_clk2x_buf, aux0_clk2x,     aux0_clk2x90})
   );
 
   // rely on inference of Xilinx internal clock inversion structures down the line
@@ -130,6 +159,9 @@ module roach_infrastructure(
   assign aux0_clk270 = ~aux0_clk90;
   assign aux1_clk180 = ~aux1_clk;
   assign aux1_clk270 = ~aux1_clk90;
+
+  assign aux0_clk2x180 = ~aux0_clk2x;
+  assign aux0_clk2x270 = ~aux0_clk2x90;
 
 
   /* Delay Clock */
