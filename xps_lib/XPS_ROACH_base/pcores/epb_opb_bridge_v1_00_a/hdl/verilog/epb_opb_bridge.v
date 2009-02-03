4d94e986a574d43fd8306f7b7e6d5854dc7bcf47
Jason Manley <jasonmanley@gmail.com>
2009-02-03 06:22:37 +0000

    OPB bridge plays along with OPB arbiter. Added register stages.

--- xps_lib/XPS_ROACH_base/pcores/epb_opb_bridge_v1_00_a/hdl/verilog/epb_opb_bridge.v
+++ xps_lib/XPS_ROACH_base/pcores/epb_opb_bridge_v1_00_a/hdl/verilog/epb_opb_bridge.v
@@ -54,29 +54,53 @@ module epb_opb_bridge(
 
   /********************** *******************/
 
+  reg  epb_cs_n_reg, epb_oe_n_reg, epb_r_w_n_reg;
+  reg   [1:0] epb_be_n_reg;
+  reg  [22:0] epb_addr_reg;
+  reg   [5:0] epb_addr_gp_reg;
+  reg  [15:0] epb_data_i_reg;
+
+  always @(posedge OPB_Clk) begin
+    epb_cs_n_reg    <= epb_cs_n;
+    epb_oe_n_reg    <= epb_oe_n;
+    epb_r_w_n_reg   <= epb_r_w_n;
+    epb_be_n_reg    <= epb_be_n;
+    epb_addr_reg    <= epb_addr;
+    epb_addr_gp_reg <= epb_addr_gp;
+    epb_data_i_reg  <= epb_data_i;
+  end
+
+  //synthesis attribute IOB of epb_data_i_reg is true
+  //synthesis attribute IOB of epb_addr_gp_reg is true
+  //synthesis attribute IOB of epb_addr_reg is true
+  //synthesis attribute IOB of epb_be_n_reg is true
+  //synthesis attribute IOB of epb_cs_n_reg is true
+  //synthesis attribute IOB of epb_oe_n_reg is true
+  //synthesis attribute IOB of epb_r_w_n_reg is true
+
+
   /***** EPB CS edge detection *****/
   reg prev_cs_n;
   always @(posedge OPB_Clk) begin 
-    prev_cs_n <= epb_cs_n;
+    prev_cs_n <= epb_cs_n_reg;
   end
 
   /***** Misc Assignments *****/
-  wire epb_trans_strb = (prev_cs_n != epb_cs_n && !epb_cs_n);
-  wire epb_trans      = !epb_cs_n;
+  wire epb_trans_strb = (prev_cs_n && !epb_cs_n_reg);
+  wire epb_trans      = !epb_cs_n_reg;
   wire OPB_reply      = OPB_xferAck | OPB_errAck | OPB_timeout | OPB_retry;
 
-  assign epb_data_oe_n = (!epb_r_w_n) | (!epb_trans) | epb_oe_n; //0 when read = 1 and epb_tran = 1 and epb_oe_n = 0 else 1
+  assign epb_data_oe_n = (!epb_r_w_n_reg) | (!epb_trans) | epb_oe_n_reg; //0 when read = 1 and epb_tran = 1 and epb_oe_n = 0 else 1
 
   /***** OPB Output Assignments *****/
   assign M_request = 1'b1;
-  assign M_busLock = 1'b0;
+  assign M_busLock = 1'b1;
   assign M_seqAddr = 1'b0; //TODO: implement bursting
 
   reg M_RNW;
   reg [0:31] M_ABus;
   reg [0:3 ] M_BE;
   reg [0:31] M_DBus;
-
   always @(*) begin
     if (!M_select) begin
       M_RNW  <= 1'b0;
@@ -84,26 +108,27 @@ module epb_opb_bridge(
       M_BE   <= 4'b0;
       M_DBus <= 32'b0;
     end else begin
-      M_RNW  <= epb_r_w_n;
-      M_ABus <= {5'b0, epb_addr_gp[2:0], epb_addr[22:1], 2'b0}; //bit truncated to support 32 bit addressing
-      if (epb_addr[0]) begin
-        M_DBus <= {16'b0, epb_data_i};
-        if (epb_r_w_n) begin
+      M_RNW  <= epb_r_w_n_reg;
+      M_ABus <= {5'b0, epb_addr_gp_reg[2:0], epb_addr_reg[22:1], 2'b0}; //bit truncated to support 32 bit addressing
+      if (epb_addr_reg[0]) begin
+        if (epb_r_w_n_reg) begin
           M_BE   <= {2'b0, 2'b11};
+          M_DBus <= 32'b0;
         end else begin
-          M_BE   <= {2'b0, !epb_be_n[1], !epb_be_n[0]};
+          M_BE   <= {2'b0, !epb_be_n_reg[1], !epb_be_n_reg[0]};
+          M_DBus <= {16'b0, epb_data_i_reg};
         end
       end else begin
-        M_DBus <= {epb_data_i, 16'b0};
-        if (epb_r_w_n) begin
+        if (epb_r_w_n_reg) begin
           M_BE   <= {2'b11, 2'b00};
+          M_DBus <= 32'b0;
         end else begin
-          M_BE   <= {!epb_be_n[1], !epb_be_n[0], 2'b00};
+          M_BE   <= {!epb_be_n_reg[1], !epb_be_n_reg[0], 2'b00};
+          M_DBus <= {epb_data_i_reg, 16'b0};
         end
       end
     end
   end
-
 
   /******************** EPB/OPB State Machine ********************/
 
@@ -111,25 +136,23 @@ module epb_opb_bridge(
   localparam OPB_STATE_IDLE = 2'd0; 
   localparam OPB_STATE_ARB  = 2'd1; 
   localparam OPB_STATE_WAIT = 2'd2; 
-  localparam OPB_STATE_BACKOFF = 2'd3; 
-
-  reg M_select_reg;
 
   /* Cut Through routed M_select */
-  assign M_select = M_select_reg                                                  ||
-                    (opb_state == OPB_STATE_IDLE && epb_trans_strb && OPB_MGrant) ||
-                    (opb_state == OPB_STATE_ARB  && OPB_MGrant);
+  reg M_select_reg;
+  assign M_select = M_select_reg || (opb_state == OPB_STATE_ARB && OPB_MGrant) || (opb_state == OPB_STATE_IDLE && epb_trans_strb);
+ 
   /* Cut Through routed epb_rdy and epb_data_o */
-  assign epb_rdy    = opb_state == OPB_STATE_WAIT && OPB_reply;
-  assign epb_data_o = epb_addr[0] ? OPB_DBus[16:31] : OPB_DBus[0:15];
+  wire         epb_rdy_int = opb_state == OPB_STATE_WAIT && OPB_reply;
+  wire [15:0] epb_data_int = epb_addr_reg[0] ? OPB_DBus[16:31] : OPB_DBus[0:15];
 
-  reg [3:0] backoff;
+  reg [15:0] epb_data_o;
+  reg epb_rdy;
 
-  localparam BACKOFF_HACK = 2;
-  /* TODO: find out my this backoff business is required
-   *       workaround to temporally close reads being or'ed
-   *       together
-   */
+  always @(posedge OPB_Clk) begin
+    epb_rdy   <= epb_rdy_int;
+    if (epb_rdy_int)
+      epb_data_o <= epb_data_int;
+  end
 
   always @(posedge OPB_Clk) begin
     //strobes
@@ -139,8 +162,8 @@ module epb_opb_bridge(
     end else begin
       case (opb_state)
         OPB_STATE_IDLE: begin
-          if (epb_trans_strb) begin //on first
-            if (OPB_MGrant) begin
+          if (epb_trans_strb) begin
+            if (1'b1) begin
               M_select_reg <= 1'b1;
               opb_state    <= OPB_STATE_WAIT;
             end else begin
@@ -163,19 +186,7 @@ module epb_opb_bridge(
             opb_state    <= OPB_STATE_IDLE;
           end else if (OPB_reply) begin
             M_select_reg <= 1'b0;
-            opb_state    <= OPB_STATE_BACKOFF;
-            backoff      <= BACKOFF_HACK;
-          end
-        end
-        OPB_STATE_BACKOFF: begin
-          if (backoff) begin
-            backoff <= backoff - 1;
-          end else begin
-            if (epb_trans) begin
-              opb_state    <= OPB_STATE_ARB;
-            end else begin
-              opb_state    <= OPB_STATE_IDLE;
-            end
+            opb_state    <= OPB_STATE_IDLE;
           end
         end
       endcase
