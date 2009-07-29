e57d02be7ea783be015e65b443a4cd49c5985ed6
Henry Chen <hchen05@ssl.berkeley.edu>
2009-07-29 00:10:33 +0000

    - Enabled targeting ROACH.

--- xps_library/@xps_quadc/xps_quadc.m
+++ xps_library/@xps_quadc/xps_quadc.m
@@ -30,7 +30,14 @@ end
 
 blk_name = get(blk_obj,'simulink_name');
 xsg_obj = get(blk_obj,'xsg_obj');
-s.hw_sys = 'iBOB';
+
+[hw_sys, hw_subsys] = xps_get_hw_plat(get(xsg_obj, 'hw_sys'));
+if isempty(strmatch(hw_sys, {'iBOB', 'ROACH'}))
+    error(['Unsupported hardware system for QuADC: ',hw_sys]);
+else
+    s.hw_sys = hw_sys;
+end % if isempty(strmatch(hw_sys, {'iBOB', 'ROACH'}))
+
 s.adc_brd = get_param(blk_name, 'adc_brd');
 s.adc_str = ['adc', s.adc_brd];
 s.adc_clk_rate = eval_param(blk_name,'adc_clk_rate');
@@ -57,25 +64,25 @@ b = set(b,'misc_ports',misc_ports);
 % external ports
 
 % port indices are 0-indexed, +1 to convert to Matlab 1-indexing
-ext_ports.adc0_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc0_clk_in_p']     ['{iBOB.zdok',s.adc_brd,'_p{[19]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc1_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc1_clk_in_p']     ['{iBOB.zdok',s.adc_brd,'_p{[39]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc2_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc2_clk_in_p']     ['{iBOB.zdok',s.adc_brd,'_p{[32]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc3_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc3_clk_in_p']     ['{iBOB.zdok',s.adc_brd,'_p{[30]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc0_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc0_data_in_p']    ['{iBOB.zdok',s.adc_brd,'_p{[29 9 18 28  8  7 17 27]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
-ext_ports.adc1_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc1_data_in_p']    ['{iBOB.zdok',s.adc_brd,'_p{[37 6 16 26 36  5 15 25]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
-ext_ports.adc2_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc2_data_in_p']    ['{iBOB.zdok',s.adc_brd,'_p{[35 4 14 24 34  3 13 23]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
-ext_ports.adc3_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc3_data_in_p']    ['{iBOB.zdok',s.adc_brd,'_p{[33 2 12 22 20 10 11 21]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
+ext_ports.adc0_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc0_clk_in_p']     ['{',hw_sys,'.zdok',s.adc_brd,'_p{[19]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc1_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc1_clk_in_p']     ['{',hw_sys,'.zdok',s.adc_brd,'_p{[39]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc2_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc2_clk_in_p']     ['{',hw_sys,'.zdok',s.adc_brd,'_p{[32]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc3_clk_in_p  = {1 'in' ['quadc',s.adc_brd,'_adc3_clk_in_p']     ['{',hw_sys,'.zdok',s.adc_brd,'_p{[30]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc0_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc0_data_in_p']    ['{',hw_sys,'.zdok',s.adc_brd,'_p{[29 9 18 28  8  7 17 27]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
+ext_ports.adc1_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc1_data_in_p']    ['{',hw_sys,'.zdok',s.adc_brd,'_p{[37 6 16 26 36  5 15 25]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
+ext_ports.adc2_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc2_data_in_p']    ['{',hw_sys,'.zdok',s.adc_brd,'_p{[35 4 14 24 34  3 13 23]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
+ext_ports.adc3_data_in_p = {8 'in' ['quadc',s.adc_brd,'_adc3_data_in_p']    ['{',hw_sys,'.zdok',s.adc_brd,'_p{[33 2 12 22 20 10 11 21]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
 
-ext_ports.adc0_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc0_clk_in_n']     ['{iBOB.zdok',s.adc_brd,'_n{[19]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc1_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc1_clk_in_n']     ['{iBOB.zdok',s.adc_brd,'_n{[39]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc2_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc2_clk_in_n']     ['{iBOB.zdok',s.adc_brd,'_n{[32]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc3_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc3_clk_in_n']     ['{iBOB.zdok',s.adc_brd,'_n{[30]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.adc0_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc0_data_in_n']    ['{iBOB.zdok',s.adc_brd,'_n{[29 9 18 28  8  7 17 27]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
-ext_ports.adc1_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc1_data_in_n']    ['{iBOB.zdok',s.adc_brd,'_n{[37 6 16 26 36  5 15 25]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
-ext_ports.adc2_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc2_data_in_n']    ['{iBOB.zdok',s.adc_brd,'_n{[35 4 14 24 34  3 13 23]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
-ext_ports.adc3_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc3_data_in_n']    ['{iBOB.zdok',s.adc_brd,'_n{[33 2 12 22 20 10 11 21]+1 ,:}}']   'LVDS_25_DT'    'vector=true'};
+ext_ports.adc0_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc0_clk_in_n']     ['{',hw_sys,'.zdok',s.adc_brd,'_n{[19]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc1_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc1_clk_in_n']     ['{',hw_sys,'.zdok',s.adc_brd,'_n{[39]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc2_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc2_clk_in_n']     ['{',hw_sys,'.zdok',s.adc_brd,'_n{[32]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc3_clk_in_n  = {1 'in' ['quadc',s.adc_brd,'_adc3_clk_in_n']     ['{',hw_sys,'.zdok',s.adc_brd,'_n{[30]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.adc0_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc0_data_in_n']    ['{',hw_sys,'.zdok',s.adc_brd,'_n{[29 9 18 28  8  7 17 27]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
+ext_ports.adc1_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc1_data_in_n']    ['{',hw_sys,'.zdok',s.adc_brd,'_n{[37 6 16 26 36  5 15 25]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
+ext_ports.adc2_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc2_data_in_n']    ['{',hw_sys,'.zdok',s.adc_brd,'_n{[35 4 14 24 34  3 13 23]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
+ext_ports.adc3_data_in_n = {8 'in' ['quadc',s.adc_brd,'_adc3_data_in_n']    ['{',hw_sys,'.zdok',s.adc_brd,'_n{[33 2 12 22 20 10 11 21]+1 ,:}}'] 'LVDS_25_DT'    'vector=true'};
 
-ext_ports.sync_in_p      = {1 'in' ['quadc',s.adc_brd,'_sync_in_p']         ['{iBOB.zdok',s.adc_brd,'_p{[38]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
-ext_ports.sync_in_n      = {1 'in' ['quadc',s.adc_brd,'_sync_in_n']         ['{iBOB.zdok',s.adc_brd,'_n{[38]+1,:}}']                        'LVDS_25_DT'    'vector=false'};
+ext_ports.sync_in_p      = {1 'in' ['quadc',s.adc_brd,'_sync_in_p']         ['{',hw_sys,'.zdok',s.adc_brd,'_p{[38]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
+ext_ports.sync_in_n      = {1 'in' ['quadc',s.adc_brd,'_sync_in_n']         ['{',hw_sys,'.zdok',s.adc_brd,'_n{[38]+1,:}}']                      'LVDS_25_DT'    'vector=false'};
 
 b = set(b,'ext_ports',ext_ports);
