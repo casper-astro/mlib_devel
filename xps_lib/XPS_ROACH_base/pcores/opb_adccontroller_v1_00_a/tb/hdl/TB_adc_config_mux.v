f2f4a0ce77c640e8c0dae5fac491b51ec22bb6c3
Wan Cheng <Wan.Cheng@csiro.au>
2009-12-01 05:50:01 +0000

    Roll back to previous version because a wrong file is uploaded.

--- xps_lib/XPS_ROACH_base/pcores/opb_adccontroller_v1_00_a/tb/hdl/TB_adc_config_mux.v
+++ xps_lib/XPS_ROACH_base/pcores/opb_adccontroller_v1_00_a/tb/hdl/TB_adc_config_mux.v
@@ -17,6 +17,7 @@ module TB_adc_config_mux();
     .rst (sys_rst),
     .request (1'b0),
     .ddrb_i  (1'b0),
+    .mode_i  (1'b0),
 
     .config_start_i (1'b0),
     .config_busy_o  (),
@@ -25,6 +26,7 @@ module TB_adc_config_mux();
 
     .ddrb_o      (),
     .dcm_reset_o (),
+    .mode_o      (),
     .ctrl_clk_o  (),
     .ctrl_strb_o (),
     .ctrl_data_o ()
