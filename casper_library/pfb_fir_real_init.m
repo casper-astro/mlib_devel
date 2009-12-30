3013c0470c44c4f5d9a4fbc800b1973c143618bc
Billy Mallard <wjm@llard.net>
2009-12-30 04:29:46 +0000

    Added a checkbox to the pfb_fir_real block to chose whether to use behavioral HDL in the adder_tree adders instead of pipelining them. Cleaned up the Dialog Callback code for the specify_mult checkbox.

--- casper_library/pfb_fir_real_init.m
+++ casper_library/pfb_fir_real_init.m
@@ -67,6 +67,7 @@ quantization = get_var('quantization', 'defaults', defaults, varargin{:});
 fwidth = get_var('fwidth', 'defaults', defaults, varargin{:});
 specify_mult = get_var('specify_mult', 'defaults', defaults, varargin{:});
 mult_spec = get_var('mult_spec', 'defaults', defaults, varargin{:});
+behavioral = get_var('behavioral', 'defaults', defaults, varargin{:});
 
 if MakeBiplex, pols = 2;
 else, pols = 1;
@@ -146,7 +147,7 @@ for p=1:pols,
 	%add adder tree
 	reuse_block(blk, ['adder_', num2str(p), '_' ,num2str(i)], 'casper_library/Misc/adder_tree', ...
 		'n_inputs', num2str(TotalTaps), 'latency', num2str(add_latency), ...
-		'behavioral', 'on', ...
+		'behavioral', behavioral, ...
 		'Position', [150*(TotalTaps+1) 50*portnum*TotalTaps 150*(TotalTaps+1)+100 50*(portnum+1)*TotalTaps-20]);
 
 	%add shift, convert blocks
