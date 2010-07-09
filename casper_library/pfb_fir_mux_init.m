4d45fa4a09a7a834cea61ff92683eeaf6d6232d4
Griffin Foster <griffin.foster@gmail.com>
2010-07-09 14:50:53 +0100

    Added support for a pfb which handles multiple signals per input, useful for x64 adc which has 4 signals per line

---
+++ casper_library/pfb_fir_mux_init.m
@@ -0,0 +1,170 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%   Center for Astronomy Signal Processing and Electronics Research           %
+%   http://seti.ssl.berkeley.edu/casper/                                      %
+%   Copyright (C) 2007 Terry Filiba, Aaron Parsons                            %
+%                                                                             %
+%   This program is free software; you can redistribute it and/or modify      %
+%   it under the terms of the GNU General Public License as published by      %
+%   the Free Software Foundation; either version 2 of the License, or         %
+%   (at your option) any later version.                                       %
+%                                                                             %
+%   This program is distributed in the hope that it will be useful,           %
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of            %
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             %
+%   GNU General Public License for more details.                              %
+%                                                                             %
+%   You should have received a copy of the GNU General Public License along   %
+%   with this program; if not, write to the Free Software Foundation, Inc.,   %
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function pfb_fir_mux_init(blk, varargin)
+% Initialize and configure the Polyphase Filter Bank.
+%
+% pfb_fir_mux_init(blk, varargin)
+%
+% blk = The block to configure.
+% varargin = {'varname', 'value', ...} pairs
+% 
+% Valid varnames for this block are:
+% PFBSize = The size of the PFB
+% mux_inputs = number of signals per input
+% TotalTaps = Total number of taps in the PFB
+% WindowType = The type of windowing function to use.
+% n_inputs = The number of parallel inputs
+% MakeBiplex = Double up the PFB to feed a biplex FFT
+% BitWidthIn = Input Bitwidth
+% BitWidthOut = Output Bitwidth
+% CoeffBitWidth = Bitwidth of Coefficients.
+% CoeffDistMem = Implement coefficients in distributed memory
+% add_latency = Latency through each adder.
+% mult_latency = Latency through each multiplier
+% bram_latency = Latency through each BRAM.
+% quantization = 'Truncate', 'Round  (unbiased: +/- Inf)', or 'Round
+% (unbiased: Even Values)'
+% fwidth = Scaling of the width of each PFB channel
+
+% Declare any default values for arguments you might like.
+defaults = {};
+if same_state(blk, 'defaults', defaults, varargin{:}), return, end
+check_mask_type(blk, 'pfb_fir_mux');
+munge_block(blk, varargin{:});
+
+PFBSize = get_var('PFBSize', 'defaults', defaults, varargin{:});
+mux_inputs = get_var('mux_inputs', 'defaults', defaults, varargin{:});
+TotalTaps = get_var('TotalTaps', 'defaults', defaults, varargin{:});
+WindowType = get_var('WindowType', 'defaults', defaults, varargin{:});
+n_inputs = get_var('n_inputs', 'defaults', defaults, varargin{:});
+MakeBiplex = get_var('MakeBiplex', 'defaults', defaults, varargin{:});
+BitWidthIn = get_var('BitWidthIn', 'defaults', defaults, varargin{:});
+BitWidthOut = get_var('BitWidthOut', 'defaults', defaults, varargin{:});
+CoeffBitWidth = get_var('CoeffBitWidth', 'defaults', defaults, varargin{:});
+CoeffDistMem = get_var('CoeffDistMem', 'defaults', defaults, varargin{:});
+add_latency = get_var('add_latency', 'defaults', defaults, varargin{:});
+mult_latency = get_var('mult_latency', 'defaults', defaults, varargin{:});
+bram_latency = get_var('bram_latency', 'defaults', defaults, varargin{:});
+quantization = get_var('quantization', 'defaults', defaults, varargin{:});
+fwidth = get_var('fwidth', 'defaults', defaults, varargin{:});
+specify_mult = get_var('specify_mult', 'defaults', defaults, varargin{:});
+mult_spec = get_var('mult_spec', 'defaults', defaults, varargin{:});
+
+if MakeBiplex, pols = 2;
+else, pols = 1;
+end
+
+delete_lines(blk);
+
+% Add ports
+portnum = 1;
+reuse_block(blk, 'sync', 'built-in/inport', ...
+    'Position', [0 50*portnum 30 50*portnum+15], 'Port', num2str(portnum));
+reuse_block(blk, 'sync_out', 'built-in/outport', ...
+    'Position', [150*(TotalTaps+1) 50*portnum 150*(TotalTaps+1)+30 50*portnum+15], 'Port', num2str(portnum));
+for p=1:pols,
+    for i=1:2^n_inputs,
+        portnum = portnum + 1; % Skip one to allow sync & sync_out to be 1
+        in_name = ['pol',num2str(p),'_in',num2str(i)];
+        out_name = ['pol',num2str(p),'_out',num2str(i)];
+        reuse_block(blk, in_name, 'built-in/inport', ...
+            'Position', [0 50*portnum 30 50*portnum+15], 'Port', num2str(portnum));
+        reuse_block(blk, out_name, 'built-in/outport', ...
+            'Position', [150*(TotalTaps+1) 50*portnum 150*(TotalTaps+1)+30 50*portnum+15], 'Port', num2str(portnum));
+    end
+end
+
+% Add Blocks and Lines
+portnum = 0;
+for p=1:pols,
+    for i=1:2^n_inputs,
+        portnum = portnum + 1;
+        for t=1:TotalTaps,
+            in_name = ['pol',num2str(p),'_in',num2str(i)];
+            out_name = ['pol',num2str(p),'_out',num2str(i)];
+            use_hdl = 'on';
+            use_embedded = 'off';
+            if( strcmp(specify_mult,'on') ) 
+                if( mult_spec(t) == 0 ), 
+                    use_embedded = 'off';
+                elseif( mult_spec(t) == 2);
+                    use_hdl = 'on';
+                    use_embedded = 'off';
+                end
+            end
+
+            if t==1,
+                blk_name = [in_name,'_first_tap_mux'];
+                reuse_block(blk, blk_name, 'casper_library/PFBs/first_tap_mux', ...
+                    'use_hdl', tostring(use_hdl), 'use_embedded', tostring(use_embedded),...
+                    'nput', num2str(i-1), 'Position', [150*t 50*portnum 150*t+100 50*portnum+30]);
+                propagate_vars([blk,'/',blk_name],'defaults', defaults, varargin{:});
+                add_line(blk, [in_name,'/1'], [blk_name,'/1']);
+                add_line(blk, 'sync/1', [blk_name,'/2']);
+            elseif t==TotalTaps,
+                blk_name = [in_name,'_last_tap'];
+		        reuse_block(blk, blk_name, 'casper_library/PFBs/last_tap', ...
+                    'use_hdl', tostring(use_hdl), 'use_embedded', tostring(use_embedded),...
+                    'Position', [150*t 50*portnum 150*t+100 50*portnum+30]);
+                propagate_vars([blk,'/',blk_name],'defaults', defaults, varargin{:});
+		        %if t==2,
+                %    prev_blk_name = ['pol',num2str(p),'_in',num2str(i),'_first_tap_mux'];
+                %else,
+                    prev_blk_name = ['pol',num2str(p),'_in',num2str(i),'_tap',num2str(t-1)];
+                %end
+                for n=1:4, add_line(blk, [prev_blk_name,'/',num2str(n)], [blk_name,'/',num2str(n)]);
+                end
+                add_line(blk, [blk_name,'/1'], [out_name,'/1']);
+                if i==1 && p==1, add_line(blk, [blk_name,'/2'], 'sync_out/1');
+                end
+            else,
+                blk_name = ['pol',num2str(p),'_in',num2str(i),'_tap',num2str(t)];
+                
+		%changed by Andrew to include parameters
+
+%		reuse_block(blk, blk_name, 'casper_library/PFBs/tap', ...
+%                   'Position', [150*t 50*portnum 150*t+100 50*portnum+30]);
+		        reuse_block(blk, blk_name, 'casper_library/PFBs/tap', ...
+                    'use_hdl', tostring(use_hdl), 'use_embedded', tostring(use_embedded),...
+		            'mult_latency',tostring(mult_latency), 'coeff_width', tostring(CoeffBitWidth), ...
+		            'coeff_frac_width',tostring(CoeffBitWidth-1), 'delay', tostring(2^(PFBSize+mux_inputs-n_inputs)), ...
+		            'data_width',tostring(BitWidthIn), 'bram_latency', tostring(bram_latency), ...
+                    'Position', [150*t 50*portnum 150*t+100 50*portnum+30]);
+%		    
+		        if t==2,
+                    prev_blk_name = ['pol',num2str(p),'_in',num2str(i),'_first_tap_mux'];
+                else,
+                    prev_blk_name = ['pol',num2str(p),'_in',num2str(i),'_tap',num2str(t-1)];
+                end
+                for n=1:4, add_line(blk, [prev_blk_name,'/',num2str(n)], [blk_name,'/',num2str(n)]);
+                end
+            end
+        end
+    end
+end
+
+clean_blocks(blk);
+
+fmtstr = sprintf('taps=%d, add_latency=%d', TotalTaps, add_latency);
+set_param(blk, 'AttributesFormatString', fmtstr);
+save_state(blk, 'defaults', defaults, varargin{:});
