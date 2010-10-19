fb730679e641cc9375e6a4823c415e1c8536b782
Jack Hickish <jack@maezar.(none)>
2010-10-19 01:25:19 +0100

    Revert "Merge branch 'master' of ssh://scm.physics.ox.ac.uk/gitroot/skadsdsp"
    This reverts commit 01371cb53885fb77bac7ba2acf7396bcb27383b2, reversing
    changes made to ff37e9f488ec4803a81176ee9389dcf7024e55df.

--- xps_lib/XPS_BEE2_ctrl_base/pcores/plb_ddr2_v1_00_b/mem_tester_sim/temp/tcl/ddr2_controller_v2_1_0.bbd.tcl
+++
@@ -1,7 +0,0 @@
-set output_file [open "mem_tester_sim/ddr2_controller_v1_00_a/data/ddr2_controller_v2_1_0.bbd" w ]
-set line_num 1; puts $output_file "## Black box definition file"
-set line_num 2; puts $output_file ""
-set line_num 3; puts $output_file "Files"
-set line_num 4; puts $output_file "ddr2_controller.edf, dcmx3y0_2vp70.edn"
-set line_num 5; puts $output_file ""
-close $output_file
