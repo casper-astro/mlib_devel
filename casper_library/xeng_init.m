casper-astro/mlib_devel

commit 9a373e8efaf14fcb2c460b06bc2bfe03c486979a

Xengine fixes
Fixes to correct for cross-pol terms in "last triangle".

Descramble block now contains all functions for casting datatypes and correcting conjugations. xeng_conj_fix and xeng_cast blocks thus removed from library. Might be slight increase in logic usage for cases of demux 2, 4 or 8 over previous scheme. Logic usage decreased for case demux=1.

casper_library/xeng_init.m
@@ -67,7 +67,7 @@ delete_lines(blk);
 % Add taps
 x = 275;
 
-reuse_block(blk, 'auto_tap', 'casper_library_correlator/auto_tap',...
+reuse_block(blk, 'auto_tap', 'casper_library/Correlator/auto_tap',...
                 'Position',[135, 51, 230, 169], ...
                 'n_ants',num2str(n_ants), ...                            
                 'n_bits',num2str(n_bits), ...
@@ -79,7 +79,7 @@ reuse_block(blk, 'auto_tap', 'casper_library_correlator/auto_tap',...
             
 for i=1:floor(n_ants/2),
         name = ['baseline_tap', num2str(i)];
-        reuse_block(blk, name, 'casper_library_correlator/baseline_tap', ...
+        reuse_block(blk, name, 'casper_library/Correlator/baseline_tap', ...
             'n_ants',num2str(n_ants), ...            
             'n_bits',num2str(n_bits), ...
             'acc_len',num2str(acc_len), ...
@@ -97,29 +97,16 @@ reuse_block(blk, 'Constant', 'xbsIndex_r4/Constant', 'Position', [15,89,85,121],
             'arith_type','Unsigned',...
             'n_bits',sprintf('%d',8 * n_bits_xeng_out));
         
-reuse_block(blk, 'sample_and_hold1', 'casper_library_misc/sample_and_hold', 'Position', [140,255,180,315],...
+reuse_block(blk, 'sample_and_hold1', 'casper_library/Misc/sample_and_hold', 'Position', [140,255,180,315],...
             'period',sprintf('%d',n_ants * acc_len));
 
-reuse_block(blk, 'sample_and_hold2', 'casper_library_misc/sample_and_hold', 'Position', [x + 200,190,x+240,250],...
+reuse_block(blk, 'sample_and_hold2', 'casper_library/Misc/sample_and_hold', 'Position', [x + 100,190,x+140,250],...
             'period',sprintf('%d',n_ants * acc_len));
 
-reuse_block(blk, 'sample_and_hold3', 'casper_library_misc/sample_and_hold', 'Position', [x + 350,190,x+390,250],...
+reuse_block(blk, 'sample_and_hold3', 'casper_library/Misc/sample_and_hold', 'Position', [x + 250,190,x+290,250],...
             'period',sprintf('%d',n_ants * acc_len));
 
-reuse_block(blk, 'x_cast', 'casper_library_correlator/x_cast', ...
-    'Position', [x + 100, 100, x + 150, 125], ...
-    'n_bits_in', sprintf('%d',n_bits_xeng_out), ...
-    'demux_factor', sprintf('%d',demux_factor), ...
-    'n_bits_out', sprintf('%d',n_bits_scaled_out), ...
-    'fix_pnt_pos', sprintf('%d',(n_bits-1)*2));
-
-reuse_block(blk, 'xeng_conj_fix', 'casper_library_correlator/xeng_conj_fix', ...
-    'Position', [x + 350, 105, x + 450, 163], ...
-    'n_bits_in', sprintf('%d',(n_bits_scaled_out)), ...
-    'n_ants', sprintf('%d',n_ants), ...
-    'demux_factor', sprintf('%d',demux_factor));
-
-reuse_block(blk, 'delay', 'xbsIndex_r4/Delay','Position', [x + 430, 220, x + 470, 230],...
+reuse_block(blk, 'delay', 'xbsIndex_r4/Delay','Position', [x + 350, 220, x + 380, 230],...
             'reg_retiming','on',...
             'latency','1');
 
@@ -128,11 +115,8 @@ reuse_block(blk, 'Constant1', 'xbsIndex_r4/Constant', 'Position', [15,124,85,156
             'arith_type','Boolean',...
             'n_bits','1');
         
-reuse_block(blk, 'Logical', 'xbsIndex_r4/Logical', 'Position', [x + 50, 130, x + 80, 150],...
-                'latency','0',...
-                'logical_function','AND');
             
-reuse_block(blk, 'window_delay', 'casper_library_delays/window_delay',...
+reuse_block(blk, 'window_delay', 'casper_library/Delays/window_delay',...
             'Position', [135, 200, x-135+95, 230], ...
             'delay',num2str(xeng_delay));
         
@@ -143,10 +127,10 @@ reuse_block(blk, 'Term2', 'built-in/Terminator', 'Position', [x, 65, x + 20, 85]
 reuse_block(blk, 'Term3', 'built-in/Terminator', 'Position', [x, 130, x + 20, 150]);
      
 % Set output port positions
-reuse_block(blk, 'acc', 'built-in/outport','Position', [x + 500, 100, x + 530, 114]);
-reuse_block(blk, 'valid', 'built-in/outport','Position', [x + 500, 130, x + 530, 144]);
-reuse_block(blk, 'sync_out', 'built-in/outport','Position', [x + 500, 160, x + 530, 174]);
-reuse_block(blk, 'mcnt_out', 'built-in/outport','Position', [x + 500, 190, x + 530, 204]);
+reuse_block(blk, 'acc', 'built-in/outport','Position', [x + 400, 100, x + 430, 114]);
+reuse_block(blk, 'valid', 'built-in/outport','Position', [x + 400, 130, x + 430, 144]);
+reuse_block(blk, 'sync_out', 'built-in/outport','Position', [x + 400, 160, x + 430, 174]);
+reuse_block(blk, 'mcnt_out', 'built-in/outport','Position', [x + 400, 190, x + 430, 204]);
 
 % Set input port positions
 reuse_block(blk, 'sync_in', 'built-in/inport','Position', [55,178,85,192]);
@@ -158,31 +142,37 @@ reuse_block(blk, 'mcnt_in', 'built-in/inport','Position', [55,293,85,307]);
 last_baseline_tap = ['baseline_tap', num2str(floor(n_ants/2))];
 
 if (n_ants == 4)
-    reuse_block(blk, 'xeng_descramble_4ant', 'casper_library_correlator/xeng_descramble_4ant', ...
-        'Position', [x + 200, 105, x + 300, 163], ...
-        'n_bits_in', sprintf('%d',n_bits_scaled_out*8), ...
+    reuse_block(blk, 'xeng_descramble_4ant', 'casper_library/Correlator/xeng_descramble_4ant', ...
+        'Position', [x + 100, 105, x + 200, 163], ...
+        'n_bits', sprintf('%d',n_bits), ...
         'demux_factor', sprintf('%d',demux_factor),...
         'acc_len', sprintf('%d',acc_len));
-    add_line(blk, 'xeng_descramble_4ant/1', 'xeng_conj_fix/1', 'autorouting', 'on');
-    add_line(blk, 'xeng_descramble_4ant/2', 'xeng_conj_fix/2', 'autorouting', 'on');
-    add_line(blk, 'xeng_descramble_4ant/3', 'xeng_conj_fix/3', 'autorouting', 'on');
-    add_line(blk, 'xeng_descramble_4ant/3', 'sample_and_hold3/1', 'autorouting', 'on');
-    add_line(blk, 'x_cast/1', 'xeng_descramble_4ant/1', 'autorouting', 'on');
+    
+    add_line(blk, [last_baseline_tap, '/4'], 'xeng_descramble_4ant/1','autorouting', 'on');
     add_line(blk, [last_baseline_tap, '/5'], 'xeng_descramble_4ant/2', 'autorouting', 'on');
     add_line(blk, [last_baseline_tap, '/7'], 'xeng_descramble_4ant/3', 'autorouting', 'on');
     add_line(blk, 'window_delay/1', 'xeng_descramble_4ant/4', 'autorouting', 'on');
+    
+    add_line(blk, 'xeng_descramble_4ant/1', 'acc/1', 'autorouting', 'on');
+    add_line(blk, 'xeng_descramble_4ant/2', 'valid/1', 'autorouting', 'on');
+    add_line(blk, 'xeng_descramble_4ant/3', 'sync_out/1', 'autorouting', 'on');
+    add_line(blk, 'xeng_descramble_4ant/3', 'sample_and_hold3/1', 'autorouting', 'on');
+    
+
+    
 elseif (n_ants >=6)
-    reuse_block(blk, 'xeng_descramble', 'casper_library_correlator/xeng_descramble', ...
-        'Position', [x + 200, 105, x + 300, 163], ...
+    reuse_block(blk, 'xeng_descramble', 'casper_library/Correlator/xeng_descramble', ...
+        'Position', [x + 100, 105, x + 200, 163], ...
         'num_ants', sprintf('%d',n_ants), ...
-        'n_bits_in', sprintf('%d',n_bits_scaled_out*8), ...
+        'n_bits', sprintf('%d',n_bits), ...
         'demux_factor', sprintf('%d',demux_factor),...
         'acc_len', sprintf('%d',acc_len));
-    add_line(blk, 'xeng_descramble/1', 'xeng_conj_fix/1', 'autorouting', 'on');
-    add_line(blk, 'xeng_descramble/2', 'xeng_conj_fix/2', 'autorouting', 'on');
-    add_line(blk, 'xeng_descramble/3', 'xeng_conj_fix/3', 'autorouting', 'on');
+    add_line(blk, 'xeng_descramble/1', 'acc/1', 'autorouting', 'on');
+    add_line(blk, 'xeng_descramble/2', 'valid/1', 'autorouting', 'on');
+    add_line(blk, 'xeng_descramble/3', 'sync_out/1', 'autorouting', 'on');
     add_line(blk, 'xeng_descramble/3', 'sample_and_hold3/1', 'autorouting', 'on');
-    add_line(blk, 'x_cast/1', 'xeng_descramble/1', 'autorouting', 'on');
+    
+    add_line(blk, [last_baseline_tap, '/4'], 'xeng_descramble/1','autorouting', 'on');
     add_line(blk, [last_baseline_tap, '/5'], 'xeng_descramble/2', 'autorouting', 'on');
     add_line(blk, [last_baseline_tap, '/7'], 'xeng_descramble/3', 'autorouting', 'on');
     add_line(blk, 'window_delay/1', 'xeng_descramble/4', 'autorouting', 'on');
@@ -202,11 +192,8 @@ add_line(blk, 'Constant1/1', 'auto_tap/5', 'autorouting', 'on');
 add_line(blk, 'sync_in/1', 'auto_tap/6', 'autorouting', 'on');
 add_line(blk, 'window_valid/1', 'window_delay/1', 'autorouting', 'on');
 
-add_line(blk, 'xeng_conj_fix/3', 'sync_out/1', 'autorouting', 'on');
-add_line(blk, 'xeng_conj_fix/1', 'acc/1', 'autorouting', 'on');
-add_line(blk, 'xeng_conj_fix/2', 'valid/1', 'autorouting', 'on');
-
 add_line(blk, 'sync_in/1', 'sample_and_hold1/1', 'autorouting', 'on');
+
 add_line(blk, 'mcnt_in/1', 'sample_and_hold1/2', 'autorouting', 'on');
 add_line(blk, 'sample_and_hold1/1', 'sample_and_hold2/2', 'autorouting', 'on');
 add_line(blk, 'sample_and_hold2/1', 'sample_and_hold3/2', 'autorouting', 'on');
@@ -232,7 +219,6 @@ end
 add_line(blk, [last_baseline_tap, '/1'], 'auto_tap/3', 'autorouting', 'on');
 add_line(blk, [last_baseline_tap, '/2'], 'Term1/1', 'autorouting', 'on');
 add_line(blk, [last_baseline_tap, '/3'], 'Term2/1', 'autorouting', 'on');
-add_line(blk, [last_baseline_tap, '/4'], 'x_cast/1', 'autorouting', 'on');
 add_line(blk, [last_baseline_tap, '/6'], 'Term3/1', 'autorouting', 'on');
 add_line(blk, [last_baseline_tap, '/7'], 'sample_and_hold2/1', 'autorouting', 'on');
 

casper_library/xeng_conj_fix_init.m
@@ -1,230 +1,0 @@
-function xeng_conj_fix_init(blk, varargin)
-% Initialize and configure the windowed CASPER X Engine with configurable output demux.
-%
-%n_bits_in is size of a component of a complex number.
-
-
-
-% Declare any default values for arguments you might like.
-defaults = {'n_ants', 8, 'n_bits_in', 4, ...
-    'demux_factor', 4};
-if same_state(blk, 'defaults', defaults, varargin{:}), return, end
-check_mask_type(blk, 'xeng_conj_fix');
-munge_block(blk, varargin{:});
-
-%fprintf('starting variables processing\n');
-
-n_ants = get_var('n_ants', 'defaults', defaults, varargin{:});
-n_bits_in = get_var('n_bits_in', 'defaults', defaults, varargin{:});
-demux_factor = get_var('demux_factor', 'defaults', defaults, varargin{:});
-
-n_taps = floor(n_ants/2) + 1;
-n_elements = demux_factor*(nchoosek(n_ants, 2) + n_ants);
-elements_bits = ceil(log2(n_elements));
-pivot_point = demux_factor*(n_taps*n_ants*3/4);
-
-
-%fprintf('variables all done\n');
-
-% Begin redrawing
-%================
-
-delete_lines(blk);
-
-
-% Add Misc static blocks
-reuse_block(blk, 'acc', 'built-in/inport','Position', [15,303,45,317]);
-reuse_block(blk, 'valid', 'built-in/inport','Position', [15,100,45,114]);
-reuse_block(blk, 'sync', 'built-in/inport','Position', [15,150,45,164]);
-
-
-reuse_block(blk, 'pos_cnt', 'xbsIndex_r4/Counter', 'Position', [135,196,185,249],...
-		'cnt_type','Count Limited',...
-		'cnt_to',sprintf('%d',n_elements-1),...
-		'n_bits',sprintf('%d',elements_bits),...
-		'en','on',...
-		'rst','on');
-
-reuse_block(blk, 'pivot_point', 'xbsIndex_r4/Constant', 'Position', [135,261,185,279],...
-		'const',sprintf('%d',pivot_point),...
-		'bin_pt','0',...
-		'n_bits',sprintf('%d',elements_bits),...		
-		'arith_type','Unsigned');
-
-reuse_block(blk, 'pivot', 'xbsIndex_r4/Relational', 'Position', [235,227,290,283],...
-		'Latency','1',...
-		'Mode','a>=b');
-
-reuse_block(blk, 'sel_conj', 'xbsIndex_r4/Mux', 'Position', [590,242,615,308],...
-		'inputs','2',...
-		'Latency','1');
-
-
-reuse_block(blk, 'delay1', 'xbsIndex_r4/Delay', 'Position', [250,298,280,322],...
-		'reg_retiming','on',...
-		'Latency','1');
-reuse_block(blk, 'delay2', 'xbsIndex_r4/Delay', 'Position', [250,100,280,114],...
-		'reg_retiming','on',...
-		'Latency','1');
-reuse_block(blk, 'delay3', 'xbsIndex_r4/Delay', 'Position', [250,150,280,164],...
-		'reg_retiming','on',...
-		'Latency','1');
-reuse_block(blk, 'delay4', 'xbsIndex_r4/Delay', 'Position', [585,100,615,114],...
-		'reg_retiming','on',...
-		'Latency','1');
-reuse_block(blk, 'delay5', 'xbsIndex_r4/Delay', 'Position', [585,150,615,164],...
-		'reg_retiming','on',...
-		'Latency','1');
-
-		
-% Set output port positions
-reuse_block(blk, 'acc_out', 'built-in/outport','Position', [630,268,660,282]);
-reuse_block(blk, 'valid_out', 'built-in/outport','Position', [630,100,660,114]);
-reuse_block(blk, 'sync_out', 'built-in/outport','Position', [630,150,660,164]);
-
-
-% Add Static lines
-%=================
-add_line(blk, 'acc/1', 'delay1/1', 'autorouting', 'on');
-add_line(blk, 'valid/1', 'delay2/1', 'autorouting', 'on');
-add_line(blk, 'valid/1', 'pos_cnt/2', 'autorouting', 'on');
-add_line(blk, 'sync/1', 'delay3/1', 'autorouting', 'on');
-add_line(blk, 'sync/1', 'pos_cnt/1', 'autorouting', 'on');
-add_line(blk, 'delay2/1', 'delay4/1', 'autorouting', 'on');
-add_line(blk, 'delay3/1', 'delay5/1', 'autorouting', 'on');
-add_line(blk, 'delay4/1', 'valid_out/1', 'autorouting', 'on');
-add_line(blk, 'delay5/1', 'sync_out/1', 'autorouting', 'on');
-add_line(blk, 'pos_cnt/1', 'pivot/1', 'autorouting', 'on');
-add_line(blk, 'pivot_point/1', 'pivot/2', 'autorouting', 'on');
-add_line(blk, 'pivot/1', 'sel_conj/1', 'autorouting', 'on');
-add_line(blk, 'delay1/1', 'sel_conj/2', 'autorouting', 'on');
-add_line(blk, 'sel_conj/1', 'acc_out/1', 'autorouting', 'on');
-
-
-%REDRAW DYNAMIC PARTS
-switch demux_factor
-case {1,2},
-	n_unpacks = 2^(2-log2(demux_factor));
-	%fprintf('n_unpacks=%d\n',n_unpacks);
-	
-	reuse_block(blk, 'remux', 'xbsIndex_r4/Concat',...
-	'num_inputs', sprintf('%d',n_unpacks),...
-       	'Position', [550,250+70*n_unpacks/2,590,280+70*n_unpacks/2]);
-	
-	add_line(blk, 'remux/1', 'sel_conj/3', 'autorouting', 'on');
-
-    for n_unpack=1:n_unpacks,
-        %fprintf('n_unpack=%d\n',n_unpack);
-        reuse_block(blk, sprintf('slice%d',n_unpack), 'xbsIndex_r4/Slice', ...
-	'Position', [310,n_unpack*70+234,330,n_unpack*70+246],...
-	'mode','Lower Bit Location + Width',...
-	'base0','LSB of Input',...
-	'bit0',sprintf('%d',2*n_bits_in*(n_unpacks-n_unpack)),...
-	'nbits',sprintf('%d',n_bits_in*2));
-	
-	reuse_block(blk, sprintf('unpack%d',n_unpack), 'casper_library_misc/c_to_ri',...
-       	'Position', [360,219+70*n_unpack,400,261+70*n_unpack],...
-	'bin_pt','0',...
-	'n_bits',sprintf('%d',n_bits_in));
-	
-	reuse_block(blk, sprintf('negate%d',n_unpack), 'xbsIndex_r4/Negate',...
-       	'Position', [425,236+70*n_unpack,455,264+70*n_unpack],...
-	'precision','User Defined',...
-	'arith_type','Signed  (2''s comp)',...
-	'n_bits',sprintf('%d',n_bits_in),...		
-	'bin_pt','0',...
-	'Latency','0');
-
-	reuse_block(blk, sprintf('repack%d',n_unpack), 'casper_library_misc/ri_to_c',...
-       	'Position', [480,219+70*n_unpack,520,261+70*n_unpack]);
-
-
-	add_line(blk, 'delay1/1', sprintf('slice%d/1',n_unpack),'autorouting', 'on');  
-	add_line(blk, sprintf('slice%d/1',n_unpack), sprintf('unpack%d/1',n_unpack), 'autorouting', 'on');
-	add_line(blk, sprintf('unpack%d/1',n_unpack), sprintf('repack%d/1',n_unpack), 'autorouting', 'on');
-	add_line(blk, sprintf('unpack%d/2',n_unpack), sprintf('negate%d/1',n_unpack), 'autorouting', 'on');
-	add_line(blk, sprintf('negate%d/1',n_unpack), sprintf('repack%d/2',n_unpack), 'autorouting', 'on');
-	add_line(blk, sprintf('repack%d/1',n_unpack), sprintf('remux/%d',n_unpack), 'autorouting', 'on');
-	end
-
-
-case 4,
-	reuse_block(blk, 'negate', 'xbsIndex_r4/Negate', 'Position', [425,306,455,334],...
-	'precision','User Defined',...
-	'arith_type','Signed  (2''s comp)',...
-	'n_bits',sprintf('%d',n_bits_in),...		
-	'bin_pt','0',...
-	'Latency','0');
-
-	reuse_block(blk, 'unpack', 'casper_library_misc/c_to_ri', 'Position', [360,289,400,331],...
-	'bin_pt','0',...
-	'n_bits',sprintf('%d',n_bits_in));
-	
-	reuse_block(blk, 'repack', 'casper_library_misc/ri_to_c', 'Position', [480,289,520,331]);
-	
-	add_line(blk, 'unpack/1', 'repack/1', 'autorouting', 'on');
-	add_line(blk, 'unpack/2', 'negate/1', 'autorouting', 'on');
-	add_line(blk, 'negate/1', 'repack/2', 'autorouting', 'on');
-	add_line(blk, 'repack/1', 'sel_conj/3', 'autorouting', 'on');
-	add_line(blk, 'delay1/1', 'unpack/1', 'autorouting', 'on');
-
-
-
-case 8,
-	reuse_block(blk, 'negate', 'xbsIndex_r4/Negate', 'Position', [390,348,415,373],...
-	'precision','User Defined',...
-	'arith_type','Signed  (2''s comp)',...
-	'n_bits',sprintf('%d',n_bits_in),...		
-	'bin_pt','0',...
-	'Latency','0');
-
-	reuse_block(blk, 'reint_in', 'xbsIndex_r4/Reinterpret', 'Position', [335,351,365,369],...
-	'force_arith_type','on',...
-	'arith_type','Signed  (2''s comp)',...
-	'force_bin_pt','on',...
-	'bin_pt','0');
-
-	reuse_block(blk, 'reint_out', 'xbsIndex_r4/Reinterpret', 'Position', [440,351,460,369],...
-	'force_arith_type','on',...
-	'arith_type','Unsigned',...
-	'force_bin_pt','on',...
-	'bin_pt','0');
-
-	reuse_block(blk, 'slice', 'xbsIndex_r4/Slice', ...
-	'Position', [370,299,395,311],...
-	'mode','Lower Bit Location + Width',...
-	'base0','LSB of Input',...
-	'bit0','0',...
-	'nbits','1');
-
-    reuse_block(blk, 'delay6', 'xbsIndex_r4/Delay', 'Position', [250,283,280,297],...
-		'reg_retiming','on',...
-		'Latency','1');
-	
-	reuse_block(blk, 'imag_sel', 'xbsIndex_r4/Mux', 'Position', [510,292,535,358],...
-		'inputs','2',...
-		'Latency','0');
-
-	add_line(blk, 'pos_cnt/1', 'delay6/1', 'autorouting', 'on');
-    add_line(blk, 'delay6/1', 'slice/1', 'autorouting', 'on');
-	add_line(blk, 'delay1/1', 'reint_in/1', 'autorouting', 'on');
-	add_line(blk, 'reint_in/1', 'negate/1','autorouting', 'on');
-	add_line(blk, 'slice/1', 'imag_sel/1', 'autorouting', 'on');
-	add_line(blk, 'delay1/1', 'imag_sel/2', 'autorouting', 'on');
-	add_line(blk, 'negate/1', 'reint_out/1', 'autorouting', 'on');
-	add_line(blk, 'reint_out/1', 'imag_sel/3', 'autorouting', 'on');
-	add_line(blk, 'imag_sel/1', 'sel_conj/3', 'autorouting', 'on');
-
-
-otherwise,
-	errordlg('Demux factor for xeng_conj_fix must be either 1, 2, 4 or 8.');
-	error('Demux factor for xeng_conj_fix must be either 1, 2, 4 or 8.');	
-end
-
-clean_blocks(blk);
-
-fmtstr = sprintf('num_ants=%d',n_ants);
-set_param(gcb, 'AttributesFormatString', fmtstr);
-
-save_state(blk, 'defaults', defaults, varargin{:});
-

commit 966eeed75495a5802156c4926ce3bae5bf6d19fe

Run update_librefs.sh on a couple files
Before the casper_library model file was split out into sub-libraries, links to
casper_library blocks began "casper_library/...".  After the split-up, links to
casper_library blocks begin "casper_library_<sublib>/...".  The
update_librefs.sh script can be used to help migrate from the "old-style" links
to the "new-style" links.

Commit 9a373e8efaf14fcb2c460b06bc2bfe03c486979a inadvertently reintroduced the
"old-stlye" links to casper_library_correlator.mdl and xeng_init.m.

The update_librefs script was used to update the library references in these
two files.

casper_library/xeng_init.m
@@ -67,7 +67,7 @@ delete_lines(blk);
 % Add taps
 x = 275;
 
-reuse_block(blk, 'auto_tap', 'casper_library/Correlator/auto_tap',...
+reuse_block(blk, 'auto_tap', 'casper_library_correlator/auto_tap',...
                 'Position',[135, 51, 230, 169], ...
                 'n_ants',num2str(n_ants), ...                            
                 'n_bits',num2str(n_bits), ...
@@ -79,7 +79,7 @@ reuse_block(blk, 'auto_tap', 'casper_library/Correlator/auto_tap',...
             
 for i=1:floor(n_ants/2),
         name = ['baseline_tap', num2str(i)];
-        reuse_block(blk, name, 'casper_library/Correlator/baseline_tap', ...
+        reuse_block(blk, name, 'casper_library_correlator/baseline_tap', ...
             'n_ants',num2str(n_ants), ...            
             'n_bits',num2str(n_bits), ...
             'acc_len',num2str(acc_len), ...
@@ -97,13 +97,13 @@ reuse_block(blk, 'Constant', 'xbsIndex_r4/Constant', 'Position', [15,89,85,121],
             'arith_type','Unsigned',...
             'n_bits',sprintf('%d',8 * n_bits_xeng_out));
         
-reuse_block(blk, 'sample_and_hold1', 'casper_library/Misc/sample_and_hold', 'Position', [140,255,180,315],...
+reuse_block(blk, 'sample_and_hold1', 'casper_library_misc/sample_and_hold', 'Position', [140,255,180,315],...
             'period',sprintf('%d',n_ants * acc_len));
 
-reuse_block(blk, 'sample_and_hold2', 'casper_library/Misc/sample_and_hold', 'Position', [x + 100,190,x+140,250],...
+reuse_block(blk, 'sample_and_hold2', 'casper_library_misc/sample_and_hold', 'Position', [x + 100,190,x+140,250],...
             'period',sprintf('%d',n_ants * acc_len));
 
-reuse_block(blk, 'sample_and_hold3', 'casper_library/Misc/sample_and_hold', 'Position', [x + 250,190,x+290,250],...
+reuse_block(blk, 'sample_and_hold3', 'casper_library_misc/sample_and_hold', 'Position', [x + 250,190,x+290,250],...
             'period',sprintf('%d',n_ants * acc_len));
 
 reuse_block(blk, 'delay', 'xbsIndex_r4/Delay','Position', [x + 350, 220, x + 380, 230],...
@@ -116,7 +116,7 @@ reuse_block(blk, 'Constant1', 'xbsIndex_r4/Constant', 'Position', [15,124,85,156
             'n_bits','1');
         
             
-reuse_block(blk, 'window_delay', 'casper_library/Delays/window_delay',...
+reuse_block(blk, 'window_delay', 'casper_library_delays/window_delay',...
             'Position', [135, 200, x-135+95, 230], ...
             'delay',num2str(xeng_delay));
         
@@ -142,7 +142,7 @@ reuse_block(blk, 'mcnt_in', 'built-in/inport','Position', [55,293,85,307]);
 last_baseline_tap = ['baseline_tap', num2str(floor(n_ants/2))];
 
 if (n_ants == 4)
-    reuse_block(blk, 'xeng_descramble_4ant', 'casper_library/Correlator/xeng_descramble_4ant', ...
+    reuse_block(blk, 'xeng_descramble_4ant', 'casper_library_correlator/xeng_descramble_4ant', ...
         'Position', [x + 100, 105, x + 200, 163], ...
         'n_bits', sprintf('%d',n_bits), ...
         'demux_factor', sprintf('%d',demux_factor),...
@@ -161,7 +161,7 @@ if (n_ants == 4)
 
     
 elseif (n_ants >=6)
-    reuse_block(blk, 'xeng_descramble', 'casper_library/Correlator/xeng_descramble', ...
+    reuse_block(blk, 'xeng_descramble', 'casper_library_correlator/xeng_descramble', ...
         'Position', [x + 100, 105, x + 200, 163], ...
         'num_ants', sprintf('%d',n_ants), ...
         'n_bits', sprintf('%d',n_bits), ...