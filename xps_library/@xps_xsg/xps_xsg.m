4c82828757c2dc7442c64aa5fc12b7fc9509122a
System User <wesley@katfs.kat.ac.za>
2011-03-31 11:08:29 +0200

    Fixing repo

--- xps_library/@xps_xsg/xps_xsg.m
+++ xps_library/@xps_xsg/xps_xsg.m
@@ -1,4 +1,4 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
 %   Center for Astronomy Signal Processing and Electronics Research           %
 %   http://seti.ssl.berkeley.edu/casper/                                      %
@@ -66,11 +66,7 @@ switch s.hw_sys
 	    s.sw_os = 'none';
     % end case 'ROACH'
 
-	case 'ROACH2'
-	    s.sw_os = 'none';
-    % end case 'ROACH2'
-
-  otherwise
+    otherwise
   		error(['Unsupported Platform: ',s.hw_sys]);
 end % switch s.hw_sys
 
