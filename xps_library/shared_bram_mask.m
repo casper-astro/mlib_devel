774808ef9e7dedf745af8f46267372089ca9abe4
Andrew Martens <martens.andrew@gmail.com>
2011-04-27 16:36:29 +0200

    Shared BRAM flexibility upgrade
    The toolflow has supported a range of data port sizes for the ROACH besides
    the default 32 for a while now, as well as address ranges. The mask on the
    Shared BRAM has been changed to reflect these additions. The ROACH now
    supports data port sizes of 8,16,32,64,128 and an address range down to
    2 bits.
    
    If the data port is smaller than 32, the word/s occupying the least significant
    (of 32 from the PC) bits appear at lower addresses (in the design)  e.g for a
    16 bit design  data port, bits 0-15 from the PC appear as address n in the
     design, bits 16-31 appear as address n+1 from the PC etc. Similarly, for
    design data ports that are larger than 32, the least significant 32 bits in the
    design correspond to the lowest address from the PC, the next 32 as the lowest
    address+1 etc.

--- xps_library/shared_bram_mask.m
+++ xps_library/shared_bram_mask.m
@@ -4,13 +4,16 @@
 %   http://seti.ssl.berkeley.edu/casper/                                      %
 %   Copyright (C) 2006 University of California, Berkeley                     %
 %                                                                             %
+%   Meerkat Telescope Project                                                 %
+%   http://www.kat.ac.za                                                      %
+%   Copyright (C) 2011                                                        %
+%                                                                             %
 %   This program is free software; you can redistribute it and/or modify      %
 %   it under the terms of the GNU General Public License as published by      %
 %   the Free Software Foundation; either version 2 of the License, or         %
 %   (at your option) any later version.                                       %
 %                                                                             %
-%   This program is distributed in the hope that it will be useful,
-%   %
+%   This program is distributed in the hope that it will be useful,           %
 %   but WITHOUT ANY WARRANTY; without even the implied warranty of            %
 %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             %
 %   GNU General Public License for more details.                              %
@@ -24,43 +27,48 @@
 c_sys = gcb;
 
 gcb_arith_type = get_param(c_sys, 'arith_type');
-gateway_ins =find_system(c_sys,'searchdepth',1,'FollowLinks', 'on', 'lookundermasks','all','masktype','Xilinx Gateway In Block');
-set_param(gateway_ins{1},'arith_type',gcb_arith_type,'Name',clear_name([c_sys,'_data_out']));
-gateway_outs =find_system(c_sys,'searchdepth',1,'FollowLinks', 'on','lookundermasks','all','masktype','Xilinx Gateway Out Block');
+gateway_ins = find_system(c_sys, 'searchdepth', 1, 'FollowLinks', 'on', ...
+  'lookundermasks', 'all', 'masktype', 'Xilinx Gateway In Block');
+set_param(gateway_ins{1}, 'arith_type', gcb_arith_type, 'Name', clear_name([c_sys,'_data_out']));
+gateway_outs =find_system(c_sys, 'searchdepth', 1, 'FollowLinks', 'on', ...
+  'lookundermasks', 'all', 'masktype', 'Xilinx Gateway Out Block');
 for i =1:length(gateway_outs)
-    gw = gateway_outs{i};
-    if regexp(get_param(gw,'Name'),'_addr$')
-        set_param(gw,'Name',clear_name([c_sys,'_addr']));
-    elseif regexp(get_param(gw,'Name'),'_data_in$')
-        set_param(gw,'Name',clear_name([c_sys,'_data_in']));
-    elseif regexp(get_param(gw,'Name'),'_we$')
-        set_param(gw,'Name',clear_name([c_sys,'_we']));
-    else
-        errordlg(['Unknown gateway: ',get_param(gw,'Parent'),'/',get_param(gw,'Name')]);
-    end
+  gw = gateway_outs{i};
+  if regexp(get_param(gw, 'Name'), '_addr$')
+    set_param(gw, 'Name', clear_name([c_sys, '_addr']));
+  elseif regexp(get_param(gw, 'Name'), '_data_in$')
+    set_param(gw, 'Name', clear_name([c_sys, '_data_in']));
+  elseif regexp(get_param(gw, 'Name'), '_we$')
+    set_param(gw, 'Name', clear_name([c_sys, '_we']));
+  else
+    errordlg(['Unknown gateway: ', get_param(gw, 'Parent'), '/', get_param(gw, 'Name')]);
+  end
 end
 
-set_param([c_sys,'/convert_din'],'arith_type',gcb_arith_type);
-set_param([c_sys,'/mem/sim_data_in'],'arith_type',gcb_arith_type);
+set_param([c_sys, '/convert_din'], 'arith_type', gcb_arith_type);
+set_param([c_sys, '/mem/sim_data_in'], 'arith_type', gcb_arith_type);
 
-xsg_blk = [strtok(gcs,'/') '/ System Generator'];
-fpga_arch = xlgetparam(xsg_blk,'xilinxfamily');
+xsg_blk = [strtok(gcs, '/') '/ System Generator'];
+fpga_arch = xlgetparam(xsg_blk, 'xilinxfamily');
+addr_width = eval(get_param(c_sys, 'addr_width'));
+data_width = eval(get_param(c_sys, 'data_width'));
 
 switch fpga_arch
   case {'virtex5', 'Virtex5'}
-    if addr_width < 10
-      errordlg('Shared BRAM address width cannot be less than 10 on Virtex-5 boards');
+    if addr_width < 2 
+      errordlg('Shared BRAM address width cannot be less than 2 on Virtex-5 boards');
     end
   case 'virtex2p'
-    if addr_width < 11
+    if addr_width < 11 
       errordlg('Shared BRAM address width cannot be less than 11 on Virtex-II Pro boards');
     end
   otherwise
-    if addr_width < 11
+    if addr_width < 11 
       errordlg('Shared BRAM address width cannot be less than 11 on unknown board');
     end
 end
 
-if addr_width > 16
-    errordlg('Shared BRAM address width cannot be greater than 16');
+if addr_width > 16,
+  errordlg('Shared BRAM address width cannot be greater than 16');
 end
+
