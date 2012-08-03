41606b1fc0a00ad9d41162e5f382a99f3f3033e5
Andrew Martens <andrew@ska.ac.za>
2012-08-03 12:10:56 +0200

    quare_transposer input number bug fix
    the init script for the square_transposer had an incomplete name
    for the barrel_switcher block causing a script failure as reuse_block
    could not find the specified block in the libraries.
    
    Added default value so that script can be run from the command line.
    Added logic to generate an empty block when input number = 1 for storage
    in the libraries.
    Replaced i and js with p and qs as Matlab treats i and js as 'special'

--- casper_library/square_transposer_init.m
+++ casper_library/square_transposer_init.m
@@ -33,25 +33,33 @@ function square_transposer_init(blk, varargin)
 
 % Declare any default values for arguments you might like.
 
-defaults = {};
+defaults = { ...
+  'n_inputs', 3};
 if same_state(blk, 'defaults', defaults, varargin{:}), return, end
 check_mask_type(blk, 'square_transposer');
 munge_block(blk, varargin{:});
 
 n_inputs = get_var('n_inputs', 'defaults', defaults, varargin{:});
 
-if n_inputs < 1,
-    error('Number of inputs must be 2^1 or greater.');
+if n_inputs < 0,
+    error('Number of inputs must be 2^0 or greater.');
 end
 
-
 delete_lines(blk);
+
+if n_inputs == 0,
+  clean_blocks(blk);
+  set_param(blk, 'AttributesFormatString', '');
+  save_state(blk, 'defaults', defaults, varargin{:});
+  return;
+end
+
 % Add ports
 reuse_block(blk, 'sync', 'built-in/inport', 'Position', [15 10 45 24], 'Port', '1');
 reuse_block(blk, 'sync_out', 'built-in/outport', 'Position', [315 10 345 24], 'Port', '1');
-for i=1:2^n_inputs,
-    reuse_block(blk, ['In',num2str(i)], 'built-in/inport', 'Position', [15 i*80+95 45 109+80*i]);
-    reuse_block(blk, ['Out',num2str(i)], 'built-in/outport', 'Position', [315 95+i*80 345 109+80*i]);
+for p=1:2^n_inputs,
+    reuse_block(blk, ['In',num2str(p)], 'built-in/inport', 'Position', [15 p*80+95 45 109+80*p]);
+    reuse_block(blk, ['Out',num2str(p)], 'built-in/outport', 'Position', [315 95+p*80 345 109+80*p]);
 end
 
 % Add blocks
@@ -60,15 +68,14 @@ reuse_block(blk, 'counter', 'xbsIndex_r4/Counter', ...
     'arith_type', 'Unsigned', 'rst', 'on', 'operation', 'Down');
 reuse_block(blk, 'delay0', 'xbsIndex_r4/Delay', ...
     'latency', num2str(2^n_inputs - 1), 'Position', [270 0 300 30]);
-reuse_block(blk, 'barrel_switcher', 'barrel_switcher', ...
+reuse_block(blk, 'barrel_switcher', 'casper_library_reorder/barrel_switcher', ...
     'n_inputs', num2str(n_inputs), 'Position', [120 150 240 300]);
-for j=1:2^n_inputs,
-    reuse_block(blk, ['Delayf', num2str(j)], 'xbsIndex_r4/Delay', ...
-        'latency', num2str(j-1), 'Position', [60 j*80+95 90 j*80+125]);
-    reuse_block(blk, ['Delayb', num2str(j)], 'xbsIndex_r4/Delay', ...
-        'latency', num2str(2^n_inputs-j), 'Position', [270 j*80+95 300 j*80+125]);
+for q=1:2^n_inputs,
+    reuse_block(blk, ['Delayf', num2str(q)], 'xbsIndex_r4/Delay', ...
+        'latency', num2str(q-1), 'Position', [60 q*80+95 90 q*80+125]);
+    reuse_block(blk, ['Delayb', num2str(q)], 'xbsIndex_r4/Delay', ...
+        'latency', num2str(2^n_inputs-q), 'Position', [270 q*80+95 300 q*80+125]);
 end
-
 
 % Add lines
 add_line(blk, 'sync/1', 'counter/1');
@@ -77,16 +84,16 @@ add_line(blk, 'counter/1', 'barrel_switcher/1');
 add_line(blk, 'barrel_switcher/1', 'delay0/1');
 add_line(blk, 'delay0/1', 'sync_out/1');
 
-for j=1:2^n_inputs,
-    if j == 1,
+for q=1:2^n_inputs,
+    if q == 1,
         dport = 3;
     else,
-        dport = (2^n_inputs - j + 2) + 2;
+        dport = (2^n_inputs - q + 2) + 2;
     end
-    add_line(blk, ['In', num2str(j),'/1'], ['Delayf', num2str(j),'/1']);
-    add_line(blk, ['Delayf', num2str(j), '/1'], ['barrel_switcher/', num2str(dport)]);
-    add_line(blk, ['barrel_switcher/',num2str(j+1)], ['Delayb',num2str(j),'/1']);
-    add_line(blk, ['Delayb',num2str(j),'/1'], ['Out',num2str(j),'/1']);
+    add_line(blk, ['In', num2str(q),'/1'], ['Delayf', num2str(q),'/1']);
+    add_line(blk, ['Delayf', num2str(q), '/1'], ['barrel_switcher/', num2str(dport)]);
+    add_line(blk, ['barrel_switcher/',num2str(q+1)], ['Delayb',num2str(q),'/1']);
+    add_line(blk, ['Delayb',num2str(q),'/1'], ['Out',num2str(q),'/1']);
 end
 
 clean_blocks(blk);
