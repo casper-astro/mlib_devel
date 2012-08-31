51d1f391082b6c1988e9d008d40df485730a8c38
wnew <wesley@ska.ac.za>
2012-08-31 12:41:55 +0200

    Removing iBOB, BEE2 and CORR related code
    Removed the following blocks:
    XAUI - Only supported BEE2 and iBOB
    CORR_mxfe
    CORR_fr
    CORR_adc
    CORR_dac
    iBOB_lwip
    frame_buffer
    vsi
    dac_iBOB
    sram
    
    Edited the following blocks:
    XSG core config - Removed iBOB, BEE2 and CORR options
    GPIOs - Removed iBOB, BEE2 and CORR options
    DRAM - removed support for multiple DIMMs
    (cherry picked from commit 9b125fd989cf1f798dd68d1b277093866087899d)

--- xps_library/slblocks.m
+++ xps_library/slblocks.m
@@ -4,17 +4,17 @@ function blkStruct = slblocks
 
 % Copyright (c) 1998 Xilinx Inc. All Rights Reserved.
 
-blkStruct.Name    = ['BEE_XPS System Blockset'];
+blkStruct.Name    = ['CASPER XPS Blockset'];
 blkStruct.OpenFcn = '';
 blkStruct.MaskInitialization = '';
 
-blkStruct.MaskDisplay = ['disp(''BEE_XPS System Blockset'')'];
+blkStruct.MaskDisplay = ['disp(''CASPER XPS Blockset'')'];
 
 % Define the library list for the Simulink Library browser.
 % Return the name of the library model and the name for it
 %
 Browser(1).Library = 'xps_library';
-Browser(1).Name    = 'BEE_XPS System Blockset';
+Browser(1).Name    = 'CASPER XPS Blockset';
 
 blkStruct.Browser = Browser;
 
