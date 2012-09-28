387db6930acd91bc37a30d4d4a339de99711f1c5
Jason Manley <jason.manley@ska.ac.za>
2012-09-28 08:39:29 +0200

    Revisions to startup scripts.
    Default install locations.
    Fewer environment variables.
    Fix to new Xilinx paths.

--- startup.m
+++ startup.m
@@ -1,12 +1,10 @@
-xilinx_path='/data/Xilinx';
-mlib_devel='/data/casper/mlib_devel';
 warning off Simulink:SL_SaveWithParameterizedLinks_Warning
 warning off Simulink:SL_LoadMdlParameterizedLink
-addpath([xilinx_path, '/14.2/ISE_DS/ISE/sysgen/util/']);
-addpath([xilinx_path, '/14.2/ISE_DS/ISE/sysgen/bin/lin64']);
-addpath([mlib_devel, '/casper_library']);
-addpath([mlib_devel, '/xps_library']);
-xlAddSysgen(getenv('XILINX_SYS_GEN'))
+addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
+addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
+addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
+addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
+xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
 sysgen_startup
 load_system('casper_library');
 load_system('xps_library');
