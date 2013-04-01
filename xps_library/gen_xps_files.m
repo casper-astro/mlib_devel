casper-astro/mlib_devel

commit 23429dfa5ba915376867d8b48c2a95314a5b1902

Only make UNIX BOF file executable for ROACH
ROACH2 does not execute a BOF file like ROACH1 does, so do not make ROACH2 BOF
files executable.  Also cleaned up some comments and added a missing "end".

xps_library/gen_xps_files.m
@@ -601,12 +601,14 @@ if run_software
       end
       fprintf(win_fid,['copy implementation\\system.bof', ' ..\\bit_files\\', design_name,'_', time_stamp,'.bof\n']);
       end
-      fprintf(unix_fid,['chmod +x implementation/system.bof\n']);
+      if strcmp(hw_sys, 'ROACH')
+         fprintf(unix_fid,['chmod +x implementation/system.bof\n']);
+      end % if ROACH
       fprintf(unix_fid,['cp implementation/system.bof ../bit_files/', design_name,'_',time_stamp,'.bof\n']);
       if strcmp(hw_sys, 'ROACH2')
-	 fprintf(unix_fid,['gzip -c ../bit_files/', design_name,'_',time_stamp,'.bof  > ../bit_files/', design_name,'_',time_stamp,'.bof.gz\n']);
-         fprintf(unix_fid,['chmod +x ../bit_files/', design_name,'_',time_stamp,'.bof.gz\n']);
-    end % strcmp(hw_sys, 'ROACH')
+         fprintf(unix_fid,['gzip -c ../bit_files/', design_name,'_',time_stamp,'.bof  > ../bit_files/', design_name,'_',time_stamp,'.bof.gz\n']);
+      end % if ROACH2
+    end % if ROACH or ROACH2
 
     fclose(win_fid);
     fclose(unix_fid);

commit a56538709e0cee67f9d722554be0cda81a855abd

Fix syntax error in gen_xps_files.m
This syntax error was introduced in 23429df.

xps_library/gen_xps_files.m
@@ -600,7 +600,6 @@ if run_software
          fprintf(unix_fid, ['./mkbof -o implementation/system.bof', ' -s core_info.tab -t 3 implementation/system.bin\n']);
       end
       fprintf(win_fid,['copy implementation\\system.bof', ' ..\\bit_files\\', design_name,'_', time_stamp,'.bof\n']);
-      end
       if strcmp(hw_sys, 'ROACH')
          fprintf(unix_fid,['chmod +x implementation/system.bof\n']);
       end % if ROACH