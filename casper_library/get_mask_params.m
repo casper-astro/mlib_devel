1eabff34f1ac6f95d3bd8acc2076621627326579
David MacMahon <davidm@astro.berkeley.edu>
2013-05-17 13:07:23 -0700

    Add casper_library/get_mask_params.m
    The get_mask_params function can be used to get a cell array of mask parameters
    that is suitable for passing to a typical CASPER mask initialization script.
    
    Typical usage:
    
        params = get_mask_params(gcb);
        my_mask_init(gcb, params{:});

---
+++ casper_library/get_mask_params.m
@@ -0,0 +1,34 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%   Center for Astronomy Signal Processing and Electronics Research           %
+%   http://seti.ssl.berkeley.edu/casper/                                      %
+%   Copyright (C) 2013 David MacMahon
+%                                                                             %
+%   This program is free software; you can redistribute it and/or modify      %
+%   it under the terms of the GNU General Public License as published by      %
+%   the Free Software Foundation; either version 2 of the License, or         %
+%   (at your option) any later version.                                       %
+%                                                                             %
+%   This program is distributed in the hope that it will be useful,           %
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of            %
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             %
+%   GNU General Public License for more details.                              %
+%                                                                             %
+%   You should have received a copy of the GNU General Public License along   %
+%   with this program; if not, write to the Free Software Foundation, Inc.,   %
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Function for building cell array to pass into mask init scripts.
+%
+% Typical usage:
+%
+%    params = get_mask_params(gcb);
+%    my_mask_init(gcb, params{:});
+
+function params = get_mask_params(blk)
+  mask_names  = get_param(blk, 'MaskNames');
+  mask_values = get_param(blk, 'MaskValues');
+  params = [mask_names, mask_values]';
+end
