40debbacd153d1c25dac1cfab905abb46edde3a2
Andrew Martens <andrew@ska.ac.za>
2013-05-27 21:11:51 +0200

    Twiddle modification for multiple inputs and async
    * twiddle blocks fully scripted
    * parameter option to process more than one stream (number inputs)
     (still need to make bus_negate to complete twiddle_stage_2 and twiddle_coeff_1)
    * output a and b (instead of a_re, a_im, b_re, b_im) as will use bus_addsub
      blocks that can take complex inputs).
    
    Note that the fft will not work yet, butterfly_direct needs to be modded to
    use these new twiddles in future commmits. This should actually be on a branch.

--- casper_library/twiddle_general_init.m
+++ casper_library/twiddle_general_init.m
@@ -28,10 +28,10 @@
 function twiddle_general_init(blk, varargin)
 clog('entering twiddle_general_init','trace');
 
-defaults = {'n_inputs', 1, 'FFTSize', 4, 'async', 'on', 'Coeffs', [0 1 2 3], 'StepPeriod', 0, ...
+defaults = {'n_inputs', 1, 'FFTSize', 4, 'async', 'off', 'Coeffs', [0 1 2 3], 'StepPeriod', 0, ...
     'input_bit_width', 18, 'coeff_bit_width', 18, ...
     'add_latency', 1, 'mult_latency', 2, 'conv_latency', 1, 'bram_latency', 2, ...
-    'arch', 'Virtex5', 'coeffs_bram', 'off', 'use_hdl', 'off', 'use_embedded', 'off', ...
+    'coeffs_bram', 'off', 'use_hdl', 'off', 'use_embedded', 'off', ...
     'quantization', 'Round  (unbiased: +/- Inf)', 'overflow', 'Wrap'};
 
 if same_state(blk, 'defaults', defaults, varargin{:}), return, end
@@ -49,21 +49,12 @@ add_latency     = get_var('add_latency', 'defaults', defaults, varargin{:});
 mult_latency    = get_var('mult_latency', 'defaults', defaults, varargin{:});
 conv_latency    = get_var('conv_latency', 'defaults', defaults, varargin{:});
 bram_latency    = get_var('bram_latency', 'defaults', defaults, varargin{:});
-arch            = get_var('arch', 'defaults', defaults, varargin{:});
 coeffs_bram     = get_var('coeffs_bram', 'defaults', defaults, varargin{:});
 use_hdl         = get_var('use_hdl', 'defaults', defaults, varargin{:});
 use_embedded    = get_var('use_embedded', 'defaults', defaults, varargin{:});
 quantization    = get_var('quantization', 'defaults', defaults, varargin{:});
 overflow        = get_var('overflow', 'defaults', defaults, varargin{:});
 async           = get_var('async', 'defaults', defaults, varargin{:});
-
-if( strcmp(arch,'Virtex2Pro') ),
-elseif( strcmp(arch,'Virtex5') ),
-else,
-    clog(['twiddle_general_init: unknown target architecture ',arch],'error');
-    error(['twiddle_general_init: Unknown target architecture ',arch]);
-    return
-end
 
 delete_lines(blk);
 
@@ -76,70 +67,34 @@ if n_inputs == 0 || FFTSize == 0,
   return;
 end
 
-%sync input signal path
+%sync signal path
 reuse_block(blk, 'sync_in', 'built-in/Inport', 'Port', '3', 'Position', [10 108 40 122]);
-reuse_block(blk, 'sync_out', 'built-in/Outport', 'Port', '5', 'Position', [750 228 780 242]);
+reuse_block(blk, 'sync_out', 'built-in/Outport', 'Port', '3', 'Position', [750 233 780 247]);
 
-%a input signal path
-reuse_block(blk, 'a', 'built-in/Inport', 'Port', '1', 'Position', [10 273 40 287]);
+%a signal path
+reuse_block(blk, 'ai', 'built-in/Inport', 'Port', '1', 'Position', [10 273 40 287]);
+reuse_block(blk, 'ao', 'built-in/Outport', 'Port', '1', 'Position', [750 283 780 297]);
 
-%reorder real and imaginary parts so we can do a simple slice on output 
-reuse_block(blk, 'munge_a', 'casper_library_flow_control/munge', ...
-        'divisions', 'n_inputs*2', 'div_size', 'input_bit_width', ...
-        'order', '[[0:2:(n_inputs*2)-2],[1:2:(n_inputs*2)-1]]', ...
-        'Position', [70 267 115 293]);
-add_line(blk,'a/1','munge_a/1');
-
-reuse_block(blk, 'a_re', 'built-in/Outport', 'Port', '1', 'Position', [750 263 780 277]);
-reuse_block(blk, 'a_im', 'built-in/Outport', 'Port', '2', 'Position', [750 298 780 312]);
-
-%b input signal path
-reuse_block(blk, 'b', 'built-in/Inport', 'Port', '2', 'Position', [10 213 40 227]);
-
-reuse_block(blk, 'munge_b', 'casper_library_flow_control/munge', ...
-        'divisions', 'n_inputs*2', 'div_size', '(input_bit_width+1)', ...
-        'order', '[[0:2:(n_inputs*2)-2],[1:2:(n_inputs*2)-1]]', ...
-        'Position', [605 122 645 158]);
-
-%if only one output, for the moment output signed data
-if n_inputs == 1, 
-  type = 1;
-  bin_pt = input_bit_width-1;
-else, 
-  type = 0;
-  bin_pt = 0;
-end
+%b signal path
+reuse_block(blk, 'bi', 'built-in/Inport', 'Port', '2', 'Position', [10 213 40 227]);
 
 if strcmp(async, 'on'), inputNum = 4;
 else, inputNum = 3;
 end
 
-reuse_block(blk, 'bus_expand2', 'casper_library_flow_control/bus_expand', ...
-        'mode', 'divisions of arbitrary size', ...
-        'outputNum', num2str(inputNum), ...
-        'outputWidth', mat2str([n_inputs*(input_bit_width+1) n_inputs*(input_bit_width+1)]), ...
-        'outputBinaryPt', mat2str([bin_pt, bin_pt]), ...
-        'outputArithmeticType', mat2str([type, type]), ...
-        'Position', [670 112 720 168]);
-
-add_line(blk, 'munge_b/1', 'bus_expand2/1');
-
-reuse_block(blk, 'bw_re', 'built-in/Outport', 'Port', '3', 'Position', [750 118 780 132]);
-add_line(blk, 'bus_expand2/1', 'bw_re/1');
-reuse_block(blk, 'bw_im', 'built-in/Outport', 'Port', '4', 'Position', [750 148 780 162]);
-add_line(blk, 'bus_expand2/2', 'bw_im/1');
+reuse_block(blk, 'bwo', 'built-in/Outport', 'Port', '2', 'Position', [750 133 780 147]);
 
 %data valid for asynchronous operation
 if strcmp(async, 'on'),
   reuse_block(blk, 'dvi', 'built-in/Inport', 'Port', '4', 'Position', [10 183 40 197]);
-  reuse_block(blk, 'dvo', 'built-in/Outport', 'Port', '6', 'Position', [750 333 780 347]);
+  reuse_block(blk, 'dvo', 'built-in/Outport', 'Port', '4', 'Position', [750 333 780 347]);
 end
 
 reuse_block(blk, 'bus_create', 'casper_library_flow_control/bus_create', ...
         'inputNum', num2str(inputNum), 'Position', [145 204 200 326]);
-add_line(blk, 'b/1', 'bus_create/1');
+add_line(blk, 'bi/1', 'bus_create/1');
 add_line(blk, 'sync_in/1', 'bus_create/2');
-add_line(blk, 'munge_a/1', 'bus_create/3');
+add_line(blk, 'ai/1', 'bus_create/3');
 
 % Coefficient generator
 reuse_block(blk, 'coeff_gen', 'casper_library_ffts_twiddle_coeff_gen/coeff_gen', ...
@@ -213,21 +168,22 @@ reuse_block(blk, 'bus_convert', 'casper_library_bus/bus_convert', ...
       'Position', [515 64 570 366]);
 add_line(blk, 'bus_mult/1', 'bus_convert/1'); 
 add_line(blk, 'bus_mult/2', 'bus_convert/2'); 
-add_line(blk, 'bus_convert/1', 'munge_b/1'); 
+add_line(blk, 'bus_convert/1', 'bwo/1');
 
+outputWidth = [1 n_inputs*(input_bit_width*2)];   %cut sync out
+outputBinaryPt = [0 0];                           
+outputArithmeticType = [2 0];
+
+%cut dvalid out again
 if strcmp(async, 'on'),
-  outputWidth = [1 n_inputs*input_bit_width n_inputs*input_bit_width 1]; %for bus_expand1
-  outputBinaryPt = [0  bin_pt bin_pt  0];
-  outputArithmeticType = [2 type type 2];
-else
-  outputWidth = [1 n_inputs*input_bit_width n_inputs*input_bit_width]; %for bus_expand1
-  outputBinaryPt = [0 bin_pt bin_pt];
-  outputArithmeticType = [2 type type];
+  outputWidth = [outputWidth, 1];
+  outputBinaryPt = [outputBinaryPt, 0];
+  outputArithmeticType = [outputArithmeticType, 2];
 end
 
 reuse_block(blk, 'bus_expand1', 'casper_library_flow_control/bus_expand', ...
         'mode', 'divisions of arbitrary size', ...
-        'outputNum', num2str(inputNum), ...
+        'outputNum', num2str(inputNum-1), ...
         'outputWidth', mat2str(outputWidth) , ...
         'outputBinaryPt', mat2str(outputBinaryPt) , ...
         'outputArithmeticType', mat2str(outputArithmeticType), ...
@@ -236,10 +192,9 @@ reuse_block(blk, 'bus_expand1', 'casper_library_flow_control/bus_expand', ...
 add_line(blk,'bus_convert/2','bus_expand1/1');
 
 add_line(blk,'bus_expand1/1','sync_out/1');
-add_line(blk,'bus_expand1/2','a_re/1');
-add_line(blk,'bus_expand1/3','a_im/1');
+add_line(blk,'bus_expand1/2','ao/1');
 
-if strcmp(async, 'on'), add_line(blk,'bus_expand1/4','dvo/1'); end
+if strcmp(async, 'on'), add_line(blk,'bus_expand1/3','dvo/1'); end
 
 clean_blocks(blk);
 
