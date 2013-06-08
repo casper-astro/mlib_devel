7610777d5b89f728429b5e7f6b40bbffc276e807
David MacMahon <davidm@astro.berkeley.edu>
2013-06-08 12:18:21 -0700

    Add ability to specify custom hw_routes file
    Users now have the option of loading a custom hw_routes file by setting the
    environment variable CASPER_HW_ROUTES to the name of the file to load.  If not
    set, the default file "hw_routes.mat" is loaded.
    
    The file name given is passed to the Matlab "load" function.  It can be given
    as a fully qualified file name (i.e. start with a '/') or a partially qualified
    file name.  Partially qualified names (including file names with no '/'
    characters) are searched for on the Matlab path.
    
    One use of this feature is to specify an alternate hw_routes file for ROACH2
    Revsion 1 development.

---
+++ xps_library/load_hw_routes.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%   Center for Astronomy Signal Processing and Electronics Research           %
+%   http://casper.berkeley.edu/                                               %
+%   Copyright (C) 2013 University of California, Berkeley                     %
+%                                                                             %
+%   This program is free software; you can redistribute it and/or modify      %
+%   it under the terms of the GNU General Public License as published by      %
+%   the Free Software Foundation; either version 2 of the License, or         %
+%   (at your option) any later version.                                       %
+%                                                                             %
+%   This program is distributed in the hope that it will be useful,           %
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of            %
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             %
+%   GNU General Public License for more details.                              %
+%                                                                             %
+%   You should have received a copy of the GNU General Public License along   %
+%   with this program; if not, write to the Free Software Foundation, Inc.,   %
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function hw_routes = load_hw_routes()
+  hw_routes_file = getenv('CASPER_HW_ROUTES');
+  if isempty('hw_routes_file')
+    hw_routes_file = 'hw_routes.mat';
+  end
+  hw_routes = load(hw_routes_file);
+end
