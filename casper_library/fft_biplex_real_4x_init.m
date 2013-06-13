7dcf0fc8cad5b711ed6abb8b05c165630de0e0d0
Andrew Martens <andrew@ska.ac.za>
2013-06-13 11:23:55 +0200

    fft_biplex_real_4x upgrade
    Options added:
    * Asynchronous operation
    * Multiple simultaneous inputs sharing coefficients and control logic
    * Bitgrowth option instead of shifting
    
    %TODO
    * Twiddle sharing- option there but not passed down
    * Twiddle decimation - ''
    * Diagnose strangeness when optimising coefficients

--- casper_library/fft_biplex_real_4x_init.m
+++ casper_library/fft_biplex_real_4x_init.m
@@ -32,6 +32,10 @@ function fft_biplex_real_4x_init(blk, varargin)
 %   Copyright (C) 2007 Terry Filiba, Aaron Parsons                            %
 %   Copyright (C) 2010 William Mallard                                        %
 %                                                                             %
+%   SKASA                                                                     %
+%   www.kat.ac.za                                                             %
+%   Copyright (C) 2013 Andrew Martens                                         %
+%                                                                             %
 %   This program is free software; you can redistribute it and/or modify      %
 %   it under the terms of the GNU General Public License as published by      %
 %   the Free Software Foundation; either version 2 of the License, or         %
@@ -48,35 +52,39 @@ function fft_biplex_real_4x_init(blk, varargin)
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clog('entering fft_biplex_real_4x_init','trace');
+clog('entering fft_biplex_real_4x_init',{'trace', 'fft_biplex_real_4x_init_debug'});
 
 % Set default vararg values.
 defaults = { ...
+    'n_inputs', 2, ...
     'FFTSize', 2, ...
     'input_bit_width', 18, ...
+    'bin_pt_in', 17, ...
     'coeff_bit_width', 18, ...
+    'async', 'off', ...
     'add_latency', 1, ...
     'mult_latency', 2, ...
     'bram_latency', 2, ...
     'conv_latency', 1, ...
     'quantization', 'Round  (unbiased: +/- Inf)', ...
     'overflow', 'Saturate', ...
-    'arch', 'Virtex5', ...
     'opt_target', 'logic', ...
     'coeffs_bit_limit', 8, ...
     'delays_bit_limit', 8, ...
     'mult_spec', 2, ...
+    'bitgrowth', 'on', ...
+    'max_bits', 19, ...
     'hardcode_shifts', 'off', ...
     'shift_schedule', [1 1], ...
     'dsp48_adders', 'off', ...
+    'twiddle_sharing', 'off', ...
+    'twiddle_decimation', 'off', ...
 };
 
 % Skip init script if mask state has not changed.
-if same_state(blk, 'defaults', defaults, varargin{:}),
-  return
-end
+%if same_state(blk, 'defaults', defaults, varargin{:}), return; end
 
-clog('fft_biplex_real_4x_init post same_state','trace');
+clog('fft_biplex_real_4x_init post same_state',{'trace','fft_biplex_real_4x_init_debug'});
 
 % Verify that this is the right mask for the block.
 check_mask_type(blk, 'fft_biplex_real_4x');
@@ -85,33 +93,37 @@ check_mask_type(blk, 'fft_biplex_real_4x');
 munge_block(blk, varargin{:});
 
 % Retrieve values from mask fields.
-FFTSize = get_var('FFTSize', 'defaults', defaults, varargin{:});
-input_bit_width = get_var('input_bit_width', 'defaults', defaults, varargin{:});
-coeff_bit_width = get_var('coeff_bit_width', 'defaults', defaults, varargin{:});
-add_latency = get_var('add_latency', 'defaults', defaults, varargin{:});
-mult_latency = get_var('mult_latency', 'defaults', defaults, varargin{:});
-bram_latency = get_var('bram_latency', 'defaults', defaults, varargin{:});
-conv_latency = get_var('conv_latency', 'defaults', defaults, varargin{:});
-quantization = get_var('quantization', 'defaults', defaults, varargin{:});
-overflow = get_var('overflow', 'defaults', defaults, varargin{:});
-arch = get_var('arch', 'defaults', defaults, varargin{:});
-opt_target = get_var('opt_target', 'defaults', defaults, varargin{:});
-coeffs_bit_limit = get_var('coeffs_bit_limit', 'defaults', defaults, varargin{:});
-delays_bit_limit = get_var('delays_bit_limit', 'defaults', defaults, varargin{:});
-mult_spec = get_var('mult_spec', 'defaults', defaults, varargin{:});
-hardcode_shifts = get_var('hardcode_shifts', 'defaults', defaults, varargin{:});
-shift_schedule = get_var('shift_schedule', 'defaults', defaults, varargin{:});
-dsp48_adders = get_var('dsp48_adders', 'defaults', defaults, varargin{:});
-
-clog(flatstrcell(varargin), 'fft_biplex_real_4x_init_debug');
+n_inputs            = get_var('n_inputs', 'defaults', defaults, varargin{:});
+FFTSize             = get_var('FFTSize', 'defaults', defaults, varargin{:});
+input_bit_width     = get_var('input_bit_width', 'defaults', defaults, varargin{:});
+bin_pt_in           = get_var('bin_pt_in', 'defaults', defaults, varargin{:});
+coeff_bit_width     = get_var('coeff_bit_width', 'defaults', defaults, varargin{:});
+async               = get_var('async', 'defaults', defaults, varargin{:});
+add_latency         = get_var('add_latency', 'defaults', defaults, varargin{:});
+mult_latency        = get_var('mult_latency', 'defaults', defaults, varargin{:});
+bram_latency        = get_var('bram_latency', 'defaults', defaults, varargin{:});
+conv_latency        = get_var('conv_latency', 'defaults', defaults, varargin{:});
+quantization        = get_var('quantization', 'defaults', defaults, varargin{:});
+overflow            = get_var('overflow', 'defaults', defaults, varargin{:});
+opt_target          = get_var('opt_target', 'defaults', defaults, varargin{:});
+coeffs_bit_limit    = get_var('coeffs_bit_limit', 'defaults', defaults, varargin{:});
+delays_bit_limit    = get_var('delays_bit_limit', 'defaults', defaults, varargin{:});
+mult_spec           = get_var('mult_spec', 'defaults', defaults, varargin{:});
+bitgrowth           = get_var('bitgrowth', 'defaults', defaults, varargin{:});
+max_bits            = get_var('max_bits', 'defaults', defaults, varargin{:});
+hardcode_shifts     = get_var('hardcode_shifts', 'defaults', defaults, varargin{:});
+shift_schedule      = get_var('shift_schedule', 'defaults', defaults, varargin{:});
+dsp48_adders        = get_var('dsp48_adders', 'defaults', defaults, varargin{:});
+twiddle_sharing     = get_var('twiddle_sharing', 'defaults', defaults, varargin{:});
+twiddle_decimation  = get_var('twiddle_decimation', 'defaults', defaults, varargin{:});
 
 %default empty for storage in library
-if FFTSize == 0,
+if n_inputs == 0 | FFTSize == 0,
   delete_lines(blk);
   clean_blocks(blk);
   set_param(blk, 'AttributesFormatString', '');
   save_state(blk, 'defaults', defaults, varargin{:});
-  clog('exiting fft_biplex_real_4x_init','trace');
+  clog('exiting fft_biplex_real_4x_init',{'trace', 'fft_biplex_real_4x_init_debug'});
   return;
 end
 
@@ -135,114 +147,164 @@ end
 delete_lines(blk);
 
 %
-% Add inputs and outputs.
+% prepare bus creators
+%
+
+reuse_block(blk, 'even_bussify', 'casper_library_flow_control/bus_create', ...
+  'inputNum', num2str(n_inputs), 'Position', [150 74 210 116+((n_inputs-1)*60)]); 
+
+reuse_block(blk, 'odd_bussify', 'casper_library_flow_control/bus_create', ...
+  'inputNum', num2str(n_inputs), 'Position', [150 74+(n_inputs*60) 210 116+(((n_inputs*2)-1)*60)]); 
+
+%
+% prepare bus splitters
+%
+
+% TODO final bus width calculation
+if strcmp(bitgrowth,'on'), n_bits_out = min(input_bit_width+FFTSize, max_bits);
+else n_bits_out = input_bit_width;
+end
+
+for index = 0:3,
+  reuse_block(blk, ['pol',num2str(index),'_debus'], 'casper_library_flow_control/bus_expand', ...
+    'mode', 'divisions of equal size', 'outputNum', num2str(n_inputs), ...
+    'outputWidth', num2str(n_bits_out), 'outputBinaryPt', num2str(bin_pt_in), ...
+    'outputArithmeticType', '1', ...
+    'Position', [510 49+(n_inputs*index)*30 570 91+(n_inputs*index)*30]);
+end %for
+
+%
+% inputs and outputs.
 %
 
 reuse_block(blk, 'sync', 'built-in/inport', 'Position', [15 13 45 27], 'Port', '1');
 reuse_block(blk, 'shift', 'built-in/inport', 'Position', [15 43 45 57], 'Port', '2');
-reuse_block(blk, 'pol1', 'built-in/inport', 'Position', [15 73 45 87], 'Port', '3');
-reuse_block(blk, 'pol2', 'built-in/inport', 'Position', [15 103 45 117], 'Port', '4');
-reuse_block(blk, 'pol3', 'built-in/inport', 'Position', [15 133 45 147], 'Port', '5');
-reuse_block(blk, 'pol4', 'built-in/inport', 'Position', [15 163 45 177], 'Port', '6');
+reuse_block(blk, 'sync_out', 'built-in/outport', 'Position', [635 25 665 39], 'Port', '1');
 
-reuse_block(blk, 'sync_out', 'built-in/outport', 'Position', [535  28 565  42], 'Port', '1');
-reuse_block(blk, 'pol1_out', 'built-in/outport', 'Position', [585  53 615  67], 'Port', '2');
-reuse_block(blk, 'pol2_out', 'built-in/outport', 'Position', [535  78 565  92], 'Port', '3');
-reuse_block(blk, 'pol3_out', 'built-in/outport', 'Position', [585 103 615 117], 'Port', '4');
-reuse_block(blk, 'pol4_out', 'built-in/outport', 'Position', [535 128 565 142], 'Port', '5');
-reuse_block(blk, 'of', 'built-in/outport', 'Position', [320 138 350 152], 'Port', '6');
+for index = 0:n_inputs*4-1,
+  reuse_block(blk, ['pol',num2str(index),'_in'], 'built-in/inport', ...
+    'Position', [15 73+(index*30) 45 87+(index*30)], ...
+    'Port', num2str(3+index));
 
-%
-% Add ri_to_c blocks.
-%
+  reuse_block(blk, ['pol',num2str(index),'_out'], 'built-in/outport', ...
+    'Position', [635 53+(index*30) 665 67+(index*30)], 'Port', num2str(2+index));
 
-reuse_block(blk, 'ri_to_c0', 'casper_library_misc/ri_to_c', ...
-    'Position', [95 74 135 116], ...
-    'LinkStatus', 'inactive');
-reuse_block(blk, 'ri_to_c1', 'casper_library_misc/ri_to_c', ...
-    'Position', [95 134 135 176], ...
-    'LinkStatus', 'inactive');
+  % Add ri_to_c blocks.
+  if mod(index+1, 2) == 0,
+    reuse_block(blk, ['ri_to_c',num2str(floor(index/2))], 'casper_library_misc/ri_to_c', ...
+        'Position', [65 74+(((index-1)/2)*60) 105 116+(((index-1)/2)*60)], ...
+        'LinkStatus', 'inactive');
+    add_line(blk, ['pol',num2str(index-1),'_in/1'],['ri_to_c',num2str(floor(index/2)),'/1']);
+    add_line(blk, ['pol',num2str(index),'_in/1'],['ri_to_c',num2str(floor(index/2)),'/2']);
+
+    %connect ri_to_c to bus creators
+    if mod(((index+1)/2),2) == 1, bussify_target = 'even';
+    else bussify_target = 'odd';
+    end
+
+    %connect ri_to_c to bussify blocks
+    add_line(blk, ['ri_to_c', num2str(floor(index/2)),'/1'], [bussify_target, '_bussify/', num2str(floor(index/4)+1)]);
+  end
+
+  %connect debus outputs to output
+  add_line(blk, ['pol', num2str(mod(index,4)), '_debus/', num2str(floor(index/4)+1)], ['pol', num2str(index),'_out/1']); 
+end
+
+reuse_block(blk, 'of', 'built-in/outport', 'Position', [400 150 430 164], 'Port', num2str(1+(n_inputs*4)+1));
+
+if strcmp(async, 'on'),
+  reuse_block(blk, 'en', 'built-in/inport', ...
+    'Position', [195 73+(((n_inputs*4)+1)*30) 225 87+(((n_inputs*4)+1)*30)], 'Port', num2str(2+(n_inputs*4)+1));
+  reuse_block(blk, 'dvalid', 'built-in/outport', ...
+    'Position', [535 53+(((n_inputs*4)+1)*30) 565 67+(((n_inputs*4)+1)*30)], 'Port', num2str(1+(n_inputs*4)+1+1));
+end
 
 %
 % Add biplex_core block.
 %
+%TODO twiddle decimation and sharing options to biplex_core
 
 reuse_block(blk, 'biplex_core', 'casper_library_ffts/biplex_core', ...
-    'Position', [185 30 285 125], ...
-    'LinkStatus', 'inactive', ...
+    'Position', [250 30 335 125], ...
+    'n_inputs', num2str(n_inputs), ...
     'FFTSize', num2str(FFTSize), ...
-    'input_bit_width', num2str(input_bit_width), ...
-    'coeff_bit_width', num2str(coeff_bit_width), ...
-    'add_latency', num2str(add_latency), ...
-    'mult_latency', num2str(mult_latency), ...
-    'bram_latency', num2str(bram_latency), ...
-    'conv_latency', num2str(conv_latency), ...
-    'quantization', tostring(quantization), ...
-    'overflow', tostring(overflow), ...
-    'arch', tostring(arch), ...
-    'opt_target', tostring(opt_target), ...
-    'coeffs_bit_limit', num2str(coeffs_bit_limit), ...
-    'delays_bit_limit', num2str(delays_bit_limit), ...
-    'mult_spec', tostring(mult_spec), ...
-    'hardcode_shifts', tostring(hardcode_shifts), ...
-    'shift_schedule', tostring(shift_schedule), ...
-    'dsp48_adders', tostring(dsp48_adders));
+    'input_bit_width', 'input_bit_width', ...
+    'bin_pt_in', 'bin_pt_in', ...
+    'coeff_bit_width', 'coeff_bit_width', ...
+    'add_latency', 'add_latency', ...
+    'mult_latency', 'mult_latency', ...
+    'bram_latency', 'bram_latency', ...
+    'conv_latency', 'conv_latency', ...
+    'quantization', quantization, ...
+    'overflow', overflow, ...
+    'opt_target', opt_target, ...
+    'coeffs_bit_limit', 'coeffs_bit_limit', ...
+    'delays_bit_limit', 'delays_bit_limit', ...
+    'mult_spec', mat2str(mult_spec), ...
+    'bitgrowth', bitgrowth, ...
+    'max_bits', num2str(max_bits), ...
+    'hardcode_shifts', hardcode_shifts, ...
+    'shift_schedule', mat2str(shift_schedule), ...
+    'dsp48_adders', dsp48_adders, ...
+    'async', async);
+
+add_line(blk, 'sync/1', 'biplex_core/1');
+add_line(blk, 'shift/1', 'biplex_core/2');
+add_line(blk, 'even_bussify/1', 'biplex_core/3');
+add_line(blk, 'odd_bussify/1', 'biplex_core/4');
+
+if strcmp(async, 'on'), add_line(blk, 'en/1', 'biplex_core/5');
+end
+
+add_line(blk, 'biplex_core/4', 'of/1');
 
 %
 % Add bi_real_unscr_4x block.
 %
 
 reuse_block(blk, 'bi_real_unscr_4x', 'casper_library_ffts_internal/bi_real_unscr_4x', ...
-    'Position', [385 15 485 151], ...
-    'LinkStatus', 'inactive', ...
+    'Position', [380 30 455 120], ...
+    'n_inputs', num2str(n_inputs), ...
     'FFTSize', num2str(FFTSize), ...
-    'n_bits', num2str(input_bit_width), ...
-    'add_latency', num2str(add_latency), ...
-    'conv_latency', num2str(conv_latency), ...
-    'bram_latency', num2str(bram_latency), ...
+    'n_bits', num2str(n_bits_out), ...
+    'bin_pt', num2str(bin_pt_in), ...
+    'add_latency', 'add_latency', ...
+    'conv_latency', 'conv_latency', ...
+    'bram_latency', 'bram_latency', ...
     'bram_map', 'off', ...
     'bram_delays', bram_delays, ...
-    'dsp48_adders', dsp48_adders);
-
-%
-% Draw wires.
-%
-
-add_line(blk, 'pol1/1', 'ri_to_c0/1');
-add_line(blk, 'pol2/1', 'ri_to_c0/2');
-add_line(blk, 'pol3/1', 'ri_to_c1/1');
-add_line(blk, 'pol4/1', 'ri_to_c1/2');
-
-add_line(blk, 'sync/1', 'biplex_core/1');
-add_line(blk, 'shift/1', 'biplex_core/2');
-add_line(blk, 'ri_to_c0/1', 'biplex_core/3');
-add_line(blk, 'ri_to_c1/1', 'biplex_core/4');
+    'dsp48_adders', dsp48_adders, ...
+    'async', async);
 
 add_line(blk, 'biplex_core/1', 'bi_real_unscr_4x/1');
 add_line(blk, 'biplex_core/2', 'bi_real_unscr_4x/2');
 add_line(blk, 'biplex_core/3', 'bi_real_unscr_4x/3');
-add_line(blk, 'biplex_core/4', 'of/1');
+
+if strcmp(async, 'on'), 
+  add_line(blk, 'biplex_core/5', 'bi_real_unscr_4x/4');
+  add_line(blk, 'bi_real_unscr_4x/6', 'dvalid/1');
+end
 
 add_line(blk, 'bi_real_unscr_4x/1', 'sync_out/1');
-add_line(blk, 'bi_real_unscr_4x/2', 'pol1_out/1');
-add_line(blk, 'bi_real_unscr_4x/3', 'pol2_out/1');
-add_line(blk, 'bi_real_unscr_4x/4', 'pol3_out/1');
-add_line(blk, 'bi_real_unscr_4x/5', 'pol4_out/1');
+add_line(blk, 'bi_real_unscr_4x/2', 'pol0_debus/1');
+add_line(blk, 'bi_real_unscr_4x/3', 'pol1_debus/1');
+add_line(blk, 'bi_real_unscr_4x/4', 'pol2_debus/1');
+add_line(blk, 'bi_real_unscr_4x/5', 'pol3_debus/1');
 
 % Delete all unconnected blocks.
-clean_blocks(blk);
+%clean_blocks(blk);
 
 %%%%%%%%%%%%%%%%%%%
 % Finish drawing! %
 %%%%%%%%%%%%%%%%%%%
 
 % Set attribute format string (block annotation).
-fmtstr = sprintf('%s\n%d stages\n[%d,%d]\n%s\n%s', ...
-    arch, FFTSize, input_bit_width, coeff_bit_width, quantization, overflow);
+fmtstr = sprintf('%d stages\n[%d,%d]\n%s\n%s', ...
+    FFTSize, input_bit_width, coeff_bit_width, quantization, overflow);
 set_param(blk, 'AttributesFormatString', fmtstr);
 
 % Save block state to stop repeated init script runs.
 save_state(blk, 'defaults', defaults, varargin{:});
 
-clog('exiting fft_biplex_real_4x_init','trace');
+clog('exiting fft_biplex_real_4x_init',{'trace','fft_biplex_real_4x_init_debug'});
 
