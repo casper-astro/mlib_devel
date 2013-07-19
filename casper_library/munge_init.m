2c13dabdbfb0fd74e44560f5de7b28da21097031
Andrew Martens <andrew@ska.ac.za>
2013-07-19 14:37:17 +0200

    munge output type/binary point option
    after munging the option is now provided to reinterpret the data
    as a specified type and with a specified banry point position. This
    is useful if in the input (and required output type) is not Unsigned.

--- casper_library/munge_init.m
+++ casper_library/munge_init.m
@@ -6,9 +6,11 @@
 % varargin = {'varname', 'value', ...} pairs
 %
 % Valid varnames for this block are:
-% divisions = number of divisions
-% div_size = size in bits of each division
-% order = ouput order of divisions (referenced to input order)
+% divisions       = number of divisions
+% div_size        = size in bits of each division
+% order           = ouput order of divisions (referenced to input order)
+% arith_type_out  = reinterpret resultant vector
+% bin_pt_out      = output binary point
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
@@ -37,37 +39,54 @@ function munge_init(blk,varargin)
 clog('entering munge_init', 'trace');
 check_mask_type(blk, 'munge');
 
-defaults = {'divisions', 1, 'div_size', 0, 'order', 0};
+defaults = {'divisions', 1, 'div_size', 1, 'order', 0, 'arith_type_out', 'Unsigned', 'bin_pt_out', 0};
 if same_state(blk, 'defaults', defaults, varargin{:}), return, end
 clog('munge_init: post same_state', 'trace');
 munge_block(blk, varargin{:});
 
-divisions = get_var('divisions', 'defaults', defaults, varargin{:});
-div_size = get_var('div_size', 'defaults', defaults, varargin{:});
-output_order = get_var('order', 'defaults', defaults, varargin{:});
+divisions       = get_var('divisions', 'defaults', defaults, varargin{:});
+div_size        = get_var('div_size', 'defaults', defaults, varargin{:});
+output_order    = get_var('order', 'defaults', defaults, varargin{:});
+arith_type_out  = get_var('arith_type_out', 'defaults', defaults, varargin{:});
+bin_pt_out      = get_var('bin_pt_out', 'defaults', defaults, varargin{:});
 
+%default empty state
 if divisions < 1,
-  clog(['Number of divisions must be greater than 0'], 'error');
-  errordlg(['Number of divisions must be greater than 0']);
+  delete_lines(blk);
+  clean_blocks(blk);
+  set_param(blk,'AttributesFormatString','');
+  save_state(blk, 'defaults', defaults, varargin{:});  % Save and back-populate mask parameter values
+  clog('exiting munge_init', 'trace');
   return;
 end
 
 if divisions > 1 && length(find(div_size < 1) ~= 0),
   clog(['Divisions must have non-zero size'], 'error');
-  errordlg(['Divisions must have non-zero size']);
+  error(['Divisions must have non-zero size']);
   return;
 end
 
 if length(div_size) > 1 && (length(div_size) ~= divisions),
     clog(['Reported number of divisions, ',num2str(divisions-1),' does not match division description length ',num2str(len(div_size))], 'error');
-    errordlg(['Reported number of divisions, ',num2str(divisions-1),' does not match division description length ',num2str(len(div_size))]);
+    error(['Reported number of divisions, ',num2str(divisions-1),' does not match division description length ',num2str(len(div_size))]);
     return;
 end
 
 if  length(find(output_order < 0)) ~= 0 || ... 
     length(find(output_order > divisions-1)) ~= 0,
     clog(['Output order elements must be in range 0->',num2str(divisions-1)], 'error');
-    errordlg(['Output order elements must be in range 0->',num2str(divisions-1)]);
+    error(['Output order elements must be in range 0->',num2str(divisions-1)]);
+    return;
+end
+
+%calculate resultant word size
+n_bits_out = 0;
+for index = 1:length(output_order),
+  n_bits_out = n_bits_out+div_size(output_order(index)+1);
+end
+if n_bits_out < bin_pt_out,
+    clog(['binary point position ',num2str(bin_pt_out),' greater than number of bits ',num2str(n_bits_out)], {'error', 'munge_init_debug'});
+    error(['binary point position ',num2str(bin_pt_out),' greater than number of bits ',num2str(n_bits_out)]);
     return;
 end
 
@@ -76,20 +95,25 @@ delete_lines(blk);
 ytick = 20;
 
 if length(div_size) == 1,
-mode =  'divisions of equal size';
-inputNum = divisions;
+  mode =  'divisions of equal size';
+  inputNum = divisions;
 else,
-mode = 'divisions of arbitrary size';
-inputNum = length(output_order);
+  mode = 'divisions of arbitrary size';
+  inputNum = length(output_order);
 end
 
 %input 
 reuse_block(blk, 'din', 'built-in/inport', 'Position', [40 10+ytick*(divisions+1)/2 70 30+ytick*(divisions+1)/2], 'Port', '1');
 %output
-reuse_block(blk, 'dout', 'built-in/outport', 'Position', [670 10+ytick*(inputNum+1)/2 700 30+ytick*(inputNum+1)/2], 'Port', '1');
+reuse_block(blk, 'reinterpret_out', 'xbsIndex_r4/Reinterpret', ...
+  'force_arith_type', 'on', 'arith_type', arith_type_out, ...
+  'force_bin_pt', 'on', 'bin_pt', num2str(bin_pt_out), ...
+  'Position', [655 10+ytick*(inputNum+1)/2 710 30+ytick*(inputNum+1)/2]);
+reuse_block(blk, 'dout', 'built-in/outport', 'Position', [780 10+ytick*(inputNum+1)/2 810 30+ytick*(inputNum+1)/2], 'Port', '1');
+add_line(blk, 'reinterpret_out/1', 'dout/1');
 
 if divisions < 2,
-  add_line(blk, 'din/1', 'dout/1');
+  add_line(blk, 'din/1', 'reinterpret_out/1');
 else
   %reinterpret
   reuse_block(blk, 'reinterpret', 'xbsIndex_r4/Reinterpret', ...
@@ -97,8 +121,6 @@ else
     'force_bin_pt', 'on', 'bin_pt', '0', ...
     'Position', [95 12+ytick*(divisions+1)/2 160 28+ytick*(divisions+1)/2]);
   add_line(blk, 'din/1', 'reinterpret/1');
-
-
 
   %bus expand
   reuse_block(blk, 'split', 'casper_library_flow_control/bus_expand', ...
@@ -115,7 +137,7 @@ else
   reuse_block(blk, 'join', 'casper_library_flow_control/bus_create', ...
     'inputNum', num2str(inputNum), ...
     'Position', [550 10 630 30+ytick*inputNum]);
-  add_line(blk, 'join/1', 'dout/1');
+  add_line(blk, 'join/1', 'reinterpret_out/1');
 
   %join
   for div = 1:length(output_order),
@@ -127,7 +149,7 @@ end %if
 clean_blocks(blk);
 
 % Set attribute format string (block annotation)
-annotation=sprintf('split:%s\njoin:%s',mat2str(div_size), mat2str(output_order));
+annotation=sprintf('split:%s\njoin:%s\n%s [%d,%d]',mat2str(div_size), mat2str(output_order), arith_type_out, n_bits_out, bin_pt_out);
 set_param(blk,'AttributesFormatString',annotation);
 
 save_state(blk, 'defaults', defaults, varargin{:});  % Save and back-populate mask parameter values
