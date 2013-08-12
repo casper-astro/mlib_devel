b9efaba5a99e077bc509a11a1102cc2344d5b799
Paul Prozesky <paulp@ska.ac.za>
2013-08-12 16:25:13 +0200

    Fixed for blocks without ports.

--- casper_library/clean_blocks.m
+++ casper_library/clean_blocks.m
@@ -32,23 +32,25 @@ blocks = get_param(cursys, 'Blocks');
 for i=1:length(blocks),
     blk = [cursys,'/',blocks{i}];
     ports = get_param(blk, 'PortConnectivity');
-    flag = 0;
-    for j=1:length(ports),
-        if ports(j).SrcBlock == -1,
-            clog(['block ' cursys '/' get_param(blk,'Name') ...
-                 ' input port ' num2str(j) ...
-                 ' undriven (block will be deleted)'], ...
-                 'clean_blocks_debug');
-            flag = 0;
-            break
-        elseif ~flag && (length(ports(j).SrcBlock) ~= 0 || length(ports(j).DstBlock) ~= 0),
-            flag = 1;
+    if ~isempty(ports),
+        flag = 0;
+        for j=1:length(ports),
+            if ports(j).SrcBlock == -1,
+                clog(['block ' cursys '/' get_param(blk,'Name') ...
+                     ' input port ' num2str(j) ...
+                     ' undriven (block will be deleted)'], ...
+                     'clean_blocks_debug');
+                flag = 0;
+                break
+            elseif ~flag && (length(ports(j).SrcBlock) ~= 0 || length(ports(j).DstBlock) ~= 0),
+                flag = 1;
+            end
         end
-    end
-    if flag == 0,
-        clog(['deleting block ' cursys '/' get_param(blk,'Name')], ...
-              'clean_blocks_debug');
-        delete_block(blk);
+        if flag == 0,
+            clog(['deleting block ' cursys '/' get_param(blk,'Name')], ...
+                  'clean_blocks_debug');
+            delete_block(blk);
+        end
     end
 end
 catch ex
