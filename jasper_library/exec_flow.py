38141bb57396b5bdf596f624fa101ccc2eac5768
Jack <jack@stoptellingmetoaddanemail>
2014-07-22 18:44:07 -0700

    Default build directory to dir with model name
    As original toolflow. Also some additions to accommodate ISE and Vivado
    backends. These are mostly temporary for playing.

--- jasper_library/exec_flow.py
+++ jasper_library/exec_flow.py
@@ -17,9 +17,10 @@ def shell_source(script):
 
 os.environ['USE_VIVADO_RUNTIME_FOR_MATLAB'] = '1' #see Xilinx answer record 59236
 os.environ['MATLAB_PATH'] = '/tools/MATLAB/R2013a'
-os.environ['XILINX_PATH'] = '/media/overflow/Xilinx/Vivado/2014.2'
-#os.environ['XILINX_PATH'] = '/tools/ISE/14.6/ISE_DS'
+#os.environ['XILINX_PATH'] = '/media/overflow/Xilinx/Vivado/2014.2'
+os.environ['XILINX_PATH'] = '/tools/ISE/14.6/ISE_DS'
 os.environ['MLIB_DEVEL_PATH'] = '/tools/mlib_devel'
+os.environ['SYSGEN_SCRIPT'] = os.environ['MLIB_DEVEL_PATH']+'/startsg'
 os.environ['MATLAB'] = os.environ['MATLAB_PATH']
 os.environ['CASPER_BASE_PATH'] = '/tools/mlib_devel'
 os.environ['HDL_ROOT'] = os.environ['CASPER_BASE_PATH']+'/jasper_library/hdl_sources'
@@ -39,17 +40,21 @@ parser.add_option("-m", "--model", dest="model", type='string',
                   default='/tools/mlib_devel/jasper_library/test_models/test.slx',
                   help="model to compile")
 parser.add_option("-c", "--builddir", dest="builddir", type='string',
-                  default='/home/jack/temp/toolflow_test',
-                  help="build directory")
+                  default='',
+                  help="build directory. Default: Use directory with same name as model")
 
 (opts, args) = parser.parse_args()
 
+# get build directory
+# use user defined directory else use a directory with same name as model
+builddir = opts.builddir or opts.model[:-4]
+
 # logging stuff...
-os.system('mkdir -p %s'%opts.builddir)
+os.system('mkdir -p %s'%builddir)
 logger = logging.getLogger('jasper')
 logger.setLevel(logging.DEBUG)
 
-handler = logging.FileHandler('%s/jasper.log'%opts.builddir, mode='w')
+handler = logging.FileHandler('%s/jasper.log'%builddir, mode='w')
 handler.setLevel(logging.DEBUG)
 format = logging.Formatter('%(levelname)s - %(asctime)s - %(name)s - %(message)s')
 handler.setFormatter(format)
@@ -61,8 +66,12 @@ logger.addHandler(ch)
 
 logger.info('Starting compile')
 
+if opts.be == 'vivado':
+    os.environ['SYSGEN_SCRIPT'] = os.environ['MLIB_DEVEL_PATH']+'/startsg'
+if opts.be == 'ise':
+    os.environ['SYSGEN_SCRIPT'] = os.environ['MLIB_DEVEL_PATH']+'/startsg_ise'
 
 # initialise the toolflow
-toolflow = toolflow.Toolflow(frontend='simulink', backend=opts.be, compile_dir=opts.builddir, frontend_target=opts.model)
+toolflow = toolflow.Toolflow(frontend='simulink', backend=opts.be, compile_dir=builddir, frontend_target=opts.model)
 
 toolflow.exec_flow(gen_per=opts.skipyb, frontend_compile=opts.skipfe, backend_compile=opts.skipbe)
