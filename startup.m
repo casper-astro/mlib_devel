d6cb5ed2182cdbf2a581d7e7baea703a0d955d9f
Jack <jack@stoptellingmetoaddanemail>
2014-07-22 18:38:01 -0700

    Made changes to start sysgen via vivado or ISE
    There are now two startup scripts, startsg_ise
    and startsg. These are called by the toolflow
    depending which backend is being used.
    This is a temporary measure, it's pretty dirty. The
    way the start scripts currently exist, startup.m is only
    really appropriate for ISE, uin vivado, it reimports
    stuff which is already loaded

--- startup.m
+++ startup.m
@@ -4,16 +4,16 @@ warning off Simulink:Engine:SaveWithParameterizedLinks_Warning
 warning off Simulink:Engine:SaveWithDisabledLinks_Warning
 warning off Simulink:Commands:LoadMdlParameterizedLink
 
-%if length(getenv('XILINX_PATH')) == 0
-%  setenv('XILINX_PATH', regexprep(getenv('XILINX'),'/ISE$',''));
-%end
-%addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
-%addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
+if length(getenv('XILINX_PATH')) == 0
+  setenv('XILINX_PATH', regexprep(getenv('XILINX'),'/ISE$',''));
+end
+addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
+addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
 addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
 addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
 addpath([getenv('MLIB_DEVEL_PATH'), '/jasper_library']);
-%xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
-%sysgen_startup
+xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
+sysgen_startup
 
 % If CASPER_BACKPORT is in the environment with non-zero length, then force
 % block reuse and do NOT preload CASPER libraries.  This prevents problems when
