1b7a395a775cab521c24e6c565c9fc0d7b5e9374
Jack H <jackhickish@gmail.com>
2015-04-10 22:02:24 +0100

    Path changes
    TODO: just write a single file that can be sourced

--- jasper_library/exec_flow.py
+++ jasper_library/exec_flow.py
@@ -39,14 +39,14 @@ parser.add_option("-c", "--builddir", dest="builddir", type='string',
 
 os.environ['MATLAB_PATH'] = '/tools/MATLAB/R2013a'
 if opts.be == 'vivado':
-    os.environ['XILINX_PATH'] = '/media/overflow/Xilinx/Vivado/2014.2'
+    os.environ['XILINX_PATH'] = '/opt/Xilinx/Vivado/2014.4'
     os.environ['USE_VIVADO_RUNTIME_FOR_MATLAB'] = '1' #see Xilinx answer record 59236
 else:
     os.environ['XILINX_PATH'] = '/tools/ISE/14.6/ISE_DS'
-os.environ['MLIB_DEVEL_PATH'] = '/tools/mlib_devel'
+os.environ['MLIB_DEVEL_PATH'] = '/home/jack/github/jack-h/jasper/mlib_devel'
 os.environ['SYSGEN_SCRIPT'] = os.environ['MLIB_DEVEL_PATH']+'/startsg'
 os.environ['MATLAB'] = os.environ['MATLAB_PATH']
-os.environ['CASPER_BASE_PATH'] = '/tools/mlib_devel'
+os.environ['CASPER_BASE_PATH'] = os.environ['MLIB_DEVEL_PATH']
 os.environ['HDL_ROOT'] = os.environ['CASPER_BASE_PATH']+'/jasper_library/hdl_sources'
 shell_source(os.environ['XILINX_PATH']+'/settings64.sh')
 
