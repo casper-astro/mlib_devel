c428ec409dff400685716e109eed2a3cb11c4394
Jack Hickish <jackh@acme1.digilab.pvt>
2016-05-30 15:45:00 -0700

    Revert "Ignore local configuration files"
    This reverts commit cd5e8efea517e2e5c280df2aa13b6e61cb9a4104.

---
+++ vivado_config.sh
@@ -0,0 +1,12 @@
+export XILINX_PATH=/opt/Xilinx/Vivado/2014.3.1
+export USE_VIVADO_RUNTIME_FOR_MATLAB=1
+export MATLAB_PATH=/opt/Matlab/R2015b
+export MLIB_DEVEL_PATH=~/work/git_work/ska_sa/projects/mlib_devel
+export PLATFORM=lin64
+
+export SYSGEN_SCRIPT=$MLIB_DEVEL_PATH/startsg
+export MATLAB=$MATLAB_PATH
+export CASPER_BASE_PATH=$MLIB_DEVEL_PATH
+export HDL_ROOT=$CASPER_BASE_PATH/jasper_library/hdl_sources
+export XPS_BASE_PATH=$MLIB_DEVEL_PATH/xps_base
+source $XILINX_PATH/settings64.sh
