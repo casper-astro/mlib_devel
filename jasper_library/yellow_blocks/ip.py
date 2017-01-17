0db7838c9001c0a81a25fad6b9d651adc977bdbc
Jack Hickish <jackh@acme1.acme.pvt>
2017-01-16 16:38:27 -0800

    Update ip block to be compatible with toolflow
    JASPER changed the way it handles libraries when the
    AXI-WB IP was added from the skarab.

--- jasper_library/yellow_blocks/ip.py
+++ jasper_library/yellow_blocks/ip.py
@@ -2,5 +2,10 @@ from yellow_block import YellowBlock
 
 class ip(YellowBlock):
     def initialize(self):
-        self.ips = [[self.lib_path, self.ip_name]]
+        self.ips = [{'path':self.lib_path, 
+                     'name':self.ip_name,
+                     'vendor':'User_Company',
+                     'library':'Sysgen',
+                     'version':'1.0',
+                     }]
         
