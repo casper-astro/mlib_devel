806ec05e2e4b45c28d39751e3d062b809c921c2e
hkriel <henno@ska.ac.za>
2017-02-13 14:28:43 +0200

    Modified to be able to issue write and read request simultaniously. FLIT 2..0 - Write32 Request, FLIT3 - Read32 Request

--- jasper_library/hdl_sources/hmc/src/flit_gen_user.v
+++ jasper_library/hdl_sources/hmc/src/flit_gen_user.v
@@ -223,6 +223,8 @@ module flit_gen_user #(
   end else begin
       s_axis_tx_TVALID_i <= 1'b0;
       s_axis_tx_TUSER_i  <= {NUM_DATA_BYTES{1'b0}};
+      s_axis_tx_TDATA_i  <= 512'd0;
+
       case (wr_flit_state)
         // State: Entry to state machine (from reset)
         STATE_IDLE: begin
@@ -243,19 +245,20 @@ module flit_gen_user #(
         // State: Request WR128 header, followed by write data (WR_REQ_DATA[447:0])
         STATE_WR_RD_DATA: begin
           req_rdy <= 1'b1;
-          if (s_axis_tx_TREADY == 1'b1) begin // Make sure AXI TX FIFO is not FULL             
-            if (WR_REQ == 1'b1) begin        
-              s_axis_tx_TVALID_i <= 1'b1; // Issue a write to TX AXI FIFO
-              //                      WR_REQ_DATA[447:320](s_axis_tx_TDATA_i[511:384]),  WR_REQ_DATA[319:64] (s_axis_tx_TDATA_i[383:128])  , WR_REQ_DATA[63:0] (s_axis_tx_TDATA_i[127:64]),   WR REQ Header (s_axis_tx_TDATA_i[63:0])      
-              s_axis_tx_TDATA_i  <= {128'd0, 64'd0, DATA_IN,                  {3'd0,3'd0,{2'd0,WR_ADDRESS,5'd0},tag, 4'd3, 4'd3, 1'b0, 6'b011001}}; // posted write req 32 bytes
-              s_axis_tx_TUSER_i[11:0]  <= 12'b0100_0001_0111; // No tails, Header on FLIT0, all FLITS 3-0 valid   
+          if (s_axis_tx_TREADY == 1'b1) begin // Make sure AXI TX FIFO is not FULL, with a WRITE32 command request ie (write 32bytes / 256bits from requested address)             
+            if (WR_REQ == 1'b1) begin // Write request uses FLITs 2..0       
+              s_axis_tx_TVALID_i <= 1'b1; // Issue a write to TX AXI FIFO                
+              s_axis_tx_TDATA_i[383:0]  <= {64'd0, DATA_IN,                  {3'd0,3'd0,{2'd0,WR_ADDRESS,5'd0},tag, 4'd3, 4'd3, 1'b0, 6'b011001}}; // posted write req 32 bytes
+              s_axis_tx_TUSER_i[2:0]  <= 3'b111; // all FLITS 2-0 valid  - Write Req 
+              s_axis_tx_TUSER_i[6:4]  <= 3'b001; // Header on FLIT0 - Write Req  
+              s_axis_tx_TUSER_i[10:8]  <= 3'b100; // Tail on FLIT2 - Write Req 
             end
-            if (RD_REQ == 1'b1) begin        
-              s_axis_tx_TVALID_i <= 1'b1; // Issue a write to TX AXI FIFO
-              //                      WR_REQ_DATA[447:320](s_axis_tx_TDATA_i[511:384]),  WR_REQ_DATA[319:64] (s_axis_tx_TDATA_i[383:128])  , WR_REQ_DATA[63:0] (s_axis_tx_TDATA_i[127:64]),   WR REQ Header (s_axis_tx_TDATA_i[63:0])      
-              //                                                                                               RD_ADDRESS[8:0] = tag
-              s_axis_tx_TDATA_i  <= {128'd0, 64'd0, 256'd0,                  {3'd0,3'd0,{2'd0,RD_ADDRESS,5'd0},TAG_IN, 4'd1, 4'd1, 1'b0, 6'b110001}}; // red req 32 bytes
-              s_axis_tx_TUSER_i[11:0]  <= 12'b0001_0001_0001; // Only FLIT0 (Header & Tail)    
+            if (RD_REQ == 1'b1) begin  // Read request uses FLIT 3        
+              s_axis_tx_TVALID_i <= 1'b1; // Issue a write to TX AXI FIFO, with a READ32 command request ie (read 32bytes / 256bits from requested address)
+              s_axis_tx_TDATA_i[511:384]  <= {64'd0, {3'd0,3'd0,{2'd0,RD_ADDRESS,5'd0},TAG_IN, 4'd1, 4'd1, 1'b0, 6'b110001}}; // red req 32 bytes
+              s_axis_tx_TUSER_i[3]  <= 1'b1; // Valid on FLIT3 - Read Req   
+              s_axis_tx_TUSER_i[7]  <= 1'b1; // Header on FLIT3 - Read Req
+              s_axis_tx_TUSER_i[11]  <= 1'b1; // Tail on FLIT3 - Read Req  
             end
           end 
         end
