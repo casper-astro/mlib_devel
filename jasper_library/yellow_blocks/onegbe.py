812ed3ba2e192ef4a2f6233eb7f698e038bdd887
Jack Hickish <jackh@berkeley.edu>
2017-08-08 16:33:21 -0700

    First attempt at supporting vcu118

--- jasper_library/yellow_blocks/onegbe.py
+++ jasper_library/yellow_blocks/onegbe.py
@@ -4,27 +4,14 @@ from helpers import to_int_list
 from yellow_block_typecodes import *
 
 class onegbe(YellowBlock):
-    def initialize(self):
-        self.typecode = TYPECODE_ETHCORE
-        self.add_source('onegbe/*.v')
-        self.add_source('onegbe/*.xci')
-        self.add_source('onegbe/*.coe')
-
-        self.provides = ['ethernet']
-        if (not self.dis_cpu_tx) and (not self.dis_cpu_tx):
-            self.provides += ['cpu_ethernet']
-
-        if self.platform.name in ['snap2']:
-            self.use_lvds = True
+    @staticmethod
+    def factory(blk, plat, hdl_root=None):
+        if plat.name in ['vcu118']:
+            return onegbe_vcu118(blk, plat, hdl_root)
         else:
-            self.use_lvds = False
+            return onegbe_snap(blk, plat, hdl_root)
 
-        if self.platform.name == 'snap2':
-            self.refclk_freq = 625.0
-        else:
-            self.refclk_freq = 125.0
-
-    def modify_top(self,top):
+    def _instantiate_udp(self, top):
         gbe_udp = top.get_instance(entity='gbe_udp', name=self.fullname, comment=self.fullname)
         gbe_udp.add_parameter('LOCAL_ENABLE',   '%d' % int(self.local_en))
         gbe_udp.add_parameter('LOCAL_MAC',      '48\'d%d' % self.local_mac)
@@ -75,8 +62,7 @@ class onegbe(YellowBlock):
         gbe_udp.add_port('mac_syncacquired', self.fullname + '_mac_syncacquired')
 
         gbe_udp.add_wb_interface(regname=self.unique_name, mode='rw', nbytes=65536, typecode=self.typecode)
-        
-        ###### MAC MODULE #######
+    def _instantiate_mac(self, top):
         gbe_mac = top.get_instance(entity='gig_eth_mac', name=self.fullname+'_mac', comment=self.fullname)
         gbe_mac.add_parameter('MAX_FRAME_SIZE_STANDARD', 1522)
         gbe_mac.add_parameter('MAX_FRAME_SIZE_JUMBO', 9022)
@@ -123,6 +109,107 @@ class onegbe(YellowBlock):
         gbe_mac.add_port('gmii_col', '0', parent_sig=False)
         gbe_mac.add_port('gmii_crs', '0', parent_sig=False)
 
+    def modify_top(self,top):
+        self._instantiate_udp(top)
+        self._instantiate_mac(top)
+        self._instantiate_phy(top)
+
+class onegbe_vcu118(onegbe):
+    def initialize(self):
+        self.typecode = TYPECODE_ETHCORE
+        self.add_source('onegbe/*.v')
+        self.add_source('onegbe/*.xci')
+        self.add_source('onegbe/*.coe')
+
+        self.provides = ['ethernet']
+        if (not self.dis_cpu_tx) and (not self.dis_cpu_tx):
+            self.provides += ['cpu_ethernet']
+
+        self.refclk_freq = 625.0
+
+    def _instantiate_phy(self, top):
+        gbe_pcs = top.get_instance(entity='gig_ethernet_pcs_pma_sgmii_lvds', name=self.fullname+'_pcs_pma')
+        
+        gbe_pcs.add_port('refclk625_p', self.fullname+'_refclk625_p', dir='in', parent_port=True)
+        gbe_pcs.add_port('refclk625_n', self.fullname+'_refclk625_n', dir='in', parent_port=True)
+        gbe_pcs.add_port('clk125_out', 'gbe_userclk2_out')
+        gbe_pcs.add_port('clk625_out', '')
+        gbe_pcs.add_port('clk312_out', '')
+        gbe_pcs.add_port('idelay_rdy_out', '')
+        gbe_pcs.add_port('rst_125_out', '')
+        gbe_pcs.add_port('gmii_isolate', '')
+        gbe_pcs.add_port('an_interrupt', '')
+        gbe_pcs.add_port('status_vector', '')
+
+        gbe_pcs.add_port('txn', self.fullname+'_tx_n', dir='out', parent_port=True)
+        gbe_pcs.add_port('txp', self.fullname+'_tx_p', dir='out', parent_port=True)
+        gbe_pcs.add_port('rxn', self.fullname+'_rx_n', dir='in',  parent_port=True)
+        gbe_pcs.add_port('rxp', self.fullname+'_rx_p', dir='in',  parent_port=True)
+
+        gbe_pcs.add_port('mmcm_locked_out', 'gbe_mmcm_locked_out')
+        gbe_pcs.add_port('sgmii_clk_r', '')
+        gbe_pcs.add_port('sgmii_clk_f', '')
+        gbe_pcs.add_port('sgmii_clk_en', '')
+        gbe_pcs.add_port('gmii_txd',   self.fullname+'_mac_gmii_tx_data', width=8)
+        gbe_pcs.add_port('gmii_tx_en', self.fullname+'_mac_gmii_tx_en')
+        gbe_pcs.add_port('gmii_tx_er', self.fullname+'_mac_gmii_tx_er')
+        gbe_pcs.add_port('gmii_rxd',   self.fullname+'_mac_gmii_rx_data', width=8)
+        gbe_pcs.add_port('gmii_rx_dv', self.fullname+'_mac_gmii_rx_dvld')
+        gbe_pcs.add_port('gmii_rx_er', self.fullname+'_mac_gmii_rx_er')
+        gbe_pcs.add_port('configuration_vector', '5\'b10000', parent_sig=False)
+        gbe_pcs.add_port('speed_is_10_100', '0', parent_sig=False)
+        gbe_pcs.add_port('speed_is_100', '0', parent_sig=False)
+        gbe_pcs.add_port('reset', 'sys_rst')
+        gbe_pcs.add_port('signal_detect', '1', parent_sig=False)
+
+        gbe_pcs.add_port('an_adv_config_vector', '16\'b1000100000000001', parent_sig=False)
+        gbe_pcs.add_port('an_restart_config', '1\'b0', parent_sig=False)
+
+        top.add_port('phy_rst_n', dir='out', width=0)
+        top.assign_signal('phy_rst_n', '~sys_rst')
+        top.add_port('phy_pdown_n', dir='out', width=0)
+        top.assign_signal('phy_pdown_n', '1\'b1')
+
+    def gen_constraints(self):
+        consts = []
+        consts += [PortConstraint(self.fullname+'_tx_p', 'gbe_phy_sgmii_out_p')]
+        consts += [PortConstraint(self.fullname+'_tx_n', 'gbe_phy_sgmii_out_n')]
+        consts += [PortConstraint(self.fullname+'_rx_p', 'gbe_phy_sgmii_in_p')]
+        consts += [PortConstraint(self.fullname+'_rx_n', 'gbe_phy_sgmii_in_n')]
+        consts += [PortConstraint('phy_rst_n', 'gbe_phy_rst_n')]
+        consts += [PortConstraint('phy_pdown_n', 'gbe_phy_power_down_n')]
+        consts += [PortConstraint(self.fullname+'_refclk625_p', 'gbe_phy_sgmii_clk_p')]
+        consts += [PortConstraint(self.fullname+'_refclk625_n', 'gbe_phy_sgmii_clk_n')]
+        consts += [ClockConstraint(self.fullname+'_refclk625_p', name='onegbe_clk', freq=self.refclk_freq)]
+        return consts
+        
+      
+    
+
+
+class onegbe_snap(onegbe):
+    def initialize(self):
+        self.typecode = TYPECODE_ETHCORE
+        self.add_source('onegbe/*.v')
+        self.add_source('onegbe/*.xci')
+        self.add_source('onegbe/*.coe')
+
+        self.provides = ['ethernet']
+        if (not self.dis_cpu_tx) and (not self.dis_cpu_tx):
+            self.provides += ['cpu_ethernet']
+
+        if self.platform.name in ['snap2']:
+            self.use_lvds = True
+        else:
+            self.use_lvds = False
+
+        if self.platform.name == 'snap2':
+            self.refclk_freq = 625.0
+        else:
+            self.refclk_freq = 125.0
+
+
+    def _instantiate_phy(self, top):
         ############ PCS / PMA (Xilinx Core) #############
         if self.use_lvds:
            gbe_pcs = top.get_instance(entity='gig_ethernet_pcs_pma_sgmii_lvds', name=self.fullname+'_pcs_pma')
