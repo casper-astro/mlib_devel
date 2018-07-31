0a44fb5eca6de0eb86d17849195258d9eb989da9
jack <jack@berkeley>
2018-07-31 18:02:05 +0200

    Go back to the ska-sa/devel wbs_arbiter for SKARAB
    Some flakiness emerging in tutorials using new code.

--- jasper_library/yellow_blocks/skarab.py
+++ jasper_library/yellow_blocks/skarab.py
@@ -5,7 +5,7 @@ from constraints import ClockConstraint, PortConstraint, RawConstraint
 class skarab(YellowBlock):
     def initialize(self):
         self.add_source('infrastructure')
-        self.add_source('wbs_arbiter')
+        self.add_source('wbs_arbiter_skarab')
         pass
 
     def modify_top(self,top):
