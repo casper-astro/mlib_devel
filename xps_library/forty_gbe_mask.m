0e4814dea8ac55fb44ee952fa9af9ff8ddae2b55
amitbansod <bansod.amit@gmail.com>
2018-08-02 21:21:42 +0200

    periodic commit

--- xps_library/forty_gbe_mask.m
+++ xps_library/forty_gbe_mask.m
@@ -115,7 +115,7 @@ for s=1:num_qsfp_ports
    reuse_block(cursys, strcat('rx_vld_or_out_',num2str(s),'_pipe'), 'casper_library_delays/pipeline', ...
             'latency', '2', 'Position', [1750 (s*800)+127 1800 157+(s*800)]);
    reuse_block(cursys, strcat('rx_eof',num2str(s),'_pipe'), 'casper_library_delays/pipeline', ...
-            'latency', '2', 'Position', [1750 (s*800)+177 1800 197+(s*800)]);
+            'latency', '3', 'Position', [1750 (s*800)+177 1800 197+(s*800)]);
   add_line(cursys,strcat('rx_valid',num2str(s),'_pipe/1'),strcat('rx_vld_bus',num2str(s),'/1'));
   add_line(cursys,strcat('rx_vld_bus',num2str(s),'/1'),strcat('rx_vld_or',num2str(s),'/1'));
   add_line(cursys,strcat('rx_vld_bus',num2str(s),'/2'),strcat('rx_vld_or',num2str(s),'/2'));
@@ -198,15 +198,17 @@ catch
     pipe_no_pipe = cursys;
 end
 
-valid_source = 'rx_vld_bus_out';
-
 
 function draw_counter(sys, xpos, ypos, targetname, sourcename)
     ctr_name = [targetname, '_ctr'];
     delay_name = [targetname, '_del'];
+    rst_pipe = [targetname,'_r_pipe'];
+    ctr_pipe = [targetname,'_c_pipe'];
     delete_block_lines_s([sys, '/', targetname]);
     delete_block_lines_s([sys, '/', ctr_name]);
     delete_block_lines_s([sys, '/', delay_name]);
+    delete_block_lines_s([sys, '/', ctr_pipe]);
+    delete_block_lines_s([sys, '/', rst_pipe]);
     draw_block = false;
     try
         if ((strcmp(get_param(sys, 'debug_en_all'), 'on') == 1)) && ...
@@ -214,31 +216,39 @@ function draw_counter(sys, xpos, ypos, targetname, sourcename)
             draw_block = true;
         end
     catch
-        if strcmp(get_param(sys, targetname), 'on') == 1
-            draw_block = true;
-        end
+%         if strcmp(get_param(sys, targetname), 'on') == 1
+%             draw_block = true;
+%         end
     end
     if draw_block
         reuse_block(sys, delay_name, 'xbsIndex_r4/Delay', ...
             'reg_retiming', 'on', 'latency', '1', ...
-            'Position', [xpos ypos xpos+20 ypos+25]);
+            'Position', [xpos ypos+15 xpos+20 ypos+35]);
+          reuse_block(sys, rst_pipe, 'casper_library_delays/pipeline', ...
+             'latency', '3', 'Position', [xpos+60 ypos+15 xpos+100 ypos+35]);
         reuse_block(sys, targetname, 'xps_library/Memory/software_register', ...
             'io_dir', 'To Processor', 'arith_types', '0', ...
             'io_delay', '1', 'bitwidths', debug_ctr_width, ...
-            'sim_port', 'no', 'Position', [xpos+150 ypos xpos+200 ypos+20]);
+            'sim_port', 'no', 'Position', [xpos+360 ypos xpos+410 ypos+20]);
         reuse_block(sys, ctr_name, 'xbsIndex_r4/Counter', ...
             'arith_type', 'Unsigned', 'n_bits', debug_ctr_width, ...
             'explicit_period', 'on', 'period', '1', ...
             'use_behavioral_HDL', 'on', 'rst', 'on', 'en', 'on', ...
-            'Position', [xpos+50 ypos xpos+100 ypos+45]);
+            'Position', [xpos+200 ypos xpos+250 ypos+45]);
+         reuse_block(sys, ctr_pipe, 'casper_library_delays/pipeline', ...
+             'latency', '3', 'Position', [xpos+300 ypos xpos+320 ypos+45]);
         add_line_s(sys, 'debug_rst/1', [delay_name, '/1']);
-        add_line_s(sys, [delay_name, '/1'], [ctr_name, '/1']);
+        add_line_s(sys, [delay_name, '/1'], [rst_pipe, '/1']);
+        add_line_s(sys, [rst_pipe, '/1'], [ctr_name, '/1']);
         add_line_s(sys, [sourcename, '/1'], [ctr_name, '/2']);
-        add_line_s(sys, [ctr_name, '/1'], [targetname, '/1']);
+        add_line_s(sys, [ctr_name, '/1'], [ctr_pipe, '/1']);
+        add_line_s(sys, [ctr_pipe, '/1'], [targetname, '/1']);
     else
         delete_block_s([sys, '/', delay_name]);
         delete_block_s([sys, '/', targetname]);
         delete_block_s([sys, '/', ctr_name]);
+        delete_block_s([sys, '/', ctr_pipe]);
+        delete_block_s([sys, '/', rst_pipe]);
     end
 end
 
@@ -252,6 +262,7 @@ function draw_errorcounter(sys, xpos, ypos, targetname, frame_len, sourceeof, so
     vld_pipe = [targetname,'_v_pipe'];
     eof_pipe = [targetname,'_e_pipe'];
     bad_pipe = [targetname,'_b_pipe'];
+    errchk_name_pipe = [targetname, '_errchk_pipe'];
     delete_block_lines_s([sys, '/', errchk_name]);
     delete_block_lines_s([sys, '/', nobad_name]);
     delete_block_lines_s([sys, '/', targetname]);
@@ -262,6 +273,7 @@ function draw_errorcounter(sys, xpos, ypos, targetname, frame_len, sourceeof, so
     delete_block_lines_s([sys, '/', vld_pipe]);
     delete_block_lines_s([sys, '/', eof_pipe]);
     delete_block_lines_s([sys, '/', bad_pipe]);
+    delete_block_lines_s([sys, '/', errchk_name_pipe]);
     draw_block = false;
     try
         if ((strcmp(get_param(sys, 'debug_en_all'), 'on') == 1)) && ...
@@ -277,33 +289,35 @@ function draw_errorcounter(sys, xpos, ypos, targetname, frame_len, sourceeof, so
         reuse_block(sys, delay_name, 'xbsIndex_r4/Delay', ...
             'reg_retiming', 'on', 'latency', '1', ...
             'Position', [xpos ypos xpos+20 ypos+25]);
+          reuse_block(sys, rst_pipe, 'casper_library_delays/pipeline', ...
+             'latency', '3', 'Position', [xpos+60 ypos-25 xpos+100 ypos]);
         reuse_block(sys, targetname, 'xps_library/Memory/software_register', ...
             'io_dir', 'To Processor', 'arith_types', '0', ...
             'io_delay', '1', 'bitwidths', debug_ctr_width, ...
-            'sim_port', 'no', 'Position', [xpos+150 ypos xpos+200 ypos+20]);
+            'sim_port', 'no', 'Position', [xpos+360 ypos xpos+410 ypos+20]);
         reuse_block(sys, errchk_name, 'casper_library_communications/frame_len_checker', ...
             'frame_len', frame_len, ...
             'Position', [xpos-100, ypos, xpos, ypos+45]);
           reuse_block(sys, errchk_name_pipe, 'casper_library_delays/pipeline', ...
-             'latency', '3', 'Position', [xpos+10 ypos xpos+30 ypos+45]);
+             'latency', '3', 'Position', [xpos+60 ypos+20 xpos+100 ypos+45]);
         set_param([sys, '/', errchk_name], 'LinkStatus', 'inactive');
         reuse_block(sys, nobad_name, 'xbsIndex_r4/Constant', ...
             'arith_type', 'boolean', 'const', '0', 'explicit_period', 'on', 'period', '1', ...
-            'Position', [xpos-150 ypos+30 xpos-130 ypos+45]);
+            'Position', [xpos-160 ypos+30 xpos-120 ypos+45]);
         reuse_block(sys, ctr_name, 'xbsIndex_r4/Counter', ...
             'arith_type', 'Unsigned', 'n_bits', debug_ctr_width, 'explicit_period', 'on', ...
             'period', '1', 'use_behavioral_HDL', 'on', 'rst', 'on', 'en', 'on', ...
-            'Position', [xpos+50 ypos xpos+100 ypos+45]);
+            'Position', [xpos+200 ypos xpos+250 ypos+45]);
          reuse_block(sys, ctr_pipe, 'casper_library_delays/pipeline', ...
-             'latency', '3', 'Position', [xpos+70 ypos xpos+130 ypos+45]);
+             'latency', '3', 'Position', [xpos+300 ypos xpos+320 ypos+45]);
         reuse_block(sys, vld_pipe, 'casper_library_delays/pipeline', ...
-             'latency', '3', 'Position', [xpos+30 ypos xpos+40 ypos+45]);
+             'latency', '3', 'Position', [xpos-110 ypos+5 xpos-105 ypos+15]);
          reuse_block(sys, eof_pipe, 'casper_library_delays/pipeline', ...
-             'latency', '3', 'Position', [xpos+30 ypos+5 xpos+40 ypos+45+5]);
+             'latency', '3', 'Position', [xpos-110 ypos+20 xpos-105 ypos+30]);
           reuse_block(sys, rst_pipe, 'casper_library_delays/pipeline', ...
-             'latency', '3', 'Position', [xpos+10 ypos+5 xpos+20 ypos+40]);
+             'latency', '3', 'Position', [xpos+60 ypos-25 xpos+100 ypos]);
           reuse_block(sys, bad_pipe, 'casper_library_delays/pipeline', ...
-             'latency', '3', 'Position', [xpos+100 ypos+5 xpos+120 ypos+40]);
+             'latency', '3', 'Position',  [xpos-110 ypos+35 xpos-105 ypos+45]);
         add_line_s(sys, [sourcevalid, '/1'], [vld_pipe, '/1']);
         add_line_s(sys, [vld_pipe, '/1'], [errchk_name, '/1']);
         add_line_s(sys, [sourceeof, '/1'], [eof_pipe, '/1']);
@@ -328,6 +342,7 @@ function draw_errorcounter(sys, xpos, ypos, targetname, frame_len, sourceeof, so
         delete_block_s([sys, '/', vld_pipe]);
         delete_block_s([sys, '/', eof_pipe]);
         delete_block_s([sys, '/', bad_pipe]);
+        delete_block_s([sys, '/', errchk_name_pipe]);
     end
 end
 
@@ -415,55 +430,55 @@ end
     % tx error counter
     starty = starty + 50;
     draw_errorcounter(cursys, 400, starty, strcat('txerrctr',num2str(s)), get_param(cursys, 'txerrctr_len'), strcat('tx_end_of_frame',num2str(s)), strcat('tx_valid',num2str(s)));
-% 
-%     % tx overflow counter
-%     starty = starty + 50;
-%     draw_counter(cursys, 400, starty, 'txofctr', clear_name([pipe_no_pipe, '_tx_overflow']));
-% 
-%     % tx full counter
-%     starty = starty + 50;
-%     draw_counter(cursys, 400, starty, 'txfullctr', clear_name([pipe_no_pipe, '_tx_afull']));
-% 
-%     % tx valid counter
-%     starty = starty + 50;
-%     draw_counter(cursys, 400, starty, 'txvldctr', 'tx_valid')
+
+    % tx overflow counter
+    starty = starty + 50;
+    draw_counter(cursys, 400, starty, strcat('txofctr',num2str(s)), strcat('tx_overflow',num2str(s),'_pipe'));
+
+    % tx full counter
+    starty = starty + 50;
+    draw_counter(cursys, 400, starty, strcat('txfullctr',num2str(s)),  strcat('tx_afull',num2str(s),'_pipe'));
+
+    % tx valid counter
+    starty = starty + 50;
+    draw_counter(cursys, 400, starty, strcat('txvldctr',num2str(s)), strcat('tx_valid',num2str(s),'_pipe'));
 % 
 %     % draw all the tx registers
 % 
     % rx counter
     starty = 130 + s*600;
     % draw_rxcounter(cursys, 1400, starty, 'rxctr', clear_name([cursys, '_rx_end_of_frame']), valid_source)
-    draw_rxcounter(cursys, 1600, starty, strcat('rxctr',num2str(s)), strcat('rx_eof',num2str(s),'_pipe'), strcat('rx_vld_or_out_',num2str(s),'_pipe'))
-% 
-%     % rx error counter
-%     starty = starty + 50;
-%     % draw_errorcounter(cursys, 1400, starty, 'rxerrctr', get_param(cursys, 'rxerrctr_len'), clear_name([cursys, '_rx_end_of_frame']), valid_source);
-%     draw_errorcounter(cursys, 1400, starty, 'rxerrctr', get_param(cursys, 'rxerrctr_len'), clear_name([pipe_no_pipe, '_rx_end_of_frame']), valid_source);
-% 
-%     % rx overflow counter
-%     starty = starty + 50;
-%     % draw_counter(cursys, 1400, starty, 'rxofctr', clear_name([cursys, '_rx_overrun']));
-%     draw_counter(cursys, 1400, starty, 'rxofctr', clear_name([pipe_no_pipe, '_rx_overrun']));
-% 
-%     % rx bad frame counter
-%     starty = starty + 50;
-%     % draw_counter(cursys, 1400, starty, 'rxbadctr', clear_name([cursys, '_rx_bad_frame']));
-%     draw_counter(cursys, 1400, starty, 'rxbadctr', clear_name([pipe_no_pipe, '_rx_bad_frame']));
-% 
-%     % rx valid counter
-%     starty = starty + 50;
-%     draw_counter(cursys, 1400, starty, 'rxvldctr', valid_source);
-% 
-%     % rx eof counter
-%     starty = starty + 50;
-%     % draw_counter(cursys, 1400, starty, 'rxeofctr', clear_name([cursys, '_rx_end_of_frame']));
-%     draw_counter(cursys, 1400, starty, 'rxeofctr', clear_name([pipe_no_pipe, '_rx_end_of_frame']));
-% 
-%     % rx snapshot
-%     forty_gbe_mask_draw_rxsnap(cursys, pipe_no_pipe);
-% 
-%     % tx snapshot
-%     forty_gbe_mask_draw_txsnap(cursys, pipe_no_pipe);
+    draw_rxcounter(cursys, 1600, starty, strcat('rxctr',num2str(s)), strcat('rx_eof',num2str(s),'_pipe'), strcat('rx_vld_or_out_',num2str(s),'_pipe'));
+
+    % rx error counter
+    starty = starty + 100;
+    % draw_errorcounter(cursys, 1400, starty, 'rxerrctr', get_param(cursys, 'rxerrctr_len'), clear_name([cursys, '_rx_end_of_frame']), valid_source);
+    draw_errorcounter(cursys, 1800, starty, strcat('rxerrctr',num2str(s)), get_param(cursys, 'rxerrctr_len'), strcat('rx_eof',num2str(s),'_pipe'), strcat('rx_vld_or_out_',num2str(s),'_pipe'));
+
+    % rx overflow counter
+    starty = starty + 50;
+    % draw_counter(cursys, 1400, starty, 'rxofctr', clear_name([cursys, '_rx_overrun']));
+    draw_counter(cursys, 1800, starty, strcat('rxofctr',num2str(s)), strcat('rx_overrun',num2str(s),'_pipe'));
+
+    % rx bad frame counter
+    starty = starty + 50;
+    % draw_counter(cursys, 1400, starty, 'rxbadctr', clear_name([cursys, '_rx_bad_frame']));
+    draw_counter(cursys, 1800, starty, strcat('rxbadctr',num2str(s)), strcat('rx_bad_frame',num2str(s),'_pipe'));
+
+    % rx valid counter
+    starty = starty + 50;
+    draw_counter(cursys, 1800, starty, strcat('rxvldctr',num2str(s)),  strcat('rx_vld_or_out_',num2str(s),'_pipe'));
+
+    % rx eof counter
+    starty = starty + 50;
+    % draw_counter(cursys, 1400, starty, 'rxeofctr', clear_name([cursys, '_rx_end_of_frame']));
+    draw_counter(cursys, 1800, starty, strcat('rxeofctr',num2str(s)), strcat('rx_end_of_frame',num2str(s),'_pipe'));
+
+    %rx snapshot
+    forty_gbe_mask_draw_rxsnap(cursys, num2str(s), strcat(num2str(s),'_pipe'),strcat('rx_vld_or_out_',num2str(s),'_pipe'));
+
+    % tx snapshot
+    %forty_gbe_mask_draw_txsnap(cursys, strcat('pipeline',num2str(s)));
 end
 
 
