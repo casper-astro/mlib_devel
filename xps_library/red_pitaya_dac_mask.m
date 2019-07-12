ea91307638723843c663a3a3064c076c7dd5c1a3
talonmyburgh <myburgh.talon@gmail.com>
2019-07-12 16:43:30 +0200

    Added support for 14-bit Red Pitaya ADC and DAC: - Renamed the red_pitaya.yaml file to red_pitaya_10.yaml. - Added .yaml file for 14-bit Red Pitaya platform. - Modified Red Pitaya ADC and DAC masks to allow for 10-bit or 14-bit selection. - Modified Red Pitaya platform YellowBlock for 10-bit or 14-bit selection. - Modified Red Pitaya ADC and DAC YellowBlocks for 10-bit or 14-bit selection.

--- xps_library/red_pitaya_dac_mask.m
+++ xps_library/red_pitaya_dac_mask.m
@@ -4,6 +4,8 @@ function red_pitaya_dac_mask(blk)
 
 myname = blk;
 % get hardware platform from XSG block
+adc_blk = find_system(myname);
+bits_str=convertCharsToStrings(get_param(adc_blk{1},'bits'));
 
 gateway_outs = find_system(myname, 'searchdepth', 1, 'FollowLinks', 'on', 'lookundermasks', 'all', 'masktype', 'Xilinx Gateway Out Block');
 for i =1:length(gateway_outs)
@@ -25,8 +27,18 @@ for i =1:length(gateway_outs)
     end
 end
 
-gateway_ins =find_system(myname, 'searchdepth', 1, 'FollowLinks', 'on', 'lookundermasks', 'all', 'masktype', 'Xilinx Gateway In Block');
-for i =1:length(gateway_ins)
-    gw = gateway_ins{i};       
-end
+casts = find_system(myname, 'searchdepth', 1, 'FollowLinks', 'on', 'lookundermasks', 'all', 'masktype', 'Xilinx Type Converter Block');
+for i =1:length(casts)
+    cst = casts{i};
+    if regexp(get_param(cst, 'Name'), '(convert_dac0_data_i_in)$')
+        set_param(cst, 'n_bits', bits_str); 
+    elseif regexp(get_param(cst, 'Name'), '(convert_dac1_data_q_in1)$')
+        set_param(cst, 'n_bits', bits_str);      
+    elseif regexp(get_param(cst, 'Name'), '(convert_dac_data_valid_in)$')
 
+    elseif regexp(get_param(cst, 'Name'), '(convert_dac_reset_in)$')
+	
+    else 
+        error(['Unknown gateway name: ', cst]);
+    end
+end
