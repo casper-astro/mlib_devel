e1545139c11931c4e3eee0ec02703af624d16e36
amitbansod <bansod.amit@gmail.com>
2019-07-12 22:55:36 +0200

    microblaze changes to support VCU128
    VCU128 fpga family: virtexuplusHBM

--- jasper_library/yellow_blocks/microblaze.py
+++ jasper_library/yellow_blocks/microblaze.py
@@ -9,7 +9,7 @@ class microblaze(YellowBlock):
         if self.platform.name in ['snap2']:
             self.memfile= 'executable_no_xadc.mem'
             self.blkdiagram = 'microblaze_wb_no_xadc.tcl'
-        elif self.platform.name in ['vcu118']:
+        elif self.platform.name in ['vcu118','vcu128']:
             self.memfile = 'executable_us_plus.mem'
             self.blkdiagram = 'microblaze_wb_us_plus.tcl'
             self.include_spi_ports = False
