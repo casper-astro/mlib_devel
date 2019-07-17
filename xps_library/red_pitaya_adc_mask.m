b20d48d8a6b9db8edcb0aeac4140ee25c4e10d2b
hpw1 <hpw1@casper1.lan>
2019-07-17 21:39:53 +0200

    Removed reinterpret and cast in ADC yellow block The reinterpret and cast in the ADC yellow block has been removed. This has been tested and still works.

--- xps_library/red_pitaya_adc_mask.m
+++ xps_library/red_pitaya_adc_mask.m
@@ -5,7 +5,8 @@ function red_pitaya_adc_mask(blk)
 myname = blk;
 % get hardware platform from XSG block
 adc_blk = find_system(myname);
-bits_str=convertCharsToStrings(get_param(adc_blk{1},'bits'));
+bit_num=str2double(get_param(adc_blk{1},'bits'));
+binarypoint=0; 
 
 gateway_outs = find_system(myname, 'searchdepth', 1, 'FollowLinks', 'on', 'lookundermasks', 'all', 'masktype', 'Xilinx Gateway Out Block');
 for i =1:length(gateway_outs)
@@ -25,31 +26,18 @@ for i =1:length(gateway_ins)
         toks = regexp(get_param(gw, 'Name'), '(adc_data_val_out)$', 'tokens');
         set_param(gw, 'Name', clear_name([myname, '_', toks{1}{1}]));
     elseif regexp(get_param(gw, 'Name'), '(adc0_data_i_out)$')
-        set_param(gw, 'n_bits', bits_str);
+        set_param(gw, 'bin_pt', num2str(binarypoint));        
+        set_param(gw, 'n_bits', num2str(bit_num));    
         toks = regexp(get_param(gw, 'Name'), '(adc0_data_i_out)$', 'tokens');
-        set_param(gw, 'Name', clear_name([myname, '_', toks{1}{1}]));
+        set_param(gw, 'Name', clear_name([myname, '_', toks{1}{1}]));      
     elseif regexp(get_param(gw, 'Name'), '(adc1_data_q_out)$')
-        set_param(gw, 'n_bits', bits_str);
+        set_param(gw, 'bin_pt', num2str(binarypoint));
+        set_param(gw, 'n_bits', num2str(bit_num));     
         toks = regexp(get_param(gw, 'Name'), '(adc1_data_q_out)$', 'tokens');
-        set_param(gw, 'Name', clear_name([myname, '_', toks{1}{1}]));
+        set_param(gw, 'Name', clear_name([myname, '_', toks{1}{1}]));      
     else
         error(['Unknown gateway name: ', gw]);
     end
 end
 
-casts = find_system(myname, 'searchdepth', 1, 'FollowLinks', 'on', 'lookundermasks', 'all', 'masktype', 'Xilinx Type Converter Block');
-for i =1:length(casts)
-    cst = casts{i};
-    if regexp(get_param(cst, 'Name'), '(Convert6)$')
-        set_param(cst, 'n_bits', bits_str); 
-    elseif regexp(get_param(cst, 'Name'), '(Convert18)$')
-        set_param(cst, 'n_bits', bits_str);      
-    elseif regexp(get_param(cst, 'Name'), '(Convert2)$')
-
-    elseif regexp(get_param(cst, 'Name'), '(convert_adc_reset_in)$')
-	
-    else 
-        error(['Unknown gateway name: ', cst]);
-    end
-end
 
