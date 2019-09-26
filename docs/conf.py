3e9626455fa097cf2ac5597eba87c3f1ba8eb15f
Morag <mbrown@ska.ac.za>
2019-09-26 10:05:05 +0200

    added sphinx_markdown_tables to extensions to render md tables

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,8 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
