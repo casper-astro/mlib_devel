73463a9444b4314a1ef48de8188e324815c32bd2
Jack <jack@realtimeradio.co.uk>
2020-08-14 14:51:41 +0000

    syntax fix

--- jasper_library/yellow_blocks/onehundred_gbe.py
+++ jasper_library/yellow_blocks/onehundred_gbe.py
@@ -28,7 +28,7 @@ class onehundredgbe_usplus(onehundred_gbe):
             Register('gmac_reg_local_ip_address',     mode='rw', offset=0x14, default_val=self.fab_ip),
             Register('gmac_reg_gateway_ip_address',   mode='rw', offset=0x18, default_val=self.fab_gate),
 
-            Register('gmac_reg_netmask',              mode='rw', offset=0x1C) #100G core doesn't have this...
+            Register('gmac_reg_netmask',              mode='rw', offset=0x1C), #100G core doesn't have this...
             Register('gmac_reg_multicast_ip_address', mode='rw', offset=0x20, default_val=0),
             Register('gmac_reg_multicast_ip_mask',    mode='rw', offset=0x24, default_val=0),
             Register('gmac_reg_bytes_rdy',            mode='rw', offset=0x28),
