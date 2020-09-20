9308e67150a6334f6ff71917e69f6f4570571938
Jack <jack@realtimeradio.co.uk>
2020-09-20 12:07:22 +0000

    Fixes

--- jasper_library/hdl_sources/spi_master/wb_spi_master.v
+++ jasper_library/hdl_sources/spi_master/wb_spi_master.v
@@ -1,3 +1,7 @@
+// NB: This core requires a WB timeout allowance
+// of ~2^NCLKDIVBITS * (NBITS + 2)
+
+
 module wb_spi_master#(
     parameter NBITS = 24,
     parameter NCLKDIVBITS = 5,
@@ -41,28 +45,38 @@ module wb_spi_master#(
   reg [31:0] spi_event_count = 32'b0;
 
   always @(posedge wb_clk_i) begin
-    // defaults
+    // strobes
     wb_ack <= 1'b0;
     spi_trigger <= 1'b0;
     spi_ack <= 1'b0;
     if (wb_rst_i) begin
+      wait_for_spi <= 1'b0;
+      spi_trigger<= 1'b0;
     end else begin
+      // Ack SPI transactions outside of WB transaction
+      // clause. This means that if the WB bus times out,
+      // the SPI command could still succeed and update
+      // the SPI read data.
+      // Reading back address 0 will verify if the
+      // SPI transaction is still pending and/or has
+      // completed but hasn't been acknowledged
+      if (wait_for_spi && spi_dvld) begin
+        wait_for_spi <= 1'b0;
+        spi_event_count <= spi_event_count + 1'b1;
+        spi_dout_reg[NBITS-1:0] <= spi_dout;
+        spi_ack <= 1'b1;
+      end
       if (wb_stb_i && wb_cyc_i && !wb_ack) begin
-        if (wait_for_spi && spi_dvld) begin
-          spi_event_count <= spi_event_count + 1'b1;
-          wait_for_spi <= 1'b0;
-          spi_dout_reg[NBITS-1:0] <= spi_dout;
-          spi_ack <= 1'b1;
-          wb_ack <= 1'b1;
-        end else if (wb_we_i) begin
+        if (wb_we_i) begin
           case (wb_adr_i[4:2])
             // Address 0 has CS_N in byte 0
             // Writing it causes a trigger
             0:  begin
-              wait_for_spi <= 1'b1;
+              wb_ack <= 1'b1;
               if (wb_sel_i[0]) begin
                 cs_n_reg <= wb_dat_i[7:0];
                 spi_trigger <= 1'b1;
+                wait_for_spi <= 1'b1;
               end
             end
             1:  begin
@@ -85,22 +99,31 @@ module wb_spi_master#(
             end
           endcase
         end else begin // if (wb_we_i)
-          wb_ack <= 1'b1;
           case (wb_adr_i[4:2])
             0: begin
-              wb_data_out_reg <= {24'b0, cs_n_reg};
+              // Use the MSB for wait_for_spi.
+              // If everything is working (not timing out)
+              // You should never see this high (since any
+              // SPI writes don't return until the transaction
+              // is complete)
+              wb_data_out_reg <= {wait_for_spi, spi_dvld, 22'b0, cs_n_reg};
+              wb_ack <= 1'b1;
             end
             1: begin
               wb_data_out_reg <= din_reg;
+              wb_ack <= 1'b1;
             end
             2: begin
               wb_data_out_reg[NBITS-1:0] <= spi_dout_reg[NBITS-1:0];
+              wb_ack <= 1'b1;
             end
             3: begin
               wb_data_out_reg <= spi_event_count;
+              wb_ack <= 1'b1;
             end
             default: begin
               wb_data_out_reg <= 32'b0;
+              wb_ack <= 1'b1;
             end
           endcase
         end // if not (wb_we_i)
@@ -116,7 +139,7 @@ module wb_spi_master#(
     .NCLKDIVBITS(NCLKDIVBITS),
     .NBITS(NBITS),
     .NCSBITS(NCSBITS)
-    ) uut (
+    ) spi_master_inst (
     .clk(wb_clk_i),
     .cs_n_in(cs_n_reg[NCSBITS-1:0]),
     .din(din_reg[NBITS-1:0]),
