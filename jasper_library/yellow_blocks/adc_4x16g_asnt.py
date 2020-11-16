4c27ebec6b2b0b5bae6345667b4d7998e07d1e4b
liuweiseu <liuwei_seu@seu.edu.cn>
2020-11-15 17:00:38 -0800

    update adc_4x16g_asnt.py, adding ADC IP core in the py file.

--- jasper_library/yellow_blocks/adc_4x16g_asnt.py
+++ jasper_library/yellow_blocks/adc_4x16g_asnt.py
@@ -6,10 +6,17 @@ from math import ceil, floor
 
 class adc_4x16g_asnt(YellowBlock):
     def initialize(self):
-    
+        #add the adc4x16g IP 
+        self.ips = [{'path':'%s/adc4x16g/ip_repo_0' % env['HDL_ROOT'],
+                     'name':'adc4x16g_core',
+                     'vendor':'xilinx.com',
+                     'library':'user',
+                     'version':'1.1',
+                    }]
     
     def modify_top(self,top):
-        module = 'adc_4x16g_asnt_interface'
+        module = 'adc4x16g_adc4x16g_core_0_0'
+        inst = top.get_instance(entity=module, 'adc4x16g_adc4x16g_inst%d'%self.channel_sel)
         
     def gen_constraints(self):
         cons = []
