45d74eb67c5cf95540afc3a5cf4bdc2f4958bbb6
Mitch Burnett <mitch.burnett@byu.edu>
2021-04-09 11:11:05 -0600

    Add support and simple snapshot test for the htg zrf16 49dr board
    The `test_htg` model is able to build out for the 49dr part. But fails for the
    29dr version. This is most liekly due to the lack of implementing out support
    for the gen 1 rfsocs in the xps library mask and rfdc yellow block. Need to
    investigate. Will go back and extend/improve when also adding support for the
    rfsocs on the zcu208/zcu111 as they are dual tile architectures that also needs
    to be supported in addition to any gen 1 quirks in the xps library mask.
    
    The zrf16 made by htg has two versions of the board. One with a 1st gen 29dr
    rfsoc and the other that is a 3rd gen 49dr rfsoc. The boards I have worked on
    are almost identical in layout except for the PS DDR4 configuration on the 49dr
    is different.
    
    The approach here is then to have a single zrf16 platform to instance that
    selects between the two revisions of the board. This seemd to make the most
    sense as to try to achieve a way to support to boards without maintaining two
    separate yellow blocks.
    
    I had also explored how to get a single platform configuration file (.yaml) with
    having a list of fpga part numbers in the 'fpga' dict value and have the
    toolflow determine the correct one and update the platform dictionary based on
    the `hw_sys` selected  but that proved to be a little to difficult at the moment
    with how the platform/xsg blocks work together and instance each other. It turns
    out the xsg block runs first preparing the targeted platform yellow block. Then
    all other yellow blocks run.
    
    Even if the xsg yellow block was edited to determine the correct part based on
    hw_sys the toolflow is passing the platform dictonary to the yellow blocks and
    the xsg version that updated `fpga` key in the platform dict will get passed to
    the final platform (e.g., zrf16) but not to the other yellow blocks as a copy is
    not sent back up.
    
    But, here a single `zrf16` yellow block is maintained by a factory method in the
    `zrf16_29/49dr` yellow blocks that end up instancing the common `zrf16`
    implementation.

---
+++ jasper_library/yellow_blocks/zrf16.py
@@ -0,0 +1,131 @@
+from .yellow_block import YellowBlock
+from clk_factors import clk_factors
+from constraints import ClockConstraint, ClockGroupConstraint, PortConstraint, RawConstraint
+
+class zrf16(YellowBlock):
+    def initialize(self):
+        # TODO need any clocking infrastrucutre block - most likely can repurpose zcu216 but for htg clock pins
+        self.add_source('infrastructure/zcu216_clk_infrastructure.sv')
+        self.add_source('utils/cdc_synchroniser.vhd')
+
+        self.blkdesign = '{:s}_base'.format(self.platform.conf['name'])
+
+        # TODO: need new provides and figure the extent to provide these, because as the documentation says
+        # if these lines aren't here the the toolflow breaks.
+        self.provides.append(self.clk_src)
+        self.provides.append(self.clk_src+'90')
+        self.provides.append(self.clk_src+'180')
+        self.provides.append(self.clk_src+'270')
+        self.provides.append(self.clk_src+'_rst')
+
+        # TODO: Need to add this? Is it a bug that `axi4lite_interconnect.py` does not make a `requires` call
+        self.provides.append('axil_clk')    # from zcu216 block design infrastructure
+        self.provides.append('axil_rst_n')  # from zcu216 block desgin infrastructure
+
+        # for the rfsocs it seems appropriate to use the requires/provides for `sysref` and `pl_sysref` for MTS?
+        self.provides.append('pl_sysref') # zcu216 infrastructure provides so rfdc can require
+
+    def modify_top(self, top):
+        top.assign_signal('axil_clk', 'pl_clk0')
+        top.assign_signal('axil_rst', 'peripheral_reset')
+        top.assign_signal('axil_rst_n', 'peripheral_aresetn')
+
+        # TODO: intelligently pass in clock
+        #clkparams = clk_factors(100, self.platform.user_clk_rate)
+        target_freq = 125
+        T_clk = 8 #1/125*1000
+        clkparams = clk_factors(125, 250) # pl_clk from LMK in test environment is 125 MHz, targeting 250 MHz as the required axis data clock from the rfdc
+
+        # TODO: clk infrastructure change to accomodate the high-density global clock package pin inputs has worked -- need to decide what to do
+        inst_infr = top.get_instance('zcu216_clk_infrastructure', 'zcu216_clk_infr_inst')
+        inst_infr.add_parameter('PERIOD', "{:0.3f}".format(T_clk))
+        inst_infr.add_parameter('MULTIPLY', clkparams[0])
+        inst_infr.add_parameter('DIVIDE',   clkparams[1])
+        inst_infr.add_parameter('DIVCLK',   clkparams[2])
+        inst_infr.add_port('pl_clk_p',      "pl_clk_p", dir='in',  parent_port=True)
+        inst_infr.add_port('pl_clk_n',      "pl_clk_n", dir='in',  parent_port=True)
+
+        inst_infr.add_port('adc_clk', 'adc_clk')
+        inst_infr.add_port('adc_clk90', 'adc_clk90')
+        inst_infr.add_port('adc_clk180', 'adc_clk180')
+        inst_infr.add_port('adc_clk270', 'adc_clk270')
+        inst_infr.add_port('mmcm_locked', 'mmcm_locked', dir='out', parent_port=True)
+
+        # instance block design containing mpsoc, and axi protocol converter for casper mermory map (HPM0), axi gpio for software clk104
+        # config (HPM1), and RFDC Xilinx IP (HPM1) that the rfdc yellow block will update based on user configuration
+        bd_inst = top.get_instance(self.blk_design, self.blkdesign + '_inst')
+
+        bd_inst.add_port('m_axi_awaddr',  'M_AXI_awaddr', width=40)
+        bd_inst.add_port('m_axi_awprot',  'M_AXI_awprot', width=3)
+        bd_inst.add_port('m_axi_awvalid', 'M_AXI_awvalid')
+        bd_inst.add_port('m_axi_awready', 'M_AXI_awready')
+        bd_inst.add_port('m_axi_wdata',   'M_AXI_wdata', width=32)
+        bd_inst.add_port('m_axi_wstrb',   'M_AXI_wstrb', width=4)
+        bd_inst.add_port('m_axi_wvalid',  'M_AXI_wvalid')
+        bd_inst.add_port('m_axi_wready',  'M_AXI_wready')
+        bd_inst.add_port('m_axi_bresp',   'M_AXI_bresp', width=2)
+        bd_inst.add_port('m_axi_bvalid',  'M_AXI_bvalid')
+        bd_inst.add_port('m_axi_bready',  'M_AXI_bready')
+        bd_inst.add_port('m_axi_araddr',  'M_AXI_araddr', width=40)
+        bd_inst.add_port('m_axi_arprot',  'M_AXI_arprot', width=3)
+        bd_inst.add_port('m_axi_arvalid', 'M_AXI_arvalid')
+        bd_inst.add_port('m_axi_arready', 'M_AXI_arready')
+        bd_inst.add_port('m_axi_rdata',   'M_AXI_rdata', width=32)
+        bd_inst.add_port('m_axi_rresp',   'M_AXI_rresp', width=2)
+        bd_inst.add_port('m_axi_rvalid',  'M_AXI_rvalid')
+        bd_inst.add_port('m_axi_rready',  'M_AXI_rready')
+
+        bd_inst.add_port('pl_clk0', 'pl_clk0')
+        bd_inst.add_port('peripheral_reset', 'peripheral_reset')
+        bd_inst.add_port('peripheral_aresetn', 'peripheral_aresetn')
+
+
+    def gen_children(self):
+        children = []
+        children.append(YellowBlock.make_block({'tag': 'xps:sys_block', 'board_id': '3', 'rev_maj': '2', 'rev_min': '0', 'rev_rcs': '1'}, self.platform))
+
+        return children
+
+
+    def gen_constraints(self):
+        cons = []
+        cons.append(PortConstraint('pl_clk_p', 'pl_clk_p'))
+        cons.append(RawConstraint('set_property -dict { PACKAGE_PIN AR12 IOSTANDARD LVCMOS18 } [get_ports { mmcm_locked }]'))
+
+        # TODO: can extend to provide other onboard clocks
+        #cons.append(PortConstraint('clk_100_p', 'clk_100_p'))
+        #cons.append(ClockConstraint('clk_100_p','clk_100_p', period=10.0, port_en=True, virtual_en=False, waveform_min=0.0, waveform_max=5.0))
+        #cons.append(ClockGroupConstraint('clk_pl_0', 'clk_100_p', 'asynchronous'))
+        #cons.append(ClockGroupConstraint('clk_100_p', 'clk_pl_0', 'asynchronous'))
+        return cons
+
+
+    def gen_tcl_cmds(self):
+        tcl_cmds = {}
+        tcl_cmds['init'] = []
+        # source tcl script building out the base block design that other yellow blocks (e.g., rfdc can access and
+        # update based on user configuration
+        tcl_cmds['init'] += ['source {:s}/infrastructure/{:s}.tcl'.format(self.hdl_root, self.blkdesign)]
+        """
+        Add a block design to project with wrapper via its exported tcl script.
+          - Validate and save board design against all edits children yb made
+          - Generate output products for all IP in block design
+          - Have vivado make an HDL wrapper around the block design.
+          - Add the wrapper HDL file to project.
+        """
+        tcl_cmds['pre_synth'] = []
+        tcl_cmds['pre_synth'] += ['validate_bd_design']
+        tcl_cmds['pre_synth'] += ['save_bd_design']
+        tcl_cmds['pre_synth'] += ['generate_target all [get_files [get_property directory [current_project]]/myproj.srcs/sources_1/bd/{:s}/{:s}.bd]'.format(self.blkdesign, self.blkdesign)]
+        tcl_cmds['pre_synth'] += ['make_wrapper -files [get_files [get_property directory [current_project]]/myproj.srcs/sources_1/bd/{:s}/{:s}.bd] -top'.format(self.blkdesign, self.blkdesign)]
+        tcl_cmds['pre_synth'] += ['add_files -norecurse [get_property directory [current_project]]/myproj.srcs/sources_1/bd/zrf16_49dr_base/hdl/{:s}_wrapper.vhd'.format(self.blkdesign)]
+        tcl_cmds['pre_synth'] += ['update_compile_order -fileset sources_1']
+
+        tcl_cmds['post_synth'] = []
+        # TODO: make note of how to use HD bank global clocks (HDGC pins) to drive an MMCM on US+
+        #tcl_cmds['post_synth'] += ['set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_100_p]']
+        tcl_cmds['post_synth'] += ['set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets pl_clk_p]']
+
+        return tcl_cmds
+
+
---
+++ jasper_library/yellow_blocks/zrf16_49dr.py
@@ -0,0 +1,9 @@
+from .yellow_block import YellowBlock
+from .zrf16 import zrf16
+
+class zrf16_49dr(YellowBlock):
+    @staticmethod
+    def factory(blk, plat, hdl_root=None):
+        return zrf16(blk, plat, hdl_root)
+
+
