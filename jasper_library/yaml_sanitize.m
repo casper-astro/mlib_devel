24aaa006f00135cacdbcce4879ec5af688733167
Jack <jack@realtimeradio.co.uk>
2021-04-13 17:11:52 +0000

    Add documentation fields to shared bram for autodoc-ing

--- jasper_library/yaml_sanitize.m
+++ jasper_library/yaml_sanitize.m
@@ -2,11 +2,10 @@ function [clean_str] = yaml_sanitize(str)
 
 % catch things likely to kill yaml (namely colons in values)
 
-if ~isempty(strfind(str,':'))
+if contains(str,':')
     clean_str = ['''' str ''''];
+elseif contains(str,char(10)) % newline '\n'
+    clean_str = ['"' str '"'];
 else
     clean_str = str;
 end
-
-
-
