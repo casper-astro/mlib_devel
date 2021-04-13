b9a7758be80cbfc134dfffd10a7b3e5688d63aab
Jack <jack@realtimeradio.co.uk>
2021-04-13 19:53:12 +0000

    Switch condition order

--- jasper_library/yaml_sanitize.m
+++ jasper_library/yaml_sanitize.m
@@ -1,11 +1,10 @@
 function [clean_str] = yaml_sanitize(str)
 
 % catch things likely to kill yaml (namely colons in values)
-
-if contains(str,':')
-    clean_str = ['''' str ''''];
-elseif contains(str,char(10)) % newline '\n'
+if contains(str, newline)
     clean_str = ['"' str '"'];
+elseif contains(str,':')
+    clean_str = ['''' str ''''];
 else
     clean_str = str;
 end
