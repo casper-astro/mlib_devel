d9e2b04bd68e089dd71ce47413742f93323f51a1
Jack <jack@realtimeradio.co.uk>
2021-06-17 12:59:49 +0000

    HACK: crowbar use_pr attribute into platform object
    So other yellow blocks can make decisions based on it.

--- jasper_library/yellow_blocks/adm_pcie_9h7.py
+++ jasper_library/yellow_blocks/adm_pcie_9h7.py
@@ -2,6 +2,15 @@ from .yellow_block import YellowBlock
 from constraints import ClockConstraint, PortConstraint, RawConstraint
 
 class adm_pcie_9h7(YellowBlock):
+    def __init__(self, blk, platform, hdl_root=None):
+        # HACK to crowbar the partial reconf flag into a platform attribute
+        YellowBlock.__init__(self, blk, platform, hdl_root=hdl_root)
+        try:
+            if self.use_pr:
+                platform.use_pr = True
+        except AttributeError:
+            self.use_pr = False
+
     def initialize(self):
         self.add_source('infrastructure/adm_pcie_9h7_infrastructure.v')
         self.add_source('wbs_arbiter')
@@ -24,10 +33,25 @@ class adm_pcie_9h7(YellowBlock):
         top.assign_signal('sys_clk90', '~sys_clk270')
 
     def gen_children(self):
-        children = [YellowBlock.make_block({'tag':'xps:sys_block', 'board_id':'12', 'rev_maj':'12', 'rev_min':'0', 'rev_rcs':'32'}, self.platform)]
         enable_wishbone = 'wishbone' in self.platform.mmbus_architecture
-        children.append(YellowBlock.make_block({'tag':'xps:pci_dma_axilite_master',
-                                                'enable_wishbone': enable_wishbone}, self.platform))
+        children = [YellowBlock.make_block({
+                        'tag':'xps:sys_block',
+                        'board_id':'12',
+                        'rev_maj':'12',
+                        'rev_min':'0',
+                        'rev_rcs':'32',
+                    }, self.platform)]
+        if self.use_microblaze:
+            pass
+        else:
+            b = YellowBlock.make_block({
+                'tag' : 'xps:pci_dma_axilite_master',
+                'use_pr' : self.use_pr,
+                'template_project' : self.pr_template,
+                'enable_wishbone' : enable_wishbone,
+                }, self.platform)
+            children.append(b)
+
         return children
 
     def gen_constraints(self):
