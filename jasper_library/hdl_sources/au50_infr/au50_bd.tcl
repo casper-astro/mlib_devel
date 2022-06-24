d326289bafeee79f5737bdf3ff18a74a12c19a7b
Morag Brown <moragb96@github.com>
2022-06-24 09:38:44 +0200

    changed part rev number to latest

--- jasper_library/hdl_sources/au50_infr/au50_bd.tcl
+++ jasper_library/hdl_sources/au50_infr/au50_bd.tcl
@@ -44,7 +44,7 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 set list_projs [get_projects -quiet]
 if { $list_projs eq "" } {
    create_project project_1 myproj -part xcu50-fsvh2104-2-e
-   set_property BOARD_PART xilinx.com:au50:part0:1.2 [current_project]
+   set_property BOARD_PART xilinx.com:au50:part0:1.3 [current_project]
 }
 
 
