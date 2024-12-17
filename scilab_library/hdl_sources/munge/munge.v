e9f2907a84dcdbfea88bb6353d6b8cf8ab69a138
Wei Liu <liuwei_berkeley@berkeley.edu>
2024-12-16 20:45:07 -0800

    fixed a bug in munge.v.

--- scilab_library/hdl_sources/munge/munge.v
+++ scilab_library/hdl_sources/munge/munge.v
@@ -1,13 +1,13 @@
 module munge#(
-    parameter total_bits = 128,
+    parameter g_total_bits = 128,
     parameter g_number_of_divisions = 4,
     parameter g_division_size_bits = 32,
     parameter [15:0] g_packing_order = {4'd3, 4'd2, 4'd1, 4'd0}
 )(
     input clk,
     input ce,
-    input [total_bits - 1 : 0] in,
-    output [total_bits - 1 : 0] out
+    input [g_total_bits - 1 : 0] in,
+    output [g_total_bits - 1 : 0] out
 );
 
 assign out = {in[31:0], in[63:32], in[95:64], in[127:96]};
