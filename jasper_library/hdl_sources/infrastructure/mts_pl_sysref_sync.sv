8d2a8e2b670a2c206ae1291208c0e92202f6a497
Mitch Burnett <mitch.burnett@byu.edu>
2025-05-29 18:07:28 -0600

    add generation of dac mts sysref capture

--- jasper_library/hdl_sources/infrastructure/mts_pl_sysref_sync.sv
+++ jasper_library/hdl_sources/infrastructure/mts_pl_sysref_sync.sv
@@ -2,15 +2,19 @@
 `default_nettype none 
 
 module mts_pl_sysref_sync #(
-  parameter SYNC_FFS=3
+  parameter SYNC_FFS=3,
+  parameter ADC_SYSREF = 1,
+  parameter DAC_SYSREF = 0
 ) (
   input wire logic pl_sysref_p,
   input wire logic pl_sysref_n,
   input wire logic pl_clk,
-  output wire user_sysref_adc
+  output wire user_sysref_adc,
+  output wire user_sysref_dac
 );
 
   wire pl_sysref;
+  wire user_sysref;
 
   IBUFDS #(
     .DQS_BIAS("FALSE")
@@ -26,11 +30,27 @@ module mts_pl_sysref_sync #(
     .SIM_ASSERT_CHK(1),     // enable simulation messages
     .SRC_INPUT_REG(0)       // do not register the input
   ) cdc_inst (
-    .dest_out(user_sysref_adc),
+    .dest_out(user_sysref),
     .dest_clk(pl_clk),
     .src_clk(1'b0),
     .src_in(pl_sysref)
   );
+
+  generate
+    if (ADC_SYSREF) begin : gen_adc_sysref
+      assign user_sysref_adc = user_sysref;
+    end else begin
+      assign user_sysref_adc = 1'b0;
+    end
+  endgenerate
+
+  generate
+    if (DAC_SYSREF) begin : gen_dac_sysref
+      assign user_sysref_dac = user_sysref;
+    end else begin
+      assign user_sysref_dac = 1'b0;
+    end
+  endgenerate
 
 endmodule : mts_pl_sysref_sync
 
