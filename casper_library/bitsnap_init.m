791c477d0e5039c27cf6d61398bc967fd43afdf1
Mitch Burnett <mitch.burnett@byu.edu>
2025-09-27 22:40:02 -0600

    another tweak to correct assert block's airth type

--- casper_library/bitsnap_init.m
+++ casper_library/bitsnap_init.m
@@ -152,7 +152,7 @@ function stype = type_to_string(arith_type)
             stype = 'Unsigned';
             return
         case 1
-            stype = 'Signed  (2''s comp)';
+            stype = 'Signed (2''s comp)';
             return
         case 2
             stype = 'Boolean';
