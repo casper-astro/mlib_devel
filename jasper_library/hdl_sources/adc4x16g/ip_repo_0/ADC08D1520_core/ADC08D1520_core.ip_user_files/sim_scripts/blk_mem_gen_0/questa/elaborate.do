5d209ca420bc3567b38147741f08c6b294513dfd
Mitch Burnett <mcb@isotropicventures.com>
2026-03-03 13:33:18 -0700

    Purge adc4x16 (adc_16g_asnt) sources/block
    Continue to remove deprecated hardware and platforms.
    
    Removing this adc also removes its wishbone controller. The various adcs
    that have been added have used a version of this wb controller as a
    template for rapidly integrating adcs. The snap adc kept the adc16 wb
    controller that may serve as that template, but various additions were
    made to each controller based on the needs.

--- jasper_library/hdl_sources/adc4x16g/ip_repo_0/ADC08D1520_core/ADC08D1520_core.ip_user_files/sim_scripts/blk_mem_gen_0/questa/elaborate.do
+++
@@ -1,1 +0,0 @@
-vopt -64 +acc -l elaborate.log -L unisims_ver -L unimacro_ver -L secureip -L xil_defaultlib -L xpm -L blk_mem_gen_v8_3_3 -work xil_defaultlib xil_defaultlib.blk_mem_gen_0 xil_defaultlib.glbl -o blk_mem_gen_0_opt
--- jasper_library/hdl_sources/adc4x16g/ip_repo_0/ADC08D1520_core/ADC08D1520_core.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim testbench_behav -key {Behavioral:sim_1:Functional:testbench} -tclbatch testbench.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
