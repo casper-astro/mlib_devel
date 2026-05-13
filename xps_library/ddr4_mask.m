e7fc285675975dd8b23930a4a9355c1016c3daa1
Mitch Burnett <mcb@isotropicventures.com>
2026-05-12 21:59:40 -0600

    replace intial sources with reworked version

--- xps_library/ddr4_mask.m
+++ xps_library/ddr4_mask.m
@@ -24,20 +24,23 @@ function ddr4_mask(myname)
 gateway_outs = find_system(myname,'searchdepth',1,'FollowLinks', 'on','lookundermasks','all','masktype','Gateway Out Block');
 for i =1:length(gateway_outs)
     gw = gateway_outs{i};
-    if regexp(get_param(gw,'Name'),'(ddr4_we)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_we)$','tokens');
+    if regexp(get_param(gw,'Name'),'(ddr4_din)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_din)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_addr)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_addr)$','tokens');
+    elseif regexp(get_param(gw,'Name'),'(ddr4_vld)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_vld)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
     elseif regexp(get_param(gw,'Name'),'(ddr4_cmd)$')
         toks = regexp(get_param(gw,'Name'),'(ddr4_cmd)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_din)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_din)$','tokens');
+    elseif regexp(get_param(gw,'Name'),'(ddr4_addr)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_addr)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_rx_fifo_rd_en)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_rx_fifo_rd_en)$','tokens');
+    elseif regexp(get_param(gw,'Name'),'(ddr4_din_mask)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_din_mask)$','tokens');
+        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
+    elseif regexp(get_param(gw,'Name'),'(ddr4_user_rd_ready)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_user_rd_ready)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
     else
         error(['Unknown gateway name: ',gw]);
@@ -47,44 +50,17 @@ end
 gateway_ins = find_system(myname,'searchdepth',1,'FollowLinks', 'on','lookundermasks','all','masktype','Gateway In Block');
 for i =1:length(gateway_ins)
     gw = gateway_ins{i};
-    if regexp(get_param(gw,'Name'),'(ddr4_app_rd_data_valid)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_app_rd_data_valid)$','tokens');
+    if regexp(get_param(gw,'Name'),'(ddr4_dout)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_dout)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
     elseif regexp(get_param(gw,'Name'),'(ddr4_out_vld)$')
         toks = regexp(get_param(gw,'Name'),'(ddr4_out_vld)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_app_rdy)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_app_rdy)$','tokens');
+    elseif regexp(get_param(gw,'Name'),'(ddr4_user_cmd_ready)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_user_cmd_ready)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_fifo_out_overflow)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_fifo_out_overflow)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_fifo_out_underflow)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_fifo_out_underflow)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_fifo_in_overflow)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_fifo_in_overflow)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_fifo_in_underflow)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_fifo_in_underflow)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_calib_complete)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_calib_complete)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_dout)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_dout)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_in_fifo_rd_dc)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_in_fifo_rd_dc)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_in_fifo_wr_dc)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_in_fifo_wr_dc)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_out_fifo_rd_dc)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_out_fifo_rd_dc)$','tokens');
-        set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
-    elseif regexp(get_param(gw,'Name'),'(ddr4_out_fifo_wr_dc)$')
-        toks = regexp(get_param(gw,'Name'),'(ddr4_out_fifo_wr_dc)$','tokens');
+    elseif regexp(get_param(gw,'Name'),'(ddr4_ddr_fifo_ready)$')
+        toks = regexp(get_param(gw,'Name'),'(ddr4_ddr_fifo_ready)$','tokens');
         set_param(gw,'Name',clear_name([myname,'_',toks{1}{1}]));
     else
         error(['Unkown gateway name: ',gw]);
